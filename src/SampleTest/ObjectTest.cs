2bcf9fed5b3035bf4da33e2276571e866b36ed2b
ichiroku11 <toolbox0x10@gmail.com>
2020-11-26 08:20:30 +0900

    Comment

--- src/SampleTest/ObjectTest.cs
+++ src/SampleTest/ObjectTest.cs
@@ -26,11 +26,16 @@ namespace SampleTest {
 			Assert.False(ReferenceEquals(null, new object()));
 		}
 
+		// C#9.0から？値型の比較は警告になった様子
+		// warning CA2013
+		// 値のボックス化により、'ReferenceEquals'へのこの呼び出しは、常に'false'を返します
+		/*
 		[Fact]
 		public void ReferenceEquals_値型の比較は常にfalse() {
 			// ボックス化されるから、値型の比較は常にfalse
 			Assert.False(ReferenceEquals(1, 1));
 			Assert.False(ReferenceEquals(1L, 1L));
 		}
+		*/
 	}
 }
