be97ad78faa2524bb03aee95f9010f48e3169cf2
ichiroku11 <toolbox0x10@gmail.com>
2020-11-27 08:06:15 +0900

    Test IPAddress.AddressFamily

--- src/SampleTest/Net/IPAddressTest.cs
+++ src/SampleTest/Net/IPAddressTest.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -15,8 +16,19 @@ namespace SampleTest.Net {
 			yield return new object[] { IPAddress.None, "255.255.255.255" };
 		}
 
-		[MemberData(nameof(GetTestDataForToString))]
 		[Theory]
+		[InlineData("127.0.0.1")]
+		[InlineData("0.0.0.0")]
+		[InlineData("255.255.255.255")]
+		public void AddressFamily_IPv4かどうかを判定する(string address) {
+			// Arrange
+			// Act
+			// Assert
+			Assert.Equal(AddressFamily.InterNetwork, IPAddress.Parse(address).AddressFamily);
+		}
+
+		[Theory]
+		[MemberData(nameof(GetTestDataForToString))]
 		public void ToString_文字列表現を確認する(IPAddress address, string expected) {
 			// Arrange
 			// Act
