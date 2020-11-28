6c6c81bffaa04379c04af3f85e22abac6ca77165
ichiroku11 <toolbox0x10@gmail.com>
2020-11-28 10:02:20 +0900

    Add IPAddressExtensions

---
+++ src/SampleTest/Net/IPAddressExtensions.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest.Net {
+	public static class IPAddressExtensions {
+		/*
+		public static IPAddress GetBroadcastAddress(this IPAddress address, int prefix) {
+			// todo:
+			return IPAddress.None;
+		}
+
+		public static IPAddress GetBroadcastAddress(this IPAddress address, IPAddress mask) {
+			// todo:
+			return IPAddress.None;
+		}
+
+		public static IPAddress GetNetworkAddress(this IPAddress address, int prefix) {
+			// todo:
+			return IPAddress.None;
+		}
+
+		public static IPAddress GetNetworkAddress(this IPAddress address, IPAddress mask) {
+			// todo:
+			return IPAddress.None;
+		}
+
+		public static bool IsInSubnet(this IPAddress address, IPAddress subnet) {
+			// todo:
+
+			return false;
+		}
+
+		// https://www.nic.ad.jp/ja/translation/rfc/1918.html
+		public static bool IsPrivate(this IPAddress address) {
+			// IPv4のみとする
+			if (address.AddressFamily != AddressFamily.InterNetwork) {
+				throw new ArgumentException(nameof(address));
+			}
+
+			// todo:
+
+
+			return false;
+		}
+		*/
+	}
+}
