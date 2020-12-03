b840dc92af4c86ff3f43aac3b89f3f2d0a27169f
ichiroku11 <toolbox0x10@gmail.com>
2020-12-04 08:18:01 +0900

    Test Regex

--- src/SampleTest/Text.RegularExpressions/RegexTest.cs
+++ src/SampleTest/Text.RegularExpressions/RegexTest.cs
@@ -9,23 +9,19 @@ using Xunit;
 
 namespace SampleTest.Text.RegularExpressions {
 	public class RegexTest {
-		private const string _input = @"xyz
-abc
-efg
-ab c
-xabc";
-
-		// todo: 特定のキーワードを含む行を削除
-		// todo: 特定のキーワードを含まない行を削除
-
 		[Fact]
 		public void 特定のキーワードを含む行の番号を列挙する() {
 			// Arrange
 			var regex = new Regex("^.*abc.*$");
+			const string input = @"xyz
+abc
+efg
+ab c
+xabc";
 			var results = new List<int>();
 
 			// Act
-			foreach (var (index, line) in _input.Split(Environment.NewLine).Select((line, index) => (index, line))) {
+			foreach (var (index, line) in input.Split(Environment.NewLine).Select((line, index) => (index, line))) {
 				var match = regex.Match(line);
 				if (match.Success) {
 					results.Add(index);
@@ -36,15 +32,39 @@ xabc";
 			Assert.Equal(new[] { 1, 4 }, results);
 		}
 
+		[Fact]
+		public void 特定のキーワードを含む行を削除する() {
+			// Arrange
+			var regex = new Regex(@"^.*abc.*$(\n|\r\n)*", RegexOptions.Multiline);
+			const string input = @"xyz
+abc
+efg
+ab c
+xabc";
+			// Act
+			var actual = regex.Replace(input, "");
+
+			// Assert
+			const string expected = @"xyz
+efg
+ab c
+";
+			Assert.Equal(expected, actual);
+		}
 
 		[Fact]
 		public void 特定のキーワードを含まない行の番号を列挙する() {
 			// Arrange
 			var regex = new Regex("^(?!.*abc).*$");
+			const string input = @"xyz
+abc
+efg
+ab c
+xabc";
 			var results = new List<int>();
 
 			// Act
-			foreach (var (index, line) in _input.Split(Environment.NewLine).Select((line, index) => (index, line))) {
+			foreach (var (index, line) in input.Split(Environment.NewLine).Select((line, index) => (index, line))) {
 				var match = regex.Match(line);
 				if (match.Success) {
 					results.Add(index);
@@ -53,6 +73,24 @@ xabc";
 
 			// Assert
 			Assert.Equal(new[] { 0, 2, 3 }, results);
+		}
+
+		[Fact]
+		public void 特定のキーワードを含まない行を削除する() {
+			// Arrange
+			var regex = new Regex("^(?!.*abc).*$(\n|\r\n)*", RegexOptions.Multiline);
+			const string input = @"xyz
+abc
+efg
+ab c
+xabc";
+			// Act
+			var actual = regex.Replace(input, "");
+
+			// Assert
+			const string expected = @"abc
+xabc";
+			Assert.Equal(expected, actual);
 		}
 	}
 }
