a9f596dc23414bace6dfe7c42c8e8837869571e6
ichiroku11 <toolbox0x10@gmail.com>
2020-12-28 09:43:37 +0900

    Add XML comment

--- src/OpenApiWebApi/Controllers/MonsterController.cs
+++ src/OpenApiWebApi/Controllers/MonsterController.cs
@@ -8,10 +8,18 @@ using System.Threading.Tasks;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace OpenApiWebApi.Controllers {
+	/// <summary>
+	/// 
+	/// </summary>
 	[Route("api/[controller]")]
 	[ApiController]
 	public class MonsterController : ControllerBase {
-		// GET: api/book
+		// アクションメソッドのsummaryがOpenAPIに出力される
+		// GET: api/monster
+		/// <summary>
+		/// <see cref="Monster"/>一覧を取得
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet]
 		public IEnumerable<Monster> Get() {
 			return new [] {
@@ -28,24 +36,24 @@ namespace OpenApiWebApi.Controllers {
 
 		// todo:
 		/*
-		// GET api/<BookController>/5
+		// GET api/<MonsterController>/5
 		[HttpGet("{id}")]
 		public string Get(int id) {
 			return "value";
 		}
 
-		// POST api/<BookController>
+		// POST api/<MonsterController>
 		[HttpPost]
 		public void Post([FromBody] string value) {
 		}
 
-		// PUT api/<BookController>/5
+		// PUT api/<MonsterController>/5
 		[HttpPut("{id}")]
 		public void Put(int id, [FromBody] string value) {
 		}
 
 
-		// DELETE api/<BookController>/5
+		// DELETE api/<MonsterController>/5
 		[HttpDelete("{id}")]
 		public void Delete(int id) {
 		}
--- src/OpenApiWebApi/Models/Monster.cs
+++ src/OpenApiWebApi/Models/Monster.cs
@@ -4,8 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace OpenApiWebApi.Models {
+	// summaryがOpenAPIに出力される
+	/// <summary>
+	/// モンスター
+	/// </summary>
 	public class Monster {
+		/// <summary>
+		/// ID
+		/// </summary>
 		public int Id { get; set; }
+		/// <summary>
+		/// 名前
+		/// </summary>
 		public string Name { get; set; }
 	}
 }
