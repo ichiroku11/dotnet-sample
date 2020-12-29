e5b32b396a5b559b499b182120a711999c2eb6cc
ichiroku11 <toolbox0x10@gmail.com>
2020-12-29 16:40:37 +0900

    Add MonsterController.Post

--- src/OpenApiWebApi/Controllers/MonsterController.cs
+++ src/OpenApiWebApi/Controllers/MonsterController.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -31,10 +32,13 @@ namespace OpenApiWebApi.Controllers {
 						Name = "ドラキー"
 					}
 				}.ToDictionary(monster => monster.Id));
+		// 最大ID
+		private static int _maxId = 2;
+
 
 		// アクションメソッドのsummaryがOpenAPIに出力される
 		/// <summary>
-		/// <see cref="Monster"/>一覧を取得
+		/// モンスター一覧を取得
 		/// </summary>
 		/// <returns></returns>
 		[HttpGet]
@@ -43,7 +47,7 @@ namespace OpenApiWebApi.Controllers {
 		}
 
 		/// <summary>
-		/// モンスターIDで<see cref="Monster"/>を取得
+		/// モンスターIDでモンスターを取得
 		/// </summary>
 		/// <param name="id">モンスターID</param>
 		/// <returns></returns>
@@ -62,13 +66,30 @@ namespace OpenApiWebApi.Controllers {
 			return monster;
 		}
 
-		// todo:
-		/*
-		// POST api/<MonsterController>
+		/// <summary>
+		/// モンスターを追加
+		/// </summary>
+		/// <param name="request">モンスター追加リクエスト</param>
+		/// <returns></returns>
 		[HttpPost]
-		public void Post([FromBody] string value) {
+		[ProducesResponseType(StatusCodes.Status201Created)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public ActionResult<Monster> Post([FromBody] MonsterAddRequest request) {
+			var id = Interlocked.Increment(ref _maxId);
+			var monster = new Monster {
+				Id = id,
+				Name = request.Name,
+			};
+
+			if (!_monsters.TryAdd(id, monster)) {
+				throw new InvalidOperationException();
+			}
+
+			return CreatedAtAction(nameof(Get), new { id }, monster);
 		}
 
+		// todo:
+		/*
 		// PUT api/<MonsterController>/5
 		[HttpPut("{id}")]
 		public void Put(int id, [FromBody] string value) {
