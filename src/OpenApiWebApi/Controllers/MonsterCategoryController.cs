e521b88567d951f26334503fef86ea5b8965cf95
ichiroku11 <toolbox0x10@gmail.com>
2021-01-03 16:33:51 +0900

    Update MonsterCategoryController

--- src/OpenApiWebApi/Controllers/MonsterCategoryController.cs
+++ src/OpenApiWebApi/Controllers/MonsterCategoryController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NSwag.Annotations;
+using OpenApiWebApi.Models;
+using SampleLib;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,29 @@ using System.Threading.Tasks;
 
 namespace OpenApiWebApi.Controllers {
 	/// <summary>
-	/// 
+	/// モンスターカテゴリコントローラ
 	/// </summary>
 	[Route("api/[controller]")]
 	[ApiController]
 	[ApiConventionType(typeof(ApiConventions))]
 	[OpenApiTag("MonsterCategory", Description = "モンスターカテゴリ")]
 	public class MonsterCategoryController : ControllerBase {
+		/// <summary>
+		/// モンスターカテゴリを取得
+		/// </summary>
+		/// <param name="category"></param>
+		/// <returns></returns>
+		[HttpGet("{category}")]
+		[ApiConventionMethod(typeof(ApiConventions), nameof(ApiConventions.Get))]
+		public ActionResult<MonsterCategoryResponse> Get(MonsterCategory category) {
+			if (category == MonsterCategory.Unknown) {
+				return BadRequest();
+			}
 
+			return new MonsterCategoryResponse {
+				Category = category,
+				CategoryName = category.DisplayName(),
+			};
+		}
 	}
 }
