77ba4d26d4295dc178f9d4ab8251734977113aa4
ichiroku11 <toolbox0x10@gmail.com>
2021-01-04 10:16:31 +0900

    Test JsonStringEnumConverter

--- src/SampleTest/Text.Json/JsonStringEnumConverterTest.cs
+++ src/SampleTest/Text.Json/JsonStringEnumConverterTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,7 +13,7 @@ namespace SampleTest.Text.Json {
 		private enum SampleCode {
 			Unknown = 0,
 			Ok,
-			Error,
+			NotFound,
 		}
 
 		private class Sample {
@@ -25,6 +26,7 @@ namespace SampleTest.Text.Json {
 			var options = new JsonSerializerOptions {
 				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
 			};
+
 			var data = new Sample {
 				Code = SampleCode.Ok,
 			};
@@ -36,6 +38,27 @@ namespace SampleTest.Text.Json {
 			Assert.Equal(@"{""code"":1}", json);
 		}
 
+		[Theory]
+		// JsonStringEnumConverterを使うと、enum値を文字列でシリアライズできる
+		[InlineData(false, @"{""code"":""NotFound""}")]
+		// JsonStringEnumConverterにJsonNamingPolicy.CamelCaseを指定すると、enum値をキャメルケースでシリアライズできる
+		[InlineData(true, @"{""code"":""notFound""}")]
+		public void JsonStringEnumConverterを使ってenumを文字列としてシリアライズする(bool camelCase, string expected) {
+			// Arrange
+			var options = new JsonSerializerOptions {
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			};
+			options.Converters.Add(new JsonStringEnumConverter(camelCase ? JsonNamingPolicy.CamelCase : null));
 
+			var data = new Sample {
+				Code = SampleCode.NotFound,
+			};
+
+			// Act
+			var actual = JsonSerializer.Serialize(data, options);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
 	}
 }
