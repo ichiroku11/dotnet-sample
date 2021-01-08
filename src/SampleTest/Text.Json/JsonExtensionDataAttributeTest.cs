02bd5e6407599e985260dbd7ee9ab0627960d6e3
ichiroku11 <toolbox0x10@gmail.com>
2021-01-08 18:25:32 +0900

    Test JsonExtensionDataAttribute

--- src/SampleTest/Text.Json/JsonExtensionDataAttributeTest.cs
+++ src/SampleTest/Text.Json/JsonExtensionDataAttributeTest.cs
@@ -6,22 +6,30 @@ using System.Text.Json.Serialization;
 using Xunit;
 
 namespace SampleTest.Text.Json {
+	// JsonExtensionData属性を使ってモデルのプロパティに含まれないデータを扱う
+	// 参考
+	// https://docs.microsoft.com/ja-jp/dotnet/standard/serialization/system-text-json-handle-overflow
+	// https://docs.microsoft.com/ja-jp/dotnet/api/system.text.json.serialization.jsonextensiondataattribute?view=net-5.0
 	public class JsonExtensionDataAttributeTest {
 		private class ExtensionDataSample {
 			public int Number { get; set; }
 			public string Text { get; set; }
+			// JsonExtensionData属性を指定できるのは、
+			// Dictionary<string, object>またはDictionary<string, JsonElement>
 			[JsonExtensionData]
 			public Dictionary<string, object> Exts { get; set; }
 		}
 
 		[Fact]
-		public void JsonExtensionData属性を使ってプロパティで表されないデータをシリアライズする() {
+		public void Serialize_JsonExtensionData属性を使ってプロパティで表されないデータをシリアライズする() {
 			// Arrange
 			var data = new ExtensionDataSample {
 				Number = 1,
 				Text = "Abc",
 				Exts = new Dictionary<string, object> {
-					["other"] = "x",
+					["other1"] = "x",
+					["other2"] = true,
+					["other3"] = 1,
 				},
 			};
 			var options = new JsonSerializerOptions {
@@ -32,14 +40,14 @@ namespace SampleTest.Text.Json {
 			var actual = JsonSerializer.Serialize(data, options);
 
 			// Assert
-			var expected = @"{""number"":1,""text"":""Abc"",""other"":""x""}";
+			var expected = @"{""number"":1,""text"":""Abc"",""other1"":""x"",""other2"":true,""other3"":1}";
 			Assert.Equal(expected, actual);
 		}
 
 		[Fact]
-		public void JsonExtensionData属性を使ってプロパティで表されないデータをデシリアライズする() {
+		public void Deserialize_JsonExtensionData属性を使ってプロパティで表されないデータをデシリアライズする() {
 			// Arrange
-			var json = @"{""number"":1,""text"":""Abc"",""other"":""x""}";
+			var json = @"{""number"":1,""text"":""Abc"",""other1"":""x"",""other2"":true,""other3"":1}";
 			var options = new JsonSerializerOptions {
 				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
 			};
@@ -50,8 +58,15 @@ namespace SampleTest.Text.Json {
 			// Assert
 			Assert.Equal(1, data.Number);
 			Assert.Equal("Abc", data.Text);
-			Assert.Single(data.Exts);
-			Assert.Equal("x", data.Exts["other"].ToString());
+			Assert.Equal(3, data.Exts.Count);
+
+			// 型はJsonElementとなる
+			var other1 = Assert.IsType<JsonElement>(data.Exts["other1"]);
+			Assert.Equal("x", other1.GetString());
+			var other2 = Assert.IsType<JsonElement>(data.Exts["other2"]);
+			Assert.True(other2.GetBoolean());
+			var other3 = Assert.IsType<JsonElement>(data.Exts["other3"]);
+			Assert.Equal(1, other3.GetInt32());
 		}
 	}
 }
