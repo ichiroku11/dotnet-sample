743857c3707ee865c2ac2769dab38808d5d8d11d
ichiroku11 <toolbox0x10@gmail.com>
2021-01-25 15:56:43 +0900

    Test HashSet<T>.SetEquals

--- src/SampleTest/Collections/HashSetTest.cs
+++ src/SampleTest/Collections/HashSetTest.cs
@@ -42,5 +42,20 @@ namespace SampleTest.Collections {
 			// Assert
 			Assert.False(contains);
 		}
+
+		[Theory]
+		[InlineData(new[] { 0, 1, 2, 3, 4 }, true)]
+		[InlineData(new[] { 0, 1, 2, 3 }, false)]
+		[InlineData(new[] { 0, 1, 2, 3, 4, 5 }, false)]
+		public void SetEquals_otherが同じ要素を含んでいるかどうかを判定する(IEnumerable<int> other, bool expected) {
+			// Arrange
+			var set = new HashSet<int> { 0, 1, 2, 3, 4, };
+
+			// Act
+			var actual = set.SetEquals(other);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
 	}
 }
