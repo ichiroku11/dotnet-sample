ccb02718fe7bc86a4f83cb8232e63805fbf0536e
ichiroku11 <toolbox0x10@gmail.com>
2021-03-15 12:14:21 +0900

    Update CompositeKeyTest

--- src/SampleTest/EntityFrameworkCore/CompositeKeyTest.cs
+++ src/SampleTest/EntityFrameworkCore/CompositeKeyTest.cs
@@ -9,6 +9,21 @@ namespace SampleTest.EntityFrameworkCore {
 	// 参考
 	// https://docs.microsoft.com/ja-jp/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-composite-key%2Csimple-key#foreign-key
 	public class CompositeKeyTest : IDisposable {
+		private class Sample {
+			public int Id { get; init; }
+
+			public string Value { get; init; }
+
+			public List<SampleDetail> Details { get; init; }
+		}
+
+		private class SampleDetail {
+			public int SampleId { get; init; }
+
+			public int DetailNo { get; init; }
+
+			public string Value { get; init; }
+		}
 
 		public void Dispose() {
 			// todo:
