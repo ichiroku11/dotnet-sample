eb714393467b21acbb1cae8c40844a211b2f015d
ichiroku11 <toolbox0x10@gmail.com>
2021-03-15 12:12:39 +0900

    Add CompositeKeyTest

---
+++ src/SampleTest/EntityFrameworkCore/CompositeKeyTest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest.EntityFrameworkCore {
+	// 複合主キー、複合外部キーを試す
+	// 参考
+	// https://docs.microsoft.com/ja-jp/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-composite-key%2Csimple-key#foreign-key
+	public class CompositeKeyTest : IDisposable {
+
+		public void Dispose() {
+			// todo:
+		}
+	}
+}
