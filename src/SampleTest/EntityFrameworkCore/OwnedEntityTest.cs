9e0b9dafe49fac1a00e14160f4d3b5c990d2bb76
ichiroku11 <toolbox0x10@gmail.com>
2021-03-19 07:49:39 +0900

    Try owned types

--- src/SampleTest/EntityFrameworkCore/OwnedEntityTest.cs
+++ src/SampleTest/EntityFrameworkCore/OwnedEntityTest.cs
@@ -2,21 +2,42 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace SampleTest.EntityFrameworkCore {
 	// 参考
 	// https://docs.microsoft.com/ja-jp/ef/core/modeling/owned-entities
-	public class OwnedEntityTest {
+	public class OwnedEntityTest : IDisposable {
 		// メールアドレス
 		private record MailAddress(string Address, string Name);
+
 		// メール
-		private record Mail(int Id, MailAddress From, MailAddress To);
+		private record Mail {
+			public int Id { get; init; }
+			public MailAddress From { get; init; }
+			public MailAddress To { get; init; }
+		}
+		// このrecord型は無理っぽい（なぜ）
+		// private record Mail(int Id, MailAddress From, MailAddress To);
 
 		private class MailDbContext : AppDbContext {
-			public DbSet<Mail> Mail { get; init; }
+			public DbSet<Mail> Mails { get; init; }
 
 			protected override void OnModelCreating(ModelBuilder modelBuilder) {
-				// todo:
+				modelBuilder.Entity<Mail>().ToTable(nameof(Mail))
+					.OwnsOne(mail => mail.From, ownedBuilder => {
+						ownedBuilder.Property(address => address.Address)
+							.HasColumnName($"{nameof(Mail.From)}{nameof(MailAddress.Address)}");
+						ownedBuilder.Property(address => address.Name)
+							.HasColumnName($"{nameof(Mail.From)}{nameof(MailAddress.Name)}");
+					})
+					.OwnsOne(mail => mail.To, ownedBuilder => {
+						ownedBuilder.Property(address => address.Address)
+							.HasColumnName($"{nameof(Mail.To)}{nameof(MailAddress.Address)}");
+						ownedBuilder.Property(address => address.Name)
+							.HasColumnName($"{nameof(Mail.To)}{nameof(MailAddress.Name)}");
+					});
 			}
 		}
 
@@ -39,15 +60,45 @@ namespace SampleTest.EntityFrameworkCore {
 		}
 
 		private void InitTable() {
-			var sql = @"";
-			// todo:
+			var sql = @"
+create table dbo.Mail(
+	Id int not null,
+	FromAddress nvarchar(20) not null,
+	FromName nvarchar(10) not null,
+	ToAddress nvarchar(20) not null,
+	ToName nvarchar(10) not null,
+	constraint PK_Sample primary key(Id)
+);
+
+insert into dbo.Mail(Id, FromAddress, FromName, ToAddress, ToName)
+output inserted.*
+values
+	(1, N'from@example.jp', N'送信元', N'to@example.jp', N'宛先');";
+
 			_context.Database.ExecuteSqlRaw(sql);
 		}
 
 		private void DropTable() {
-			var sql = @"";
-			// todo:
+			var sql = @"
+drop table if exists dbo.Mail;";
+
 			_context.Database.ExecuteSqlRaw(sql);
+		}
+
+		[Fact]
+		public async Task FirstOrDefault_所有型を取得できる() {
+			// Arrange
+			var expected = new Mail {
+				Id = 1,
+				From = new MailAddress("from@example.jp", "送信元"),
+				To = new MailAddress("to@example.jp", "宛先"),
+			};
+
+			// Act
+			var actual = await _context.Mails.FirstOrDefaultAsync(mail => mail.Id == 1);
+
+			// Assert
+			Assert.Equal(expected, actual);
 		}
 	}
 }
