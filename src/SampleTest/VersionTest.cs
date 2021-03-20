653f0ee29fc3a2c717eb61c43c3a6e3f908ff5e5
ichiroku11 <toolbox0x10@gmail.com>
2021-03-20 09:18:45 +0900

    Test System.Version

--- src/SampleTest/VersionTest.cs
+++ src/SampleTest/VersionTest.cs
@@ -32,5 +32,25 @@ namespace SampleTest {
 			Assert.Equal(3, version.Build);
 			Assert.Equal(4, version.Revision);
 		}
+
+		[Theory]
+		// 左 == 右
+		[InlineData("0.0.0.0", "0.0.0.0", true)]
+		// 左 >= 右（各構成要素）
+		[InlineData("0.0.0.2", "0.0.0.1", true)]
+		[InlineData("0.0.2.0", "0.0.1.0", true)]
+		[InlineData("0.2.0.0", "0.1.0.0", true)]
+		[InlineData("2.0.0.0", "1.0.0.0", true)]
+		public void OperatorGreaterThanOrEqual_左の値が右の値以上かどうかを判断する(string leftText, string rightText, bool expected) {
+			// Arrange
+			var left = new Version(leftText);
+			var right = new Version(rightText);
+
+			// Act
+			var actual = left >= right;
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
 	}
 }
