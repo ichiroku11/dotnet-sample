81ba819431b12e035f8bb8fb240452fdcfe7d742
ichiroku11 <toolbox0x10@gmail.com>
2021-04-12 19:25:22 +0900

    Execute "PING"

--- src/AzureCacheForRedisConsoleApp/Program.cs
+++ src/AzureCacheForRedisConsoleApp/Program.cs
@@ -1,9 +1,30 @@
-﻿using System;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
 
 namespace AzureCacheForRedisConsoleApp {
 	class Program {
-		static void Main(string[] args) {
+		static async Task Main(string[] args) {
 			Console.WriteLine("Hello World!");
+			// https://docs.microsoft.com/ja-jp/azure/azure-cache-for-redis/cache-dotnet-core-quickstart
+
+			var config = new ConfigurationBuilder()
+				.AddUserSecrets<Program>()
+				.Build();
+
+			var connectionString = config.GetConnectionString("Redis");
+
+			var multiplexer = await ConnectionMultiplexer.ConnectAsync(connectionString);
+
+			var database = multiplexer.GetDatabase();
+
+			var command = "PING";
+			Console.WriteLine(command);
+			var result = await database.ExecuteAsync(command);
+
+			// PONG
+			Console.WriteLine(result);
 		}
 	}
 }
