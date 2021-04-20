d266aa6b9b00177cd27a7b23fca00c9cecf24aa6
ichiroku11 <toolbox0x10@gmail.com>
2021-04-20 22:36:44 +0900

    Update TimerHostedService

--- src/HostedServiceWebApp/Services/TimerHostedService.cs
+++ src/HostedServiceWebApp/Services/TimerHostedService.cs
@@ -11,20 +11,48 @@ namespace HostedServiceWebApp.Services {
 	// IHostedServiceを直接使う
 	// 参考
 	// https://docs.microsoft.com/ja-jp/aspnet/core/fundamentals/host/hosted-services
-	public class TimerHostedService : IHostedService {
+	public class TimerHostedService : IHostedService, IDisposable {
 		private readonly ILogger _logger;
+		private Timer _timer;
+		private int _count;
+
 		public TimerHostedService(ILogger<TimerHostedService> logger) {
 			_logger = logger;
 		}
 
+		private void Action(object state) {
+			var count = Interlocked.Increment(ref _count);
+
+			_logger.LogInformation($"{nameof(Action)} {nameof(count)} = {count}");
+
+			// Timerコールバック内で例外が発生しても、次のコールバックは呼び出される
+			/*
+			if (count >= 5) {
+				throw new Exception("Error!");
+			}
+			*/
+		}
+
+		public void Dispose() {
+			_logger.LogInformation(nameof(Dispose));
+
+			_timer?.Dispose();
+			_timer = null;
+		}
+
 		public Task StartAsync(CancellationToken cancellationToken) {
 			_logger.LogInformation(nameof(StartAsync));
+
+			// 5秒ごとにActionを呼び出す
+			_timer = new Timer(Action, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
 
 			return Task.CompletedTask;
 		}
 
 		public Task StopAsync(CancellationToken cancellationToken) {
 			_logger.LogInformation(nameof(StopAsync));
+
+			_timer.Change(Timeout.Infinite, Timeout.Infinite);
 
 			return Task.CompletedTask;
 		}
