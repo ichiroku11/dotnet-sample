418e78bafdeab44b9b3d106e11e8e533bb4d6de8
ichiroku11 <toolbox0x10@gmail.com>
2021-04-22 08:20:05 +0900

    Update RoutineBackgroundService

--- src/HostedServiceWebApp/Services/RoutineBackgroundService.cs
+++ src/HostedServiceWebApp/Services/RoutineBackgroundService.cs
@@ -10,16 +10,27 @@ namespace HostedServiceWebApp.Services {
 	// BackgroundServiceを継承し、定期的な処理を実行するホステッドサービス
 	public class RoutineBackgroundService : BackgroundService {
 		private readonly ILogger _logger;
+		private int _count;
 
 		public RoutineBackgroundService(ILogger<RoutineBackgroundService> logger) {
 			_logger = logger;
 		}
 
-		protected override Task ExecuteAsync(CancellationToken stoppingToken) {
-			_logger.LogInformation(nameof(ExecuteAsync));
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+			_logger.LogInformation($"{nameof(ExecuteAsync)} Start");
 
-			// todo:
-			return Task.CompletedTask;
+			stoppingToken.Register(() => _logger.LogInformation($"{nameof(ExecuteAsync)} Canceled"));
+
+			while (!stoppingToken.IsCancellationRequested) {
+				// 何か定期的に実行する処理
+				var count = Interlocked.Increment(ref _count);
+				_logger.LogInformation($"Loop {nameof(count)} = {count}");
+
+				// 5秒待機
+				await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+			}
+
+			_logger.LogInformation($"{nameof(ExecuteAsync)} End");
 		}
 	}
 }
