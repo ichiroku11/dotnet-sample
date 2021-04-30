bd27ea10f9d1ef4e06728bacb4581a10fcca4c55
ichiroku11 <toolbox0x10@gmail.com>
2021-04-30 09:18:00 +0900

    Test JsonPropertyNameAttribute

--- src/SampleTest/Text.Json/JsonPropertyNameAttributeTest.cs
+++ src/SampleTest/Text.Json/JsonPropertyNameAttributeTest.cs
@@ -10,6 +10,7 @@ using Xunit;
 namespace SampleTest.Text.Json {
 	public class JsonPropertyNameAttributeTest {
 		private class Sample {
+			// JSON文字列中のプロパティ名を指定する
 			[JsonPropertyName("Val")]
 			public int Value { get; init; }
 		}
@@ -27,6 +28,16 @@ namespace SampleTest.Text.Json {
 			// Assert
 			// PropertyNamingPolicyよりJsonPropertyNameAttributeが優先される
 			Assert.Equal(@"{""Val"":1}", sample);
+		}
+
+		[Fact]
+		public void Deserialize_JsonPropertyNameAttributeを使ってデシリアライズする() {
+			// Arrange
+			// Act
+			var sample = JsonSerializer.Deserialize<Sample>(@"{""Val"":1}");
+
+			// Assert
+			Assert.Equal(1, sample.Value);
 		}
 	}
 }
