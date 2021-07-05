93aa069bb862fabbb71ee7405495acfe8a9e92ce
ichiroku11 <toolbox0x10@gmail.com>
2021-07-05 22:05:13 +0900

    Use Microsoft Graph API

--- src/AzureAdB2cConsoleApp/Program.cs
+++ src/AzureAdB2cConsoleApp/Program.cs
@@ -1,9 +1,51 @@
-﻿using System;
+using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+using System.Threading.Tasks;
 
 namespace AzureAdB2cConsoleApp {
 	class Program {
-		static void Main(string[] args) {
+		// todo:
+		private const string _tenantId = "";
+		private const string _clientId = "";
+		private const string _clientSecret = "";
+
+		static async Task Main(string[] args) {
 			Console.WriteLine("Hello World!");
+
+			var confidentialClientApp = ConfidentialClientApplicationBuilder
+				.Create(_clientId)
+				.WithTenantId(_tenantId)
+				.WithClientSecret(_clientSecret)
+				.Build();
+			var clientCredentialProvider = new ClientCredentialProvider(confidentialClientApp);
+
+			var client = new GraphServiceClient(clientCredentialProvider);
+
+			// ユーザ一覧を取得
+			var result = await client.Users
+				.Request()
+				.Select(user => new {
+					user.Id,
+					user.DisplayName,
+					user.Surname,
+					user.GivenName,
+					user.Mail,
+					// todo:
+					//user.Extensions
+				})
+				.GetAsync();
+
+			foreach (var user in result.CurrentPage) {
+				Console.WriteLine($"{user.Id}, {user.Surname} {user.GivenName} {user.Mail}");
+				// todo: Extensions
+				/*
+				foreach (var extension in user.Extensions) {
+				Console.WriteLine(extension);
+				}
+				*/
+			}
 		}
 	}
 }
