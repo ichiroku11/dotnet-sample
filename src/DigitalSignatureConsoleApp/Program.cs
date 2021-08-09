1f2a7aff150eec08d9091ad22af6deec47507d82
ichiroku11 <toolbox0x10@gmail.com>
2021-08-10 08:28:26 +0900

    Add project

---
+++ src/DigitalSignatureConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DigitalSignatureConsoleApp {
+	class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+			// https://docs.microsoft.com/ja-jp/dotnet/standard/security/cryptographic-signatures
+		}
+	}
+}
