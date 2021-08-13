8fd76829347db0eb7b776fbdbfd9ed065db658bf
ichiroku11 <toolbox0x10@gmail.com>
2021-08-13 14:13:09 +0900

    Update SignatureCreator, SignatureVerifier

---
+++ src/DigitalSignatureConsoleApp/MessageHasher.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignatureConsoleApp {
+	public class MessageHasher {
+		public byte[] Hash(string message) {
+			using var sha256 = SHA256.Create();
+			return sha256.ComputeHash(Encoding.UTF8.GetBytes(message));
+		}
+	}
+}
---
+++ src/DigitalSignatureConsoleApp/SignatureCreator.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalSignatureConsoleApp {
+	// 署名を生成する
+	public class SignatureCreator {
+		private readonly MessageHasher _hasher;
+		private readonly RSAParameters _parameters;
+
+		public SignatureCreator(RSAParameters parameters) {
+			_hasher = new MessageHasher();
+			_parameters = parameters;
+		}
+
+		// （メッセージの署名を生成する）
+		public byte[] CreateSignature(string message) {
+			var messageHash = _hasher.Hash(message);
+
+			using var rsa = RSA.Create(_parameters);
+			var formatter = new RSAPKCS1SignatureFormatter(rsa);
+			formatter.SetHashAlgorithm(HashAlgorithmName.SHA256.Name);
+
+			return formatter.CreateSignature(messageHash);
+		}
+	}
+}
