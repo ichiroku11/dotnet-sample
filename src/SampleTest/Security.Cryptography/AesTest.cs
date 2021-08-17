a1f0dc64873d8ccb1b7389396bb7ea903d069b45
ichiroku11 <toolbox0x10@gmail.com>
2021-08-18 08:05:14 +0900

    Test Aes

--- src/SampleTest/Security.Cryptography/AesTest.cs
+++ src/SampleTest/Security.Cryptography/AesTest.cs
@@ -39,6 +39,10 @@ namespace SampleTest.Security.Cryptography {
 			return reader.ReadToEnd();
 		}
 
+		private static string GetHexString(byte[] bytes) {
+			return BitConverter.ToString(bytes).ToLower().Replace("-", "");
+		}
+
 		private readonly ITestOutputHelper _output;
 
 		public AesTest(ITestOutputHelper output) {
@@ -46,14 +50,35 @@ namespace SampleTest.Security.Cryptography {
 		}
 
 		[Fact]
-		public void CryptoStream_暗号化と復号を試す() {
+		public void 暗号化と復号を試す() {
 			// Arrange
-			var aes = Aes.Create();
+			using var aes = Aes.Create();
 
 			// Act
+			// 暗号化
 			var cipher = Encrypt(aes, "あいうえお");
-			_output.WriteLine(BitConverter.ToString(cipher).ToLower().Replace("-", ""));
+			_output.WriteLine(GetHexString(cipher));
+
+			// 復号
 			var actual = Decrypt(aes, cipher);
+
+			// Assert
+			Assert.Equal("あいうえお", actual);
+		}
+
+		[Fact]
+		public void 異なるAesインスタンスで暗号化と復号を試す() {
+			// Arrange
+			using var aes1 = Aes.Create();
+			using var aes2 = Aes.Create();
+			// 鍵と初期ベクトルを同じにする
+			aes2.Key = aes1.Key;
+			aes2.IV = aes1.IV;
+
+			// Act
+			var cipher = Encrypt(aes1, "あいうえお");
+			_output.WriteLine(GetHexString(cipher));
+			var actual = Decrypt(aes2, cipher);
 
 			// Assert
 			Assert.Equal("あいうえお", actual);
