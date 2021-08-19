e51979fa54895fb4d4d611a0b764e2f37e5b533a
ichiroku11 <toolbox0x10@gmail.com>
2021-08-20 08:07:34 +0900

    Encrypt IV

--- src/HybridCryptoConsoleApp/Program.cs
+++ src/HybridCryptoConsoleApp/Program.cs
@@ -16,15 +16,24 @@ namespace HybridCryptoConsoleApp {
 			using var rsa = RSA.Create(_parameters);
 			var formatter = new RSAPKCS1KeyExchangeFormatter(rsa);
 
-			using var aes = Aes.Create();
+			// 以下より、IVは暗号化する必要はないが、キーとあわせて暗号化してしまってよいのでは？と解釈
+			// https://docs.microsoft.com/ja-jp/dotnet/standard/security/cryptographic-services
+			// 共有キー暗号方式の弱点は、両者のキーと IV を一致させ、それぞれの値を転送しておく必要がある点です。
+			// IV は秘密情報とは見なされないため、平文のメッセージで転送できます。
+			// しかし、キーは承認されていないユーザーから保護する必要があります。
+			// このような問題のため、共有キー暗号方式は公開キー暗号方式と併用されることがよくあります。
+			// 公開キー暗号方式は、キーと IV の値を秘密に通信するために使用されます。
 
+			using var aes = Aes.Create();
+			// IVを暗号化する
+			var iv = formatter.CreateKeyExchange(aes.IV);
 			// 共通鍵を暗号化してセッションキーとする
 			var key = formatter.CreateKeyExchange(aes.Key);
 
 			// メッセージをAESで暗号化
 			var cipher = aes.Encrypt(plain);
 
-			return (cipher, aes.IV, key);
+			return (cipher, iv, key);
 		}
 	}
 
@@ -41,8 +50,8 @@ namespace HybridCryptoConsoleApp {
 			var deformatter = new RSAPKCS1KeyExchangeDeformatter(rsa);
 
 			using var aes = Aes.Create();
-			aes.IV = iv;
-			// セッションキーを復号して共通鍵を取り出す
+			// IVとセッションキーを復号して共通鍵を取り出す
+			aes.IV = deformatter.DecryptKeyExchange(iv);
 			aes.Key = deformatter.DecryptKeyExchange(key);
 
 			// メッセージをAESで復号
