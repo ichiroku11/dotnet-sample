4ec97768b499715ec2b529bada4b358f5e5ccad5
ichiroku11 <toolbox0x10@gmail.com>
2021-08-22 10:34:23 +0900

    Add controllers and views

--- src/ViewWebApp/Startup.cs
+++ src/ViewWebApp/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -11,6 +10,7 @@ using System.Threading.Tasks;
 namespace ViewWebApp {
 	public class Startup {
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddControllersWithViews();
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
@@ -21,9 +21,9 @@ namespace ViewWebApp {
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints => {
-				endpoints.MapGet("/", async context => {
-					await context.Response.WriteAsync("Hello World!");
-				});
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Default}/{action=Index}/{id?}");
 			});
 		}
 	}
