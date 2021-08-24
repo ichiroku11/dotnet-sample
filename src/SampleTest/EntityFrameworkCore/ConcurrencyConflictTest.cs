254d728779f28966039f767ccbff70cbfc143a00
ichiroku11 <toolbox0x10@gmail.com>
2021-08-25 08:07:36 +0900

    Add ConcurrencyConflictTest.cs

---
+++ src/SampleTest/EntityFrameworkCore/ConcurrencyConflictTest.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest.EntityFrameworkCore {
+	// 参考
+	// https://docs.microsoft.com/ja-jp/ef/core/saving/concurrency
+	// https://docs.microsoft.com/ja-jp/aspnet/core/data/ef-rp/concurrency
+	public class ConcurrencyConflictTest {
+	}
+}
