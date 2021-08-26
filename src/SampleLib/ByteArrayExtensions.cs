5c5794fa5034bfaf0759211ec850d30f970c15c2
ichiroku11 <toolbox0x10@gmail.com>
2021-08-27 08:08:22 +0900

    Add ByteArrayExtensions.ToHexString

---
+++ src/SampleLib/ByteArrayExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLib {
+	public static class ByteArrayExtensions {
+		// 16進数文字列を取得する
+		public static string ToHexString(this byte[] bytes) {
+			return BitConverter.ToString(bytes).ToLower().Replace("-", "");
+		}
+	}
+}
