f18b2ea46d7c6d5111bb667cd066ab210b55299d
ichiroku11 <toolbox0x10@gmail.com>
2021-09-18 08:48:51 +0900

    Use MVC

--- src/ViewComponentWebApp/Startup.cs
+++ src/ViewComponentWebApp/Startup.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 namespace ViewComponentWebApp {
 	public class Startup {
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddControllersWithViews();
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
@@ -21,9 +22,9 @@ namespace ViewComponentWebApp {
 			app.UseRouting();
 
 			app.UseEndpoints(endpoints => {
-				endpoints.MapGet("/", async context => {
-					await context.Response.WriteAsync("Hello World!");
-				});
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Default}/{action=Index}/{id?}");
 			});
 		}
 	}
