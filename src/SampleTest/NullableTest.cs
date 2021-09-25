f15ebdf88df6bcf8dc96b677408d29e1cae201bf
ichiroku11 <toolbox0x10@gmail.com>
2021-09-25 11:39:44 +0900

    Test Nullable.GetUnderlyingType

---
+++ src/SampleTest/NullableTest.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SampleTest {
+	public class NullableTest {
+		// https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/builtin-types/nullable-value-types#how-to-identify-a-nullable-value-type
+
+		private readonly ITestOutputHelper _output;
+
+		public NullableTest(ITestOutputHelper output) {
+			_output = output;
+		}
+
+		[Theory]
+		[InlineData(typeof(int?), "Int32")]
+		[InlineData(typeof(long?), "Int64")]
+		public void GetUnderlyingType_引数にnull許容型の型を指定すると型引数の型を取得できる(Type type, string expectedName) {
+			// Arrange
+			// Act
+			// Returns the underlying type argument of the specified nullable type.
+			var actual = Nullable.GetUnderlyingType(type);
+			_output.WriteLine(actual.Name);
+
+			// Assert
+			Assert.Equal(expectedName, actual.Name);
+		}
+	}
+}
