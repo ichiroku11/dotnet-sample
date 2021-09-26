c6571cdaa23968eb97ea00eff99b4d539b167fb6
ichiroku11 <toolbox0x10@gmail.com>
2021-09-27 07:55:45 +0900

    Test ?. operator

--- src/SampleTest/NullConditionalOperatorTest.cs
+++ src/SampleTest/NullConditionalOperatorTest.cs
@@ -13,7 +13,31 @@ namespace SampleTest {
 			action?.Invoke();
 
 			Assert.True(value);
+		}
 
+		// https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/builtin-types/nullable-value-types#how-to-identify-a-nullable-value-type
+		// 指定した値がnullableかどうか
+		private static bool IsNullable<TValue>(TValue _) {
+			var type = typeof(TValue);
+			return Nullable.GetUnderlyingType(type) != null;
+		}
+
+		private class Sample {
+			public int Value { get; init; }
+		}
+
+		[Fact]
+		public void Null条件演算子_getterプロパティはnull許容型になる() {
+			// Arrange
+			var sample = new Sample {
+				Value = 1,
+			};
+
+			// Act
+			// Assert
+			Assert.False(IsNullable(sample.Value));
+			// null条件演算子でのアクセスはnull許容型になる
+			Assert.True(IsNullable(sample?.Value));
 		}
 	}
 }
