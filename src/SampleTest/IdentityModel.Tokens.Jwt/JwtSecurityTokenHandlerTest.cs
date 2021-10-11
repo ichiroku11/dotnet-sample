64adce8b00ca7d34af9fb7ce82a91cbbef58d6b8
ichiroku11 <toolbox0x10@gmail.com>
2021-10-11 19:29:07 +0900

    Test JwtSecurityTokenHandler

--- src/SampleTest/IdentityModel.Tokens.Jwt/JwtSecurityTokenHandlerTest.cs
+++ src/SampleTest/IdentityModel.Tokens.Jwt/JwtSecurityTokenHandlerTest.cs
@@ -1,10 +1,55 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SampleTest.IdentityModel.Tokens.Jwt {
 	public class JwtSecurityTokenHandlerTest {
+		[Fact]
+		public void CanValidateToken_trueを返す() {
+			// Arrange
+			var handler = new JwtSecurityTokenHandler();
+
+			// Act
+			// Assert
+			Assert.True(handler.CanValidateToken);
+		}
+
+		[Fact]
+		public void CanWriteToken_trueを返す() {
+			// Arrange
+			var handler = new JwtSecurityTokenHandler();
+
+			// Act
+			// Assert
+			Assert.True(handler.CanWriteToken);
+		}
+
+		[Theory]
+		// null、空文字、空白はfalse
+		[InlineData(null, false)]
+		[InlineData("", false)]
+		[InlineData(" ", false)]
+		// header.payload.signatureの形式はtrue
+		[InlineData("0.0.0", true)]
+		[InlineData("a.a.a", true)]
+		// 署名はオプション
+		[InlineData("a.a.", true)]
+		// ドットは2つ必要
+		[InlineData("a.a", false)]
+		public void CanReadToken_トークンの形式を判定する(string token, bool expected) {
+			// Arrange
+			var handler = new JwtSecurityTokenHandler();
+
+			// Act
+			// JSON compact serialization formatかどうかを判定する
+			var actual = handler.CanReadToken(token);
+
+			// Assert
+			Assert.Equal(expected, actual);
+		}
 	}
 }
