dde2ded185bf11a05948a87d230c7befaf1fc099
ichiroku11 <toolbox0x10@gmail.com>
2021-10-20 07:40:40 +0900

    Add X509Certificate2Helper, X509StoreExtensions

---
+++ src/SampleLib/AspNetCore/X509Certificate2Helper.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLib.AspNetCore {
+	// https://docs.microsoft.com/ja-jp/dotnet/core/additional-tools/self-signed-certificates-guide#create-a-self-signed-certificate
+	public static class X509Certificate2Helper {
+		private static X509Certificate2 GetCertificate(string subjectName, string issuerName, string friendlyName) {
+			using var store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+
+			store.Open(OpenFlags.ReadOnly | OpenFlags.OpenExistingOnly);
+
+			return store.GetCertificate(subjectName, issuerName, friendlyName);
+		}
+
+		public static X509Certificate2 GetDevelopmentCertificate()
+			=> GetCertificate(
+				subjectName: "localhost",
+				issuerName: "localhost",
+				friendlyName: "ASP.NET Core HTTPS development certificate");
+	}
+}
