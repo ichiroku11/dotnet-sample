73096f63f3d4066107996d3892bbcecedda66dcb
ichiroku11 <toolbox0x10@gmail.com>
2021-11-03 10:06:17 +0900

    Add MatcherTest

---
+++ src/SampleTest/Extensions.FileSystemGlobbing/MatcherTest.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleTest.Extensions.FileSystemGlobbing {
+	// https://docs.microsoft.com/ja-jp/dotnet/core/extensions/file-globbing
+	public class MatcherTest {
+	}
+}
