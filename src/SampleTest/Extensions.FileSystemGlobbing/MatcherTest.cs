643a410006fe73728a8949e98ea60c87a7617f2d
ichiroku11 <toolbox0x10@gmail.com>
2021-11-03 10:27:33 +0900

    Test Matcher.Match

--- src/SampleTest/Extensions.FileSystemGlobbing/MatcherTest.cs
+++ src/SampleTest/Extensions.FileSystemGlobbing/MatcherTest.cs
@@ -1,11 +1,32 @@
+using Microsoft.Extensions.FileSystemGlobbing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace SampleTest.Extensions.FileSystemGlobbing {
 	// https://docs.microsoft.com/ja-jp/dotnet/core/extensions/file-globbing
 	public class MatcherTest {
+		[Fact]
+		public void Match_マッチが見つかる単純動きを確認する() {
+			// Arrange
+			var matcher = new Matcher();
+			// 含めるパターン指定する
+			matcher.AddInclude("*.md");
+
+			// Act
+			// 指定したパスがマッチするかどうかを確認する
+			var result = matcher.Match("readme.md");
+
+			// Assert
+			Assert.IsType<PatternMatchingResult>(result);
+			Assert.True(result.HasMatches);
+
+			var match = Assert.Single(result.Files);
+			Assert.IsType<FilePatternMatch>(match);
+			Assert.Equal("readme.md", match.Path);
+		}
 	}
 }
