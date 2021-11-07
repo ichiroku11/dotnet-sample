17616c75f2f3316f8af12184b4f29e53cb46aa49
ichiroku11 <toolbox0x10@gmail.com>
2021-11-08 07:48:31 +0900

    Test InMemoryDirectoryInfo.EnumerateFileSystemInfos

--- src/SampleTest/Extensions.FileSystemGlobbing/InMemoryDirectoryInfoTest.cs
+++ src/SampleTest/Extensions.FileSystemGlobbing/InMemoryDirectoryInfoTest.cs
@@ -6,9 +6,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace SampleTest.Extensions.FileSystemGlobbing {
 	public class InMemoryDirectoryInfoTest {
+		private readonly ITestOutputHelper _output;
+
+		public InMemoryDirectoryInfoTest(ITestOutputHelper output) {
+			_output = output;
+		}
+
 		[Fact]
 		public void FullName_フォルダのフルパスを確認できる() {
 			// Arrange
@@ -31,6 +38,45 @@ namespace SampleTest.Extensions.FileSystemGlobbing {
 
 			// Assert
 			Assert.Equal(@"temp", name);
+		}
+
+		[Fact]
+		public void EnumerateFileSystemInfos_カレントディレクトリをルートに指定するとファイルを列挙できる() {
+			// Arrange
+			var root = Directory.GetCurrentDirectory();
+			_output.WriteLine(root);
+
+			var files = new List<string> {
+				"readme.md"
+			};
+			var dirInfo = new InMemoryDirectoryInfo(root, files);
+
+			// Act
+			var fileInfos = dirInfo.EnumerateFileSystemInfos();
+
+			// Assert
+			var fileInfo = Assert.Single(fileInfos);
+			_output.WriteLine(fileInfo.FullName);
+
+			Assert.Equal("readme.md", fileInfo.Name);
+		}
+
+		[Fact]
+		public void EnumerateFileSystemInfos_適当なフォルダをルートに指定するとファイルを列挙できる() {
+			// Arrange
+			var root = @"c:\temp";
+			_output.WriteLine(root);
+
+			var files = new List<string> {
+				"readme.md"
+			};
+			var dirInfo = new InMemoryDirectoryInfo(root, files);
+
+			// Act
+			var fileInfos = dirInfo.EnumerateFileSystemInfos();
+
+			// Assert
+			Assert.Empty(fileInfos);
 		}
 	}
 }
