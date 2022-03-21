04a5269036f584cd9424a886d8e69f41d8ce2169
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-03-22 08:08:56 +0900

    Test GroupJoin

--- src/SampleTest/EntityFrameworkCore/GroupJoinTest.cs
+++ src/SampleTest/EntityFrameworkCore/GroupJoinTest.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Xunit;
 
 namespace SampleTest.EntityFrameworkCore;
 
@@ -30,10 +31,73 @@ public class GroupJoinTest : IDisposable {
 	}
 
 	private void InitTable() {
-		// todo:
+		var sql = @"
+create table dbo.[Outer](
+	Id int not null,
+	Value nvarchar(10) not null,
+	constraint PK_Outer primary key(Id)
+);
+insert into dbo.[Outer](Id, Value)
+output inserted.*
+values
+	(1, N'a'),
+	(2, N'b'),
+	(3, N'c');
+
+create table dbo.[Inner](
+	Id int not null,
+	Value nvarchar(10) not null,
+	constraint PK_Inner primary key(Id)
+);
+insert into dbo.[Inner](Id, Value)
+output inserted.*
+values
+	(1, N'A'),
+	(4, N'D');";
+
+		_context.Database.ExecuteSqlRaw(sql);
 	}
 
 	private void DropTable() {
-		// todo:
+		var sql = @"
+drop table if exists dbo.[Outer];
+drop table if exists dbo.[Inner];";
+
+		_context.Database.ExecuteSqlRaw(sql);
+	}
+
+	[Fact]
+	public async Task GroupJoin_外部結合を行う() {
+		// Arrange
+		// Act
+		var actuals = await _context.Outers
+			.GroupJoin(
+				_context.Inners,
+				outer => outer.Id,
+				inner => inner.Id,
+				(outer, inners) => new {
+					Outer = outer,
+					Inners = inners,
+				})
+			.SelectMany(
+				item => item.Inners.DefaultIfEmpty(),
+				(item, inner) => new { item.Outer, Inner = inner })
+			.OrderBy(item => item.Outer.Id)
+			.ToListAsync();
+
+		// Assert
+		Assert.Collection(actuals,
+			actual => {
+				Assert.Equal(new Outer(1, "a"), actual.Outer);
+				Assert.Equal(new Inner(1, "A"), actual.Inner);
+			},
+			actual => {
+				Assert.Equal(new Outer(2, "b"), actual.Outer);
+				Assert.Null(actual.Inner);
+			},
+			actual => {
+				Assert.Equal(new Outer(3, "c"), actual.Outer);
+				Assert.Null(actual.Inner);
+			});
 	}
 }
