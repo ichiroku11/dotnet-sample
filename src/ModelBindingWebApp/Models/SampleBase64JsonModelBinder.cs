18113c6851218ba2e9b077d1818ebca5cdf4e2cf
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-04-08 08:01:12 +0900

    Update SampleBase64JsonModelBinder

--- src/ModelBindingWebApp/Models/SampleBase64JsonModelBinder.cs
+++ src/ModelBindingWebApp/Models/SampleBase64JsonModelBinder.cs
@@ -5,9 +5,13 @@ using System.Text.Json;
 namespace ModelBindingWebApp.Models;
 
 public class SampleBase64JsonModelBinder : IModelBinder {
+	private static readonly JsonSerializerOptions _jsonSerializerOptions
+		= new() {
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		};
+
 	public Task BindModelAsync(ModelBindingContext bindingContext) {
 		// https://docs.microsoft.com/ja-jp/aspnet/core/mvc/advanced/custom-model-binding?view=aspnetcore-6.0
-
 		var modelName = bindingContext.ModelName;
 		var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
 		if (valueProviderResult == ValueProviderResult.None) {
@@ -16,10 +20,11 @@ public class SampleBase64JsonModelBinder : IModelBinder {
 
 		bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
 
-		// todo:
+		// base64をデコードしてJSON文字列を変換
 		var base64 = valueProviderResult.FirstValue ?? "";
 		var json = Encoding.UTF8.GetString(Convert.FromBase64String(base64));
-		var model = JsonSerializer.Deserialize<Sample>(json);
+		// JSONをモデルにデシリアライズ
+		var model = JsonSerializer.Deserialize<Sample>(json, _jsonSerializerOptions);
 		if (model is null) {
 			bindingContext.Result = ModelBindingResult.Failed();
 			return Task.CompletedTask;
