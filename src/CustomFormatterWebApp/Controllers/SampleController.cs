04f5a698225acba53ed0515e76f79d6ba8fb767b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-04-12 08:08:41 +0900

    Add controllers

---
+++ src/CustomFormatterWebApp/Controllers/SampleController.cs
@@ -0,0 +1,13 @@
+using CustomFormatterWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomFormatterWebApp.Controllers;
+
+[Route("api/[controller]")]
+public class SampleController : ControllerBase {
+	[HttpPost]
+	public async Task<Sample> PostAsync(Sample sample) {
+		await Task.CompletedTask;
+		return sample;
+	}
+}
---
+++ src/CustomFormatterWebApp/Models/Sample.cs
@@ -0,0 +1,6 @@
+namespace CustomFormatterWebApp.Models;
+
+public class Sample {
+	public int Id { get; init; }
+	public string Name { get; init; } = "";
+}
