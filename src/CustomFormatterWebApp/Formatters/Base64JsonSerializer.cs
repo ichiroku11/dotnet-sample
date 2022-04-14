007eea6b34acee881eaadf43de979c1f093becaa
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-04-15 08:02:47 +0900

    Add Base64JsonSerializer

---
+++ src/CustomFormatterWebApp/Formatters/Base64JsonSerializer.cs
@@ -0,0 +1,13 @@
+namespace CustomFormatterWebApp.Formatters;
+
+public static class Base64JsonSerializer {
+	public static TValue? Deserialize<TValue>(string base64json) {
+		// todo:
+		throw new NotImplementedException();
+	}
+
+	public static string Serialize<TValue>(TValue value) {
+		// todo:
+		throw new NotImplementedException();
+	}
+}
