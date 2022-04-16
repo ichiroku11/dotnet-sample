d40bafc3476372c8e3f671aa9cc98a4345c0055b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-04-16 09:06:44 +0900

    Update Base64JsonSerializer

--- src/CustomFormatterWebApp/Formatters/Base64JsonSerializer.cs
+++ src/CustomFormatterWebApp/Formatters/Base64JsonSerializer.cs
@@ -1,13 +1,21 @@
+using System.Text;
+using System.Text.Json;
+
 namespace CustomFormatterWebApp.Formatters;
 
 public static class Base64JsonSerializer {
+	private static readonly JsonSerializerOptions _options
+		= new () {
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		};
+
 	public static TValue? Deserialize<TValue>(string base64json) {
-		// todo:
-		throw new NotImplementedException();
+		var json = Encoding.UTF8.GetString(Convert.FromBase64String(base64json));
+		return JsonSerializer.Deserialize<TValue>(json, _options);
 	}
 
 	public static string Serialize<TValue>(TValue value) {
-		// todo:
-		throw new NotImplementedException();
+		var json = JsonSerializer.Serialize(value, _options);
+		return Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
 	}
 }
