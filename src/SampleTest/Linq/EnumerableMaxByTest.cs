dbd0d29e26cddc2fb0c603b4fc2a506d9a0c1a9a
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-04-25 07:26:15 +0900

    Use TheoryData

--- src/SampleTest/Linq/EnumerableMaxByTest.cs
+++ src/SampleTest/Linq/EnumerableMaxByTest.cs
@@ -10,41 +10,41 @@ namespace SampleTest.Linq;
 public class EnumerableMaxByTest {
 	public record Sample(string Name, int Value);
 
-	public static IEnumerable<object?[]> GetTestDataForMaxBy() {
-		// 最大値を持つ要素をを取得できる
-		yield return new object[] {
-			// source
-			new[] {
-				new Sample("a", 1),
-				new Sample("b", 2),
+	public static TheoryData<IEnumerable<Sample>, Sample?> GetTheoryDataForMaxBy() {
+		return new() {
+			// 最大値を持つ要素をを取得できる
+			{
+				// source
+				new[] {
+					new Sample("a", 1),
+					new Sample("b", 2),
+				},
+				// expected
+				new Sample("b", 2)
 			},
-			// expected
-			new Sample("b", 2),
-		};
-
-		// 最大値を持つ要素が複数存在するる場合は最初の要素が見つかる
-		yield return new object[] {
-			// source
-			new[] {
-				new Sample("a", 1),
-				new Sample("b", 2),
-				new Sample("c", 2),
+			// 最大値を持つ要素が複数存在するる場合は最初の要素が見つかる
+			{
+				// source
+				new[] {
+					new Sample("a", 1),
+					new Sample("b", 2),
+					new Sample("c", 2),
+				},
+				// expected
+				new Sample("b", 2)
 			},
-			// expected
-			new Sample("b", 2),
-		};
-
-		// 空のコレクションに対しての結果はnullになる
-		yield return new object?[] {
-			// source
-			Enumerable.Empty<Sample>(),
-			// expected
-			default(Sample),
+			// 空のコレクションに対しての結果はnullになる
+			{
+				// source
+				Enumerable.Empty<Sample>(),
+				// expected
+				default
+			},
 		};
 	}
 
 	[Theory]
-	[MemberData(nameof(GetTestDataForMaxBy))]
+	[MemberData(nameof(GetTheoryDataForMaxBy))]
 	public void MaxBy_最大値を取得する(IEnumerable<Sample> source, Sample? expected) {
 		// Arrange
 		// Act
--- src/SampleTest/Linq/EnumerableZipTest.cs
+++ src/SampleTest/Linq/EnumerableZipTest.cs
@@ -3,33 +3,34 @@ using Xunit;
 namespace SampleTest.Linq;
 
 public class EnumerableZipTest {
-	public static IEnumerable<object[]> GetTestDataForZip() {
-		yield return new object[] {
+	public static TheoryData<IEnumerable<int>, IEnumerable<int>, IEnumerable<(int, int)>> GetTheoryDataForZip() {
+		return new() {
+			{
 				// first
 				new[] { 1, 2, 3 },
 				// second
 				new[] { 6, 5, 4 },
 				// expected
-				new [] { (1, 6), (2, 5), (3, 4) },
-			};
-
-		// 2つのシーケンスの要素数が異なる場合は、少ない方の要素数で列挙される
-		// first < second
-		yield return new object[] {
+				new[] { (1, 6), (2, 5), (3, 4) }
+			},
+			// 2つのシーケンスの要素数が異なる場合は、少ない方の要素数で列挙される
+			// first < second
+			{
 				new[] { 1, 2, 3 },
 				new[] { 6, 5, 4, -1 },
-				new [] { (1, 6), (2, 5), (3, 4) },
-			};
-		// first > second
-		yield return new object[] {
+				new[] { (1, 6), (2, 5), (3, 4) }
+			},
+			// first > second
+			{
 				new[] { 1, 2, 3, -1 },
 				new[] { 6, 5, 4 },
-				new [] { (1, 6), (2, 5), (3, 4) },
-			};
+				new[] { (1, 6), (2, 5), (3, 4) }
+			},
+		};
 	}
 
 	[Theory]
-	[MemberData(nameof(GetTestDataForZip))]
+	[MemberData(nameof(GetTheoryDataForZip))]
 	public void Zip_2つのシーケンスからタプルを生成する(IEnumerable<int> first, IEnumerable<int> second, IEnumerable<(int, int)> expected) {
 		// Arrange
 		// Act
