462eff88fd3882a4755c241bf256177c673516c6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-05-08 08:40:19 +0900

    Use OnceHostedService

--- src/AzureAdB2cUserManagementConsoleApp/SampleService.cs
+++ src/AzureAdB2cUserManagementConsoleApp/SampleService.cs
@@ -1,49 +1,24 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SampleLib.Hosting;
 
 namespace AzureAdB2cUserManagementConsoleApp;
 
-public class SampleService : IHostedService {
-	private readonly IServiceProvider _services;
-	private readonly IHostApplicationLifetime _lifetime;
-	private readonly ILogger _logger;
-
-	public SampleService(
-		IHost host,
-		IHostApplicationLifetime lifetime,
-		ILogger<SampleService> logger) {
-		_services = host.Services;
-		_lifetime = lifetime;
-		_logger = logger;
+public class SampleService : OnceHostedService {
+	public SampleService(IHost host, IHostApplicationLifetime lifetime, ILogger<SampleService> logger)
+		: base(host, lifetime, logger) {
 	}
 
-	public Task StartAsync(CancellationToken cancellationToken) {
-		_logger.LogInformation(nameof(StartAsync));
-
-		_lifetime.ApplicationStarted.Register(async () => {
-			try {
-				await _services.GetRequiredService<GraphGetUserListSample>().RunAsync();
-				// ユーザー作成
-				//await _services.GetRequiredService<GraphCreateUserSample>().RunAsync();
-				// ユーザー更新（アカウントの有効・無効）
-				//await _services.GetRequiredService<GraphUpdateUserAccountEnabledSample>().RunAsync();
-				// ユーザー更新（カスタム属性）
-				//await _services.GetRequiredService<GraphUpdateUserCustomAttributeSample>().RunAsync();
-				// ユーザー更新（パスワードリセット）
-				//await _services.GetRequiredService<GraphUpdateUserForceChangePasswordSample>().RunAsync();
-			} catch (Exception exception) {
-				_logger.LogError(exception, nameof(IHostApplicationLifetime.ApplicationStarted));
-			} finally {
-				_lifetime.StopApplication();
-			}
-		});
-
-		return Task.CompletedTask;
-	}
-
-	public Task StopAsync(CancellationToken cancellationToken) {
-		_logger.LogInformation(nameof(StopAsync));
-		return Task.CompletedTask;
+	protected override Task RunAsync(IServiceProvider services) {
+		return services.GetRequiredService<GraphGetUserListSample>().RunAsync();
+		// ユーザー作成
+		//return services.GetRequiredService<GraphCreateUserSample>().RunAsync();
+		// ユーザー更新（アカウントの有効・無効）
+		//return services.GetRequiredService<GraphUpdateUserAccountEnabledSample>().RunAsync();
+		// ユーザー更新（カスタム属性）
+		//return services.GetRequiredService<GraphUpdateUserCustomAttributeSample>().RunAsync();
+		// ユーザー更新（パスワードリセット）
+		//return services.GetRequiredService<GraphUpdateUserForceChangePasswordSample>().RunAsync();
 	}
 }
