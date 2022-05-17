29f47967e24bbb0165137647098bf2725fd5f8f1
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-05-17 10:34:17 +0900

    Use configuration

--- src/AzureAdB2cWebApi/Program.cs
+++ src/AzureAdB2cWebApi/Program.cs
@@ -1,6 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
+
+var config = builder.Configuration;
+var clientId = config["AzureAdB2C:ClientId"];
+Console.WriteLine(clientId);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+
+
+// https://docs.microsoft.com/ja-jp/azure/active-directory-b2c/enable-authentication-web-api?tabs=csharpclient
+// https://github.com/Azure-Samples/active-directory-aspnetcore-webapp-openidconnect-v2
+
