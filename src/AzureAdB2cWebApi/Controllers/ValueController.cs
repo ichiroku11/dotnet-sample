45ef963df0c64285789c1204f9af950256f03b1b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-05-22 14:10:22 +0900

    Add ValueController

---
+++ src/AzureAdB2cWebApi/Controllers/ValueController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureAdB2cWebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class ValueController : ControllerBase {
+	[HttpGet]
+	public IEnumerable<string> Get() => new[] { "a", "b", "c" };
+}
