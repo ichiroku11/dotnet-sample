de4edc34bac4a86587c6a0ae0cb4904caa77b78b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-05-24 07:53:43 +0900

    Test Enumerable.GroupJoin

--- src/SampleTest/Linq/EnumerableGroupJoinTest.cs
+++ src/SampleTest/Linq/EnumerableGroupJoinTest.cs
@@ -38,4 +38,50 @@ public class EnumerableGroupJoinTest {
 				Assert.Equal("X", result.Inners.First().Value);
 			});
 	}
+
+	[Fact]
+	public void GroupJoin_欠番を埋めるような動きを確認する() {
+		// Arrange
+		// Id=1~5までのイメージ
+		var outers = Enumerable.Range(1, 5);
+		// Id=2と5が欠番
+		var inners = new[] {
+			new { Id = 1, Value = 11 },
+			new { Id = 3, Value = 13 },
+			new { Id = 4, Value = 14 },
+		};
+
+		// Act
+		var results = outers
+			.GroupJoin(
+				inners,
+				outer => outer,
+				inner => inner.Id,
+				(outer, inners) => inners.SingleOrDefault() ?? new { Id = outer, Value = 0 })
+			.OrderBy(entry => entry.Id);
+
+		// Assert
+		Assert.Equal(5, results.Count());
+		Assert.Collection(results,
+			result => {
+				Assert.Equal(1, result.Id);
+				Assert.Equal(11, result.Value);
+			},
+			result => {
+				Assert.Equal(2, result.Id);
+				Assert.Equal(0, result.Value);
+			},
+			result => {
+				Assert.Equal(3, result.Id);
+				Assert.Equal(13, result.Value);
+			},
+			result => {
+				Assert.Equal(4, result.Id);
+				Assert.Equal(14, result.Value);
+			},
+			result => {
+				Assert.Equal(5, result.Id);
+				Assert.Equal(0, result.Value);
+			});
+	}
 }
