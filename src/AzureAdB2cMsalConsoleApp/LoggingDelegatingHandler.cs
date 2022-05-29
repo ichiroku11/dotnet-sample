eebda0a7966252d89cbd79803712032184bdca29
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-05-29 10:29:27 +0900

    Update LoggingDelegatingHandler

--- src/AzureAdB2cMsalConsoleApp/LoggingDelegatingHandler.cs
+++ src/AzureAdB2cMsalConsoleApp/LoggingDelegatingHandler.cs
@@ -1,13 +1,26 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace AzureAdB2cMsalConsoleApp;
 
 public class LoggingDelegatingHandler : DelegatingHandler {
-	protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
-		return base.SendAsync(request, cancellationToken);
+	private readonly ILogger<LoggingDelegatingHandler> _logger;
+
+	public LoggingDelegatingHandler(ILogger<LoggingDelegatingHandler> logger) {
+		_logger = logger;
+	}
+
+	protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+		_logger.LogInformation(request.ToString());
+		if (request.Content != null) {
+			_logger.LogInformation(await request.Content.ReadAsStringAsync());
+		}
+
+		var response = await base.SendAsync(request, cancellationToken);
+
+		_logger.LogInformation(response.ToString());
+		if (response.Content != null) {
+			_logger.LogInformation(await response.Content.ReadAsStringAsync());
+		}
+		return response;
 	}
 }
