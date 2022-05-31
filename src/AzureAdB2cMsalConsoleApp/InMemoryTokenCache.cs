f981a6fe7e93af669b8219d65231d410b1c99768
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-06-01 07:52:03 +0900

    Update InMemoryTokenCache

--- src/AzureAdB2cMsalConsoleApp/InMemoryTokenCache.cs
+++ src/AzureAdB2cMsalConsoleApp/InMemoryTokenCache.cs
@@ -1,31 +1,60 @@
+using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AzureAdB2cMsalConsoleApp;
 
 public class InMemoryTokenCache {
+	private readonly ILogger _logger;
+	private byte[]? _cache;
+
+	public InMemoryTokenCache(ILogger<InMemoryTokenCache> logger) {
+		_logger = logger;
+	}
+
+	private Task OnBeforeAccessAsync(TokenCacheNotificationArgs args) {
+		_logger.LogInformation($"{nameof(OnBeforeAccessAsync)}");
+
+		// todo:
+		if (_cache is null) {
+			return Task.CompletedTask;
+		}
+
+		// トークンキャッシュをデシリアライズする
+		args.TokenCache.DeserializeMsalV3(_cache);
+
+		return Task.CompletedTask;
+	}
+
+	private Task OnBeforeWriteAsync(TokenCacheNotificationArgs arsg) {
+		_logger.LogInformation($"{nameof(OnBeforeWriteAsync)}");
+
+		// todo:
+		return Task.CompletedTask;
+	}
+
+	private Task OnAfterAccessAsync(TokenCacheNotificationArgs args) {
+		_logger.LogInformation($"{nameof(OnAfterAccessAsync)}");
+
+		// todo:
+		if (!args.HasStateChanged) {
+			return Task.CompletedTask;
+		}
+
+		// トークンキャッシュをシリアライズする
+		_cache = args.TokenCache.SerializeMsalV3();
+
+		return Task.CompletedTask;
+	}
+
+	// コールバックイベントをバインドする
 	public void Bind(ITokenCache tokenCache) {
+		// キャッシュにアクセスする前に呼び出される
 		tokenCache.SetBeforeAccessAsync(OnBeforeAccessAsync);
+
+		// キャッシュに書き込む前に呼び出される
 		tokenCache.SetBeforeWriteAsync(OnBeforeWriteAsync);
+
+		// キャッシュにアクセスした後に呼び出される
 		tokenCache.SetAfterAccessAsync(OnAfterAccessAsync);
-	}
-
-	private Task OnBeforeAccessAsync(TokenCacheNotificationArgs arg) {
-		// todo:
-		return Task.CompletedTask;
-	}
-
-	private Task OnBeforeWriteAsync(TokenCacheNotificationArgs arg) {
-		// todo:
-		return Task.CompletedTask;
-	}
-
-	private Task OnAfterAccessAsync(TokenCacheNotificationArgs arg) {
-		// todo:
-		return Task.CompletedTask;
 	}
 }
