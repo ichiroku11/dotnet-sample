cc338e187bd1499dea0c3a4c3715db067767c4a4
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-06-11 11:54:21 +0900

    Test DateTime.Kind

---
+++ src/SampleTest/DateTimeTest.cs
@@ -0,0 +1,28 @@
+namespace SampleTest;
+
+public class DateTimeTest {
+	public static TheoryData<DateTime, DateTimeKind> GetTheoryDataForKind() {
+		return new() {
+			// 現地時刻
+			{ DateTime.Today, DateTimeKind.Local },
+			{ DateTime.Now, DateTimeKind.Local },
+
+			// 未指定
+			{ new DateTime(1970, 1, 1), DateTimeKind.Unspecified },
+			{ DateTime.Parse("1970/01/01"), DateTimeKind.Unspecified },
+			{ DateTime.MinValue, DateTimeKind.Unspecified },
+			{ DateTime.MaxValue, DateTimeKind.Unspecified },
+
+			// UTC（世界協定時刻）
+			{ DateTime.UnixEpoch, DateTimeKind.Utc }
+		};
+	}
+
+	[Theory, MemberData(nameof(GetTheoryDataForKind))]
+	public void Kind_インスタンスの作り方によって値が変わることを確認する(DateTime target, DateTimeKind expected) {
+		// Arrange
+		// Act
+		// Assert
+		Assert.Equal(expected, target.Kind);
+	}
+}
