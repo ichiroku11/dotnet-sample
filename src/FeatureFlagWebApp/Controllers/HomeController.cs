76a69ae8f51042c176ee651df8bed5cfa1ea89f3
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-07-03 10:12:23 +0900

    Use FeatureManager

--- src/FeatureFlagWebApp/Controllers/HomeController.cs
+++ src/FeatureFlagWebApp/Controllers/HomeController.cs
@@ -1,8 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.FeatureManagement;
 
 namespace FeatureFlagWebApp.Controllers;
 public class HomeController : Controller {
-	public IActionResult Index() {
-		return Content("Hello World!");
+	private readonly IFeatureManager _featureManager;
+
+	public HomeController(IFeatureManager featureManager) {
+		_featureManager = featureManager;
 	}
+
+	public async Task<IActionResult> IndexAsync()
+		=> Json(new {
+			featureA = await _featureManager.IsEnabledAsync(FeatureFlags.FeatureA),
+			featureB = await _featureManager.IsEnabledAsync(FeatureFlags.FeatureB),
+			featureC = await _featureManager.IsEnabledAsync(FeatureFlags.FeatureC),
+		});
 }
