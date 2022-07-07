2b31e755462dff5ef88b1a7947536d2b03a80533
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-07-08 08:20:08 +0900

    Use RequirementType

--- src/FeatureFlagWebApp/Controllers/HomeController.cs
+++ src/FeatureFlagWebApp/Controllers/HomeController.cs
@@ -24,12 +24,25 @@ public class HomeController : Controller {
 
 	// 機能フラグによるフィルター
 	// 機能フラグが無効の場合、既定では404
+	[Route("[controller]/feature/a")]
 	[FeatureGate(FeatureFlags.FeatureA)]
 	public IActionResult FeatureA() => Content(nameof(FeatureA));
 
+	[Route("[controller]/feature/b")]
 	[FeatureGate(FeatureFlags.FeatureB)]
 	public IActionResult FeatureB() => Content(nameof(FeatureB));
 
+	[Route("[controller]/feature/c")]
 	[FeatureGate(FeatureFlags.FeatureC)]
 	public IActionResult FeatureC() => Content(nameof(FeatureC));
+
+	// AとBどちらも設定されている必要がある
+	[Route("[controller]/feature/andb")]
+	[FeatureGate(RequirementType.All, FeatureFlags.FeatureA, FeatureFlags.FeatureB)]
+	public IActionResult FeatureAAndB() => Content($"{nameof(FeatureA)} and {nameof(FeatureB)}");
+
+	// AとBどちらかが設定されていればOK
+	[Route("[controller]/feature/aorb")]
+	[FeatureGate(RequirementType.Any, FeatureFlags.FeatureA, FeatureFlags.FeatureB)]
+	public IActionResult FeatureAOrB() => Content($"{nameof(FeatureA)} or {nameof(FeatureB)}");
 }
