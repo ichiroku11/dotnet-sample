3c6199ba2a1e310210d3b02cc3deff653329db9a
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-07-15 07:40:58 +0900

    Move method

--- src/SampleTest/Text.Json/JsonSerializerOptionsTest.cs
+++ src/SampleTest/Text.Json/JsonSerializerOptionsTest.cs
@@ -21,4 +21,92 @@ public class JsonSerializerOptionsTest {
 			};
 		});
 	}
+
+	[Fact]
+	public void DictionaryKeyPolicy_違いを確認する() {
+		var model = new {
+			Number = 1,
+			Items = new Dictionary<string, int> {
+				{ "Key1", 10 },
+			},
+		};
+
+		// DictionaryKeyPolicyを指定しないとディクショナリオブジェクトのプロパティ名が大文字になる
+		{
+			var options = new JsonSerializerOptions {
+				//DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				WriteIndented = true
+			};
+
+			var actual = JsonSerializer.Serialize(model, options);
+			_output.WriteLine(actual);
+
+			var expected = @"{
+  ""number"": 1,
+  ""items"": {
+    ""Key1"": 10
+  }
+}";
+			Assert.Equal(expected, actual);
+		}
+
+		// DictionaryKeyPolicyを指定してディクショナリオブジェクトのプロパティ名を小文字にする
+		{
+			var options = new JsonSerializerOptions {
+				DictionaryKeyPolicy = JsonNamingPolicy.CamelCase,
+				PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+				WriteIndented = true
+			};
+
+			var actual = JsonSerializer.Serialize(model, options);
+			_output.WriteLine(actual);
+
+			var expected = @"{
+  ""number"": 1,
+  ""items"": {
+    ""key1"": 10
+  }
+}";
+			Assert.Equal(expected, actual);
+		}
+	}
+
+	[Fact]
+	public void PropertyNamingPolicy_を使ってプロパティ名をキャメルケースでシリアライズする() {
+		// Arrange
+		var model = new { Number = 1, Text = "Abc" };
+		var options = new JsonSerializerOptions {
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+		};
+
+		// Act
+		var actual = JsonSerializer.Serialize(model, options);
+		_output.WriteLine(actual);
+
+		// Assert
+		var expected = @"{""number"":1,""text"":""Abc""}";
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void WriteIndented_整形してシリアライズする() {
+		// Arrange
+		var model = new { Number = 1, Text = "Abc" };
+		var options = new JsonSerializerOptions {
+			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+			WriteIndented = true
+		};
+
+		// Act
+		var actual = JsonSerializer.Serialize(model, options);
+		_output.WriteLine(actual);
+
+		// Assert
+		var expected = @"{
+  ""number"": 1,
+  ""text"": ""Abc""
+}";
+		Assert.Equal(expected, actual);
+	}
 }
