0898f80a711b191398c3acfe0e59f2dd21adecc0
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-07-19 08:01:53 +0900

    Test JsonWebKey

--- src/SampleTest/IdentityModel.Tokens/JsonWebKeyTest.cs
+++ src/SampleTest/IdentityModel.Tokens/JsonWebKeyTest.cs
@@ -1,11 +1,20 @@
 using Microsoft.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SampleTest.IdentityModel.Tokens;
 
 public class JsonWebKeyTest {
 	public static TheoryData<JsonWebKey> GetTheoryDataForKeyIdIsNull() {
+		var key1 = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("0123456789abcdef"));
+
+		using var rsa = RSA.Create();
+		var key2 = new RsaSecurityKey(rsa.ExportParameters(false));
+
 		return new() {
 			{ new JsonWebKey() },
+			{ JsonWebKeyConverter.ConvertFromSymmetricSecurityKey(key1) },
+			{ JsonWebKeyConverter.ConvertFromRSASecurityKey(key2) },
 		};
 	}
 
