caa5dfc7671640992fe6d94570a6e7e36ffc0cf9
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-07-23 12:06:21 +0900

    Test EpochTime.DateTime

---
+++ src/SampleTest/IdentityModel.Tokens/EpochTimeTest.cs
@@ -0,0 +1,27 @@
+using Microsoft.IdentityModel.Tokens;
+
+namespace SampleTest.IdentityModel.Tokens;
+
+public class EpochTimeTest {
+	public static TheoryData<long, DateTime> GetTheoryDataForDateTime() {
+		return new()
+		{
+			{ 0L, EpochTime.UnixEpoch },
+			{ 1L, EpochTime.UnixEpoch.AddSeconds(1) },
+			// 負の値を指定した場合、Unix EpochTimeになる
+			{ -1L, EpochTime.UnixEpoch },
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryDataForDateTime))]
+	public void DateTime_秒数をDateTimeに変換する(long second, DateTime expected) {
+		// Arrange
+		// Act
+		var actual = EpochTime.DateTime(second);
+
+		// Assert
+		Assert.Equal(expected, actual);
+		Assert.Equal(DateTimeKind.Utc, actual.Kind);
+	}
+}
