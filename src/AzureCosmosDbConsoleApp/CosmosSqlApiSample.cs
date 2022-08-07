a0a6f7eb2783270bd12b530c291264d03cab1248
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-08-07 09:22:58 +0900

    Use UserSecrets

--- src/AzureCosmosDbConsoleApp/CosmosSqlApiSample.cs
+++ src/AzureCosmosDbConsoleApp/CosmosSqlApiSample.cs
@@ -1,7 +1,18 @@
+using Microsoft.Extensions.Configuration;
+
 namespace AzureCosmosDbConsoleApp;
 
 public class CosmosSqlApiSample {
+
+	private readonly string _connectionString;
+
+	public CosmosSqlApiSample(IConfiguration config) {
+		_connectionString = config.GetConnectionString("Cosmos");
+	}
+
 	public Task RunAsync() {
+		Console.WriteLine(_connectionString);
+
 		return Task.CompletedTask;
 	}
 }
