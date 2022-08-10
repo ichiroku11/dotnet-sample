34e759bb5b56784753bebe9c305b32a2868ebea1
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-08-10 09:10:36 +0900

    Container creation and deletion

--- src/AzureCosmosDbConsoleApp/CosmosSqlApiSample.cs
+++ src/AzureCosmosDbConsoleApp/CosmosSqlApiSample.cs
@@ -8,9 +8,12 @@ namespace AzureCosmosDbConsoleApp;
 // 参考
 // https://github.com/Azure/azure-cosmos-dotnet-v3/blob/master/Microsoft.Azure.Cosmos.Samples/Usage/ContainerManagement/Program.cs
 public class CosmosSqlApiSample {
-
 	private readonly string _connectionString;
 	private readonly ILogger _logger;
+
+	private const string _databaseId = "Test";
+	private const string _containerId = "Order";
+	private const string _partitionKeyPath = "/id";
 
 	public CosmosSqlApiSample(IConfiguration config, ILogger<CosmosSqlApiSample> logger) {
 		_connectionString = config.GetConnectionString("Cosmos");
@@ -31,15 +34,27 @@ public class CosmosSqlApiSample {
 		_logger.LogInformation(containerIds.ToString());
 	}
 
+	private Task CreateContainerAsync(Database database)
+		=> database.CreateContainerAsync(
+			id: _containerId,
+			partitionKeyPath: _partitionKeyPath);
+
+	private Task DeleteContainerAsync(Database database)
+		=> database.GetContainer(_containerId).DeleteContainerAsync();
+
 	public async Task RunAsync() {
 		_logger.LogInformation(_connectionString);
 
 		using var client = new CosmosClient(_connectionString);
 
 		// databaseはDatabaseResponse型
-		var database = await client.CreateDatabaseIfNotExistsAsync("Test");
+		var database = await client.CreateDatabaseIfNotExistsAsync(_databaseId);
 
 		// DatabaseResponse型はDatabase型に暗黙的にキャストできる
 		await ListContainerAsync(database);
+
+		// コンテナーの削除と作成
+		await DeleteContainerAsync(database);
+		await CreateContainerAsync(database);
 	}
 }
