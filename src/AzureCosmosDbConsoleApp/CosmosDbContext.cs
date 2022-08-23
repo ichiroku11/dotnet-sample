eb8a6a1d05e8ce1797fa72cb580c72a519f2b696
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-08-24 08:26:01 +0900

    Use LoggerFactory

--- src/AzureCosmosDbConsoleApp/CosmosDbContext.cs
+++ src/AzureCosmosDbConsoleApp/CosmosDbContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace AzureCosmosDbConsoleApp;
 
@@ -7,19 +8,28 @@ namespace AzureCosmosDbConsoleApp;
 // https://docs.microsoft.com/ja-jp/ef/core/providers/cosmos/?tabs=dotnet-core-cli
 // https://github.com/dotnet/EntityFramework.Docs/blob/main/samples/core/Cosmos/ModelBuilding/OrderContext.cs
 public class CosmosDbContext : DbContext {
+	private static readonly ILoggerFactory _loggerFactory = LoggerFactory.Create(builder => {
+		builder
+			.AddFilter((category, level) => category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information)
+			.AddConsole()
+			.AddDebug();
+	});
+
 	private readonly string _connectionString;
 
 	public CosmosDbContext(IConfiguration config) {
 		_connectionString = config.GetConnectionString(Constants.ConnectionStringName);
 	}
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-		=> optionsBuilder.UseCosmos(
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+		optionsBuilder.UseLoggerFactory(_loggerFactory);
+
+		optionsBuilder.UseCosmos(
 			connectionString: _connectionString,
 			databaseName: Constants.TestDatabase.Id);
+	}
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder) {
-		// todo:
 		modelBuilder.HasDefaultContainer(Constants.OrderContainer.Id);
 
 	}
