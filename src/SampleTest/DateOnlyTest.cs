32c988b18592b83d9148a2c5a1b17729253c09bd
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-10-16 08:22:44 +0900

    Test DateOnly.ToDateTime

--- src/SampleTest/DateOnlyTest.cs
+++ src/SampleTest/DateOnlyTest.cs
@@ -3,7 +3,7 @@ namespace SampleTest;
 public class DateOnlyTest {
 
 	[Fact]
-	public void FromDateTime_DateTimeから生成する() {
+	public void FromDateTime_DateTimeから変換する() {
 		// Arrange
 		var now = DateTime.Now;
 
@@ -14,5 +14,17 @@ public class DateOnlyTest {
 		Assert.Equal(now.Year, date.Year);
 		Assert.Equal(now.Month, date.Month);
 		Assert.Equal(now.Day, date.Day);
+	}
+
+	[Fact]
+	public void ToDateTime_DateTimeに変換する() {
+		// Arrange
+		var expected = DateTime.Today;
+
+		// Act
+		var actual = DateOnly.FromDateTime(expected).ToDateTime(TimeOnly.MinValue);
+
+		// Assert
+		Assert.Equal(expected, actual);
 	}
 }
