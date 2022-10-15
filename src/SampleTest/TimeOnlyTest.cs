b565ede9c012f8adf3ba0802fafc92967589b1f6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-10-16 08:25:49 +0900

    Test TimeOnly.IsBetween

--- src/SampleTest/TimeOnlyTest.cs
+++ src/SampleTest/TimeOnlyTest.cs
@@ -26,4 +26,24 @@ public class TimeOnlyTest {
 		Assert.Equal(59, time.Second);
 		Assert.Equal(999, time.Millisecond);
 	}
+
+
+	public static TheoryData<TimeOnly, TimeOnly, TimeOnly, bool> GetTheoryDataForIsBetween() {
+		return new TheoryData<TimeOnly, TimeOnly, TimeOnly, bool> {
+			// 10:00は、09:59～10:01の範囲内
+			{ new TimeOnly(10, 0), new TimeOnly(9, 59), new TimeOnly(10, 1), true },
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryDataForIsBetween))]
+	public void IsBetween_動きを確認する(TimeOnly target, TimeOnly start, TimeOnly end, bool expected) {
+		// Arrange
+
+		// Act
+		var actual = target.IsBetween(start, end);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
 }
