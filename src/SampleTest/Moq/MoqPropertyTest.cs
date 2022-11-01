0b52c7209c3f18fe5db532cfdb8f0cab2738bc24
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-11-02 08:13:02 +0900

    Test property

--- src/SampleTest/Moq/MoqPropertyTest.cs
+++ src/SampleTest/Moq/MoqPropertyTest.cs
@@ -78,4 +78,24 @@ public class MoqPropertyTest {
 			value => Assert.Equal(1, value),
 			value => Assert.Equal(2, value));
 	}
+
+	[Fact]
+	public void SetupPropertyメソッドを使って設定して取得できるプロパティをテストする() {
+		// Arrange
+		var mock = new Mock<ITestTarget>();
+
+		// 設定・取得できるプロパティ
+		mock.SetupProperty(target => target.Value, 1);
+
+		var target = mock.Object;
+
+		// Act
+		// Assert
+		// 初期値を取得できる
+		Assert.Equal(1, target.Value);
+
+		// 変更した値を取得できる
+		target.Value = 2;
+		Assert.Equal(2, target.Value);
+	}
 }
