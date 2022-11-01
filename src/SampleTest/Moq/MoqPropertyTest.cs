5b352ea53ddfc911bbdcb47cd2ad1508944be36d
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-11-02 08:10:25 +0900

    Test property

--- src/SampleTest/Moq/MoqPropertyTest.cs
+++ src/SampleTest/Moq/MoqPropertyTest.cs
@@ -39,7 +39,7 @@ public class MoqPropertyTest {
 	}
 
 	[Fact]
-	public void 固定値を返すプロパティに値をセットしても変更されない() {
+	public void 固定値を返すプロパティに値を設定しても変更されない() {
 		// Arrange
 		var mock = new Mock<ITestTarget>();
 		mock.Setup(target => target.Value).Returns(1);
@@ -52,5 +52,30 @@ public class MoqPropertyTest {
 		// Assert
 		// プロパティの値を変更しても、取得できるのはセットアップで設定した値
 		Assert.Equal(1, target.Value);
+	}
+
+	[Fact]
+	public void コレクションのプロパティをテストする() {
+		// Arrange
+		var mock = new Mock<ITestTarget>();
+
+		var expected = new List<int> { 1 };
+		mock.Setup(target => target.Values).Returns(expected);
+
+		var target = mock.Object;
+
+		// Act
+		// Assert
+		// Returnsで指定したインスタンスを取得できる
+		Assert.Same(expected, target.Values);
+
+		// Returnsで指定したインスタンスに含まれている値を取得できる
+		Assert.Single(target.Values, 1);
+
+		// コレクションに値を追加できる
+		target.Values.Add(2);
+		Assert.Collection(target.Values,
+			value => Assert.Equal(1, value),
+			value => Assert.Equal(2, value));
 	}
 }
