1267250b3b98fef9e572263065c5061fbb0fa9e6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-11-05 07:53:29 +0900

    Test method

--- src/SampleTest/Moq/MockMethodTest.cs
+++ src/SampleTest/Moq/MockMethodTest.cs
@@ -8,6 +8,7 @@ public class MockMethodTest {
 	// テスト対象
 	public interface ITestTarget {
 		int GetValue();
+		int GetValue(int value);
 
 		Task<int> GetValueAsync();
 	}
@@ -27,6 +28,39 @@ public class MockMethodTest {
 
 		// Assert
 		Assert.Equal(1, actual);
+	}
+
+	[Fact]
+	public void 引数があり固定値を返すメソッドをテストする() {
+		// Arrange
+		var mock = new Mock<ITestTarget>();
+
+		// メソッドはint型の引数を受け取り固定値を返す
+		mock.Setup(target => target.GetValue(It.IsAny<int>())).Returns(1);
+
+		var target = mock.Object;
+
+		// Act
+		var actual = target.GetValue(0);
+
+		// Assert
+		Assert.Equal(1, actual);
+	}
+
+	[Fact]
+	public void 引数の値によって戻り値が変わるメソッドをテストする() {
+		// Arrange
+		var mock = new Mock<ITestTarget>();
+
+		mock.Setup(target => target.GetValue(It.Is<int>(value => value % 2 == 0))).Returns(0);
+		mock.Setup(target => target.GetValue(It.Is<int>(value => value % 2 == 1))).Returns(1);
+
+		var target = mock.Object;
+
+		// Act
+		// Assert
+		Assert.Equal(0, target.GetValue(0));
+		Assert.Equal(1, target.GetValue(1));
 	}
 
 	[Fact]
