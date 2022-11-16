cacb0b31fc08394644201f65b0501385dec4f3b9
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-11-17 08:31:48 +0900

    Add LinkGeneratorWebApp.Test project

---
+++ src/LinkGeneratorWebApp.Test/Controllers.Test/DefaultControllerTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace LinkGeneratorWebApp.Test.Controllers.Test;
+
+public class DefaultControllerTest {
+	[Fact]
+	public void Index() {
+		// Arrange
+		// Act
+		// Assert
+		Assert.True(true);
+	}
+}
