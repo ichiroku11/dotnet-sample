a934d235b5bfe5b83f8088f9b739fddb678e6ce7
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-11-22 08:05:07 +0900

    Refactor

--- src/LinkGeneratorWebApp/Controllers/PathController.cs
+++ src/LinkGeneratorWebApp/Controllers/PathController.cs
@@ -18,20 +18,27 @@ public class PathController : Controller {
 	public IActionResult OtherAction() {
 		// このアクションとは別のアクションへのURLを生成する
 		// controllerは指定していないのでHttpContextから補完される様子
-		var path = _linkGenerator.GetPathByAction(HttpContext, action: "Other");
+		var path = _linkGenerator.GetPathByAction(
+			HttpContext,
+			action: "Other");
 		return Content(path ?? "");
 	}
 
 	public IActionResult OtherController() {
 		// このアクションとは別のコントローラーへのURLを生成する
 		// actionは指定していないのでHttpContextから補完される様子
-		var path = _linkGenerator.GetPathByAction(HttpContext, controller: "Other");
+		var path = _linkGenerator.GetPathByAction(
+			HttpContext,
+			controller: "Other");
 		return Content(path ?? "");
 	}
 
 	public IActionResult OtherControllerAction() {
 		// このアクションとは別のコントローラーへのURLを生成する
-		var path = _linkGenerator.GetPathByAction(HttpContext, action: "Index", controller: "Other");
+		var path = _linkGenerator.GetPathByAction(
+			HttpContext,
+			action: "Index",
+			controller: "Other");
 		return Content(path ?? "");
 	}
 }
