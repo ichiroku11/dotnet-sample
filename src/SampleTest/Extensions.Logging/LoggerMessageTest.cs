95c39ab31ceb217fbffc2bf4598e61a9ccd7c511
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-12-18 09:29:56 +0900

    Refactor

--- src/SampleTest/Extensions.Logging/LoggerMessageTest.cs
+++ src/SampleTest/Extensions.Logging/LoggerMessageTest.cs
@@ -4,22 +4,21 @@ namespace SampleTest.Extensions.Logging;
 
 // https://learn.microsoft.com/ja-jp/aspnet/core/fundamentals/logging/loggermessage
 public class LoggerMessageTest {
-	private static readonly Action<ILogger, Exception?> _log
+	private static readonly Action<ILogger, Exception?> _log1
 		= LoggerMessage.Define(
 			LogLevel.Information,
-			new EventId(1, nameof(_log)),
-			"This is test message");
-
+			new EventId(1, nameof(_log1)),
+			"log message");
 	[Fact]
-	public void Define_ログメッセージを出力できることを確認する() {
+	public void Define_ログメッセージの出力を確認する() {
 		// Arrange
 		var logger = new TestLogger();
 
 		// Act
-		_log(logger, null);
+		_log1(logger, null);
 
 		// Assert
 		Assert.Single(logger.Messages);
-		Assert.Equal("This is test message", logger.Messages.First());
+		Assert.Equal("log message", logger.Messages.First());
 	}
 }
