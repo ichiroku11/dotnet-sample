620b52ed51967e1509751601a616ce81ee8f9eba
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-12-20 08:33:21 +0900

    Test LoggerMessage.Define

--- src/SampleTest/Extensions.Logging/LoggerMessageTest.cs
+++ src/SampleTest/Extensions.Logging/LoggerMessageTest.cs
@@ -4,11 +4,20 @@ namespace SampleTest.Extensions.Logging;
 
 // https://learn.microsoft.com/ja-jp/aspnet/core/fundamentals/logging/loggermessage
 public class LoggerMessageTest {
+	// ログメッセージ
 	private static readonly Action<ILogger, Exception?> _log1
 		= LoggerMessage.Define(
 			LogLevel.Information,
 			new EventId(1, nameof(_log1)),
 			"log message");
+
+	// プレースホルダーを指定したログメッセージ
+	private static readonly Action<ILogger, string, Exception?> _log2
+		= LoggerMessage.Define<string>(
+			LogLevel.Information,
+			new EventId(2, nameof(_log2)),
+			"log message: arg='{arg}'");
+
 	[Fact]
 	public void Define_ログメッセージの出力を確認する() {
 		// Arrange
@@ -20,5 +29,18 @@ public class LoggerMessageTest {
 		// Assert
 		Assert.Single(logger.Messages);
 		Assert.Equal("log message", logger.Messages.First());
+	}
+
+	[Fact]
+	public void Define_プレースホルダーを指定したログメッセージの出力を確認する() {
+		// Arrange
+		var logger = new TestLogger();
+
+		// Act
+		_log2(logger, "arg", null);
+
+		// Assert
+		Assert.Single(logger.Messages);
+		Assert.Equal("log message: arg='arg'", logger.Messages.First());
 	}
 }
