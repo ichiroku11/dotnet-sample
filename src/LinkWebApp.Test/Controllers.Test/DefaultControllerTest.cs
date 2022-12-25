f33d072e9599c1b8a0966bc9912307d5fac8189f
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2022-12-26 08:25:40 +0900

    Add project

---
+++ src/LinkWebApp.Test/Controllers.Test/DefaultControllerTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using Xunit;
+
+namespace LinkWebApp.Controllers.Test;
+
+public class DefaultControllerTest : IClassFixture<WebApplicationFactory<Program>> {
+
+	private readonly WebApplicationFactory<Program> _factory;
+
+	public DefaultControllerTest(WebApplicationFactory<Program> factory) {
+		_factory = factory;
+	}
+
+	[Fact]
+	public async Task Index() {
+		// Arrange
+		var client = _factory.CreateClient();
+
+		// Act
+		var response = await client.GetAsync("/");
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+	}
+}
