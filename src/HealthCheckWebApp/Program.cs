5de946c59375a0aa03f896beb271c8108e266225
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-04 09:30:33 +0900

    Add HealthSheckService

--- src/HealthCheckWebApp/Program.cs
+++ src/HealthCheckWebApp/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+services.AddHealthChecks();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("/health");
 
 app.Run();
