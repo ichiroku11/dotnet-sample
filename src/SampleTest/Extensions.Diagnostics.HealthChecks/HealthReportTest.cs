2c8e57b573b38c2ba427ea88dbf603151dd701f1
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-07 09:25:17 +0900

    Test HealthReport.Status

--- src/SampleTest/Extensions.Diagnostics.HealthChecks/HealthReportTest.cs
+++ src/SampleTest/Extensions.Diagnostics.HealthChecks/HealthReportTest.cs
@@ -4,10 +4,30 @@ namespace SampleTest.Extensions.Diagnostics.HealthChecks;
 
 public class HealthReportTest {
 	public static TheoryData<IReadOnlyDictionary<string, HealthReportEntry>, HealthStatus> GetTheoryDataForStatus() {
+		static HealthReportEntry createEntry(HealthStatus status)
+			=> new(status: status, description: default, duration: default, exception: default, data: default);
+
 		return new() {
+			// エントリが空の場合はHealthy
 			{
 				new Dictionary<string, HealthReportEntry>(),
 				HealthStatus.Healthy
+			},
+			// 最も悪いStatusになる
+			{
+				new Dictionary<string, HealthReportEntry> {
+					["a"] = createEntry(HealthStatus.Degraded),
+					["b"] = createEntry(HealthStatus.Healthy),
+				},
+				HealthStatus.Degraded
+			},
+			{
+				new Dictionary<string, HealthReportEntry> {
+					["a"] = createEntry(HealthStatus.Degraded),
+					["b"] = createEntry(HealthStatus.Unhealthy),
+					["c"] = createEntry(HealthStatus.Healthy),
+				},
+				HealthStatus.Unhealthy
 			},
 		};
 	}
@@ -15,9 +35,10 @@ public class HealthReportTest {
 	[Theory, MemberData(nameof(GetTheoryDataForStatus))]
 	public void Status_値を確認する(IReadOnlyDictionary<string, HealthReportEntry> entries, HealthStatus expected) {
 		// Arrange
+		var report = new HealthReport(entries, default);
 
 		// Act
-		var actual = new HealthReport(entries, TimeSpan.Zero).Status;
+		var actual = report.Status;
 
 		// Assert
 		Assert.Equal(expected, actual);
