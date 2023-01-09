da096ab794ce841e5038db033024cb770b7f5283
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-09 10:41:48 +0900

    Add ValidationRequiredController

---
+++ src/ModelBindingWebApp/Controllers/ValidationRequiredController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingWebApp.Controllers;
+
+// Required属性によるバリデーションを確認する
+// 参考）
+// https://learn.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-7.0#required-validation-on-the-server
+// A non-nullable field is always valid, and the [Required] attribute's error message is never displayed.
+// NULLでないフィールドは常に有効であり、[Required]属性のエラーメッセージが表示されることはない。
+[Route("api/[controller]")]
+[ApiController]
+public class ValidationRequiredController : ControllerBase {
+	// 値型のプロパティを持つモデル
+	// 値が送信されなくてもバリデーションエラーは表示されないはず
+	public record ValueTypeModel([Required] int Value);
+
+	[HttpPost]
+	public IActionResult ValueType(ValueTypeModel model) => Ok(model);
+
+
+	// null許容値型のプロパティを持つモデル
+	public record NullableValueTypeModel([Required] int? Value);
+
+	[HttpPost]
+	public IActionResult NullableValueType(NullableValueTypeModel model) => Ok(model);
+
+
+	// 参照型のプロパティを持つモデル
+	public record ReferenceTypeModel([Required] string Value);
+
+	[HttpPost]
+	public IActionResult ReferenceType(ReferenceTypeModel model) => Ok(model);
+
+
+	// null許容参照型のプロパティを持つモデル
+	public record NullableReferenceTypeModel([Required] string? Value);
+
+	[HttpPost]
+	public IActionResult NullableReferenceType(NullableReferenceTypeModel model) => Ok(model);
+}
