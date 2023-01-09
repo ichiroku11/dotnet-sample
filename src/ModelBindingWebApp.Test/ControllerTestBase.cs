f6aa73cb82660b7827785fd282c4feaa5a805ced
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-09 09:44:49 +0900

    Use LoggingHandler

--- src/ModelBindingWebApp.Test/ControllerTestBase.cs
+++ src/ModelBindingWebApp.Test/ControllerTestBase.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Mvc.Testing.Handlers;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -20,6 +21,51 @@ public abstract class ControllerTestBase : IClassFixture<WebApplicationFactory<S
 	}
 
 	protected void WriteLine(string message) => _output.WriteLine(message);
+
+	protected HttpClient CreateClient(bool logging = true) {
+		var options = _factory.ClientOptions;
+
+		// 参考
+		// https://github.com/dotnet/aspnetcore/blob/main/src/Mvc/Mvc.Testing/src/WebApplicationFactoryClientOptions.cs#L64-L79
+		IEnumerable<DelegatingHandler> createHandlers() {
+			if (logging) {
+				yield return new LoggingHandler(_output);
+			}
+
+			if (options.AllowAutoRedirect) {
+				yield return new RedirectHandler(options.MaxAutomaticRedirections);
+			}
+
+			if (options.HandleCookies) {
+				yield return new CookieContainerHandler();
+			}
+		}
+
+		return _factory.CreateDefaultClient(options.BaseAddress, createHandlers().ToArray());
+	}
+
+	private class LoggingHandler : DelegatingHandler {
+		private readonly ITestOutputHelper _output;
+
+		public LoggingHandler(ITestOutputHelper output) {
+			_output = output;
+		}
+
+		protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) {
+			_output.WriteLine(request.ToString());
+			if (request.Content != null) {
+				_output.WriteLine(await request.Content.ReadAsStringAsync(cancellationToken));
+			}
+
+			var response = await base.SendAsync(request, cancellationToken);
+
+			_output.WriteLine(response.ToString());
+			if (response.Content != null) {
+				_output.WriteLine(await response.Content.ReadAsStringAsync(cancellationToken));
+			}
+			return response;
+		}
+	}
 
 	protected async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request) {
 		_output.WriteLine(request.ToString());
