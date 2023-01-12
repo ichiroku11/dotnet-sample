6be77cad1783c02ea0a8ab3183dbb326537f04a6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-13 08:11:53 +0900

    Test BindRequiredAttribute

---
+++ src/ModelBindingWebApp.Test/Controllers.Test/ValidationBindRequiredControllerTest.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using System.Net;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ModelBindingWebApp.Controllers.Test;
+
+// BindRequired属性
+public class ValidationBindRequiredControllerTest : ControllerTestBase {
+	public ValidationBindRequiredControllerTest(ITestOutputHelper output, WebApplicationFactory<Startup> factory) : base(output, factory) {
+	}
+
+	// 値型
+	[Fact]
+	public async Task ValueType_BindRequired属性が指定された値型のプロパティは値が送信されてなくもバリデーションエラーにならない() {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		var response = await client.PostAsJsonAsync("/api/validation/bindrequired/valuetype", new { });
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+	}
+
+	// null許容値型
+	// todo:
+
+	// 参照型
+	// todo:
+
+	// null許容参照型
+	// todo:
+}
