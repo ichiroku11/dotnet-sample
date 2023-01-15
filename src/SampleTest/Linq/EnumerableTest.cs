58f1550a7f2e110c38f29c43315105f53dc7ec40
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-15 10:05:39 +0900

    Test Enumerable.Cast

--- src/SampleTest/Linq/EnumerableTest.cs
+++ src/SampleTest/Linq/EnumerableTest.cs
@@ -1,6 +1,33 @@
 namespace SampleTest.Linq;
 
 public class EnumerableTest {
+	private interface ISample {
+	}
+
+	private class Sample : ISample {
+	}
+
+	[Fact]
+	public void Cast_castする() {
+		// Arrange
+		var values = new object[] {
+			new Sample(),
+			new { },
+		};
+
+		// Act
+		var actual = values.Cast<ISample>();
+
+		// Assert
+		// 1つ目はcastできるので取得できる
+		Assert.IsType<Sample>(actual.First());
+
+		// 2つ目を取得しようとするとcastできないので例外が発生する
+		Assert.Throws<InvalidCastException>(() => {
+			actual.Skip(1).First();
+		});
+	}
+
 	[Fact]
 	public void Prepend_シーケンスの最初に要素を追加する() {
 		// Arrange
@@ -112,12 +139,6 @@ public class EnumerableTest {
 		// Act
 		// Assert
 		Assert.True(first.SequenceEqual(second));
-	}
-
-	private interface ISample {
-	}
-
-	private class Sample : ISample {
 	}
 
 	[Fact]
