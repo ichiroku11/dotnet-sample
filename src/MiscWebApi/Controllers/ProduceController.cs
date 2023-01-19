4d94575f9587949d0cc24570787fec5cbf0a8d28
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-01-20 08:30:56 +0900

    Add ProduceController

---
+++ src/MiscWebApi/Controllers/ProduceController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiscWebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ProduceController : ControllerBase {
+	[HttpGet]
+	public IActionResult Default() => Ok();
+}
