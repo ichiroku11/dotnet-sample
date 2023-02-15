33e08187997e6eddd1ffd8e73c1e11f6d1715c79
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-02-16 08:20:00 +0900

    Test ECDsa.SignData

--- src/SampleTest/Security.Cryptography/ECDsaTest.cs
+++ src/SampleTest/Security.Cryptography/ECDsaTest.cs
@@ -1,10 +1,15 @@
-using Microsoft.VisualStudio.TestPlatform.Utilities;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace SampleTest.Security.Cryptography;
 
 public class ECDsaTest {
+	private readonly ITestOutputHelper _output;
+
+	public ECDsaTest(ITestOutputHelper output) {
+		_output = output;
+	}
+
 	[Fact]
 	public void Create_生成したインスタンスの各プロパティを確認する() {
 		// Arrange
@@ -14,5 +19,23 @@ public class ECDsaTest {
 		// Assert
 		Assert.Null(ecdsa.KeyExchangeAlgorithm);
 		Assert.Equal("ECDsa", ecdsa.SignatureAlgorithm);
+	}
+
+	[Fact]
+	public void SignData_秘密鍵がない場合は例外が発生する() {
+		// Arrange
+		using var ecdsa = ECDsa.Create(ECDsa.Create().ExportParameters(false));
+
+		var data = Encoding.UTF8.GetBytes("あいうえお");
+		var hashAlgorithm = HashAlgorithmName.SHA256;
+
+		// Act
+		// Assert
+		var exception = Assert.ThrowsAny<CryptographicException>(() => {
+			ecdsa.SignData(data, hashAlgorithm);
+		});
+
+		_output.WriteLine(exception.GetType().FullName);
+		_output.WriteLine(exception.Message);
 	}
 }
