28f1ec0a09484e9efd30f0183bfbbecde1f0c64b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-02-17 08:29:56 +0900

    Test ECDsa.VerifyData

--- src/SampleTest/Security.Cryptography/ECDsaTest.cs
+++ src/SampleTest/Security.Cryptography/ECDsaTest.cs
@@ -40,10 +40,11 @@ public class ECDsaTest {
 	}
 
 	[Fact]
-	public void VerifyData_署名を検証する() {
+	public void VerifyData_署名を検証する_ExportParameters() {
 		// Arrange
 		// 署名用のECDsa
 		using var signer = ECDsa.Create();
+
 		// 検証用のECDsa
 		using var verifier = ECDsa.Create(signer.ExportParameters(false));
 
@@ -55,6 +56,30 @@ public class ECDsaTest {
 
 		// Act
 		// 署名を検証する
+		var actual = verifier.VerifyData(data, signature, hashAlgorithm);
+
+		// Assert
+		Assert.True(actual);
+	}
+
+	// パラメーターをSubjectPublicKeyInfo形式でエクスポート・インポートする
+	[Fact]
+	public void VerifyData_署名を検証する_ExportSubjectPublicKeyInfo() {
+		// Arrange
+		// 署名用のECDsa
+		using var signer = ECDsa.Create();
+		var publicKey = signer.ExportSubjectPublicKeyInfo();
+
+		// 検証用のECDsa
+		using var verifier = ECDsa.Create();
+		verifier.ImportSubjectPublicKeyInfo(publicKey, out _);
+
+		var data = Encoding.UTF8.GetBytes("あいうえお");
+		var hashAlgorithm = HashAlgorithmName.SHA256;
+
+		var signature = signer.SignData(data, hashAlgorithm);
+
+		// Act
 		var actual = verifier.VerifyData(data, signature, hashAlgorithm);
 
 		// Assert
