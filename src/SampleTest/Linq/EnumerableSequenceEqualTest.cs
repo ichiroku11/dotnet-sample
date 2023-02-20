4ded8a526d323fc3c30fe9834e7f49700567a5ee
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-02-21 08:08:21 +0900

    Test Enumerable.SequenceEqual

--- src/SampleTest/Linq/EnumerableSequenceEqualTest.cs
+++ src/SampleTest/Linq/EnumerableSequenceEqualTest.cs
@@ -12,4 +12,19 @@ public class EnumerableSequenceEqualTest {
 			Enumerable.SequenceEqual(first, second);
 		});
 	}
+
+	[Theory]
+	[InlineData(new[] { 0 }, new[] { 0 }, true)]
+	// お互い空の場合は等しい
+	[InlineData(new int[] { }, new int[] { }, true)]
+	// 順番が異なる場合は等しくない
+	[InlineData(new[] { 0, 1 }, new[] { 1, 0 }, false)]
+	public void SequenceEqual_いろいろな比較を試す(IEnumerable<int> first, IEnumerable<int> second, bool expected) {
+		// Arrange
+		// Act
+		var actual = Enumerable.SequenceEqual(first, second);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
 }
