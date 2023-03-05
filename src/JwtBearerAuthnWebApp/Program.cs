e6faca0815bd09ea77d05723741bfbe411004b0a
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-03-05 10:30:25 +0900

    Add comment

--- src/JwtBearerAuthnWebApp/Program.cs
+++ src/JwtBearerAuthnWebApp/Program.cs
@@ -1,3 +1,7 @@
+
+// 参考
+// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/jwt-authn?view=aspnetcore-7.0&tabs=windows
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
