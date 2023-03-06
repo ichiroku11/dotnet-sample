09dd29c6b532255f4e5327b51e1d41ab0cdd17d5
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-03-07 07:50:33 +0900

    Use JwtBearer

--- src/JwtBearerAuthnWebApp/Program.cs
+++ src/JwtBearerAuthnWebApp/Program.cs
@@ -1,10 +1,22 @@
+using System.Security.Claims;
 
 // 参考
 // https://learn.microsoft.com/en-us/aspnet/core/security/authentication/jwt-authn?view=aspnetcore-7.0&tabs=windows
 
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddAuthorization();
+services.AddAuthentication("Bearer").AddJwtBearer();
+
+
 var app = builder.Build();
 
+app.UseAuthorization();
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/secret", (ClaimsPrincipal user) => $"Hello {user.Identity?.Name}. My secret")
+	.RequireAuthorization();
 
 app.Run();
