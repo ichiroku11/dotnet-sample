7b6246d660f4c5fa8ce16608f27b732a135bde7e
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-03-19 08:36:11 +0900

    Test HMACSHA256.ComputeHash

--- src/SampleTest/Security.Cryptography/HMACSHA256Test.cs
+++ src/SampleTest/Security.Cryptography/HMACSHA256Test.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography;
+using System.Text;
 
 namespace SampleTest.Security.Cryptography;
 
@@ -15,5 +16,22 @@ public class HMACSHA256Test {
 		Assert.Equal(64, hmac.Key.Length);
 		Assert.Equal(256, hmac.HashSize);
 		Assert.Equal(HashAlgorithmName.SHA256.Name, hmac.HashName);
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData("あいうえお")]
+	[InlineData("0123456789abcdefghijklmnopqrstuvwxyz_0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789abcdefghijklmnopqrstuvwxyz")]
+	public void ComputeHash_ハッシュ値の長さを確認する(string message) {
+		// Arrange
+		using var hmac = new HMACSHA256();
+		var data = Encoding.UTF8.GetBytes(message);
+
+		// Act
+		var hash = hmac.ComputeHash(data);
+
+		// Assert
+		// 32バイト（256ビット）になる
+		Assert.Equal(32, hash.Length);
 	}
 }
