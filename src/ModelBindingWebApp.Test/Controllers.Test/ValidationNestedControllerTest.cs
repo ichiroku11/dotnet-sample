112a541c5598c7c149cc860d983d8bb0e2533434
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-03-22 08:29:38 +0900

    Test nested model validation

---
+++ src/ModelBindingWebApp.Test/Controllers.Test/ValidationNestedControllerTest.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using System.Net;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace ModelBindingWebApp.Controllers.Test;
+
+public class ValidationNestedControllerTest : ControllerTestBase {
+	public ValidationNestedControllerTest(ITestOutputHelper output, WebApplicationFactory<Startup> factory) : base(output, factory) {
+	}
+
+	[Fact]
+	public async Task Nested_空のJSONをPOSTするとInnerModel自体がバインドされない() {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		var response = await client.PostAsJsonAsync("/api/validation/nested", new { });
+		var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+		// Assert
+		Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+		Assert.Collection(
+			problem!.Errors.OrderBy(error => error.Key),
+			entry => {
+				// OuterModel.Innerがnullになり、
+				// OuterModel.Innerに設定されたRequired属性のバリデーションエラーになる
+				Assert.Equal("Inner", entry.Key);
+				Assert.Single(entry.Value, "Inner is required.");
+				foreach (var message in entry.Value) {
+					WriteLine(message);
+				}
+			});
+	}
+}
