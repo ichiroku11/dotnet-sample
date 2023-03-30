2798d4d7e73589ad848e90eb5c841cc2326a7220
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-03-31 08:38:57 +0900

    Test Policy.HandleInner

--- src/SampleTest/Polly/RetryPolicyTest.cs
+++ src/SampleTest/Polly/RetryPolicyTest.cs
@@ -129,9 +129,10 @@ public class RetryPolicyTest {
 
 	public static TheoryData<Exception?> GetTheoryData_HandleInner_NotRetry() {
 		return new() {
-			// 内部例外はnull
+			// 内部例外がnullだとリトライされない
 			new Exception("", null),
-			// 内部の例外が対象の例外ではない
+
+			// 内部例外が対象の例外ではないとリトライされない
 			new Exception("", new ArgumentOutOfRangeException()),
 		};
 	}
@@ -147,6 +148,8 @@ public class RetryPolicyTest {
 		Assert.Throws<Exception>(() => {
 			policy.Execute(() => {
 				count++;
+				_output.WriteLine($"Executed: {count}");
+
 				throw exception;
 			});
 		});
@@ -154,5 +157,38 @@ public class RetryPolicyTest {
 		// Assert
 		// リトライされない
 		Assert.Equal(1, count);
+	}
+
+	public static TheoryData<Exception?> GetTheoryData_HandleInner_Retry() {
+		return new() {
+			// 内部例外が対象の例外だとリトライされる
+			// 発生する例外はExceptionではなくSampleExceptionになる様子
+			new Exception(null, new SampleException()),
+
+			// 外側の例外が対象の例外だとリトライされる
+			new SampleException(),
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryData_HandleInner_Retry))]
+	public void HandleInner_対象の例外と一致する場合はリトライする(Exception exception) {
+		// Arrange
+		var policy = Policy.HandleInner<SampleException>().Retry();
+		var count = 0;
+
+		// Act
+		Assert.Throws<SampleException>(() => {
+			policy.Execute(() => {
+				count++;
+				_output.WriteLine($"Executed: {count}");
+
+				throw exception;
+			});
+		});
+
+		// Assert
+		// リトライされている
+		Assert.Equal(2, count);
 	}
 }
