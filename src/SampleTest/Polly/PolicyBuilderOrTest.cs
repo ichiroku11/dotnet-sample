c5def32490fc3d494abcc48198d7209dbd1de335
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-04-01 08:19:27 +0900

    Test PolicyBuilder.Or

---
+++ src/SampleTest/Polly/PolicyBuilderOrTest.cs
@@ -0,0 +1,59 @@
+
+using Polly;
+
+namespace SampleTest.Polly;
+
+public class PolicyBuilderOrTest {
+	private class SampleException1 : Exception {
+	}
+
+	private class SampleException2 : Exception {
+	}
+
+	private class SampleException3 : Exception {
+	}
+
+
+	private readonly ITestOutputHelper _output;
+
+	public PolicyBuilderOrTest(ITestOutputHelper output) {
+		_output = output;
+	}
+
+	public static TheoryData<Exception, int> GetTheoryData_Or() {
+		return new() {
+			// Handleで指定したのでリトライされる
+			{ new SampleException1(), 2 },
+
+			// Orで指定したのでリトライされる
+			{ new SampleException2(), 2 },
+
+			// HnadleでもOrでも指定していないのでリトライされない
+			{  new SampleException3(), 1 }
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryData_Or))]
+	public void Or_指定した例外であればリトライされる(Exception exception, int expectedCount) {
+		// Arrange
+		var policy = Policy
+			.Handle<SampleException1>()
+			.Or<SampleException2>()
+			.Retry();
+		var actualCount = 0;
+
+		// Act
+		Assert.Throws(exception.GetType(), () => {
+			policy.Execute(() => {
+				actualCount++;
+				_output.WriteLine($"Executed: {actualCount}");
+
+				throw exception;
+			});
+		});
+
+		// Assert
+		Assert.Equal(expectedCount, actualCount);
+	}
+}
