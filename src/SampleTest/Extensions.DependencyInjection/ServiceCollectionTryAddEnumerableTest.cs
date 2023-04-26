405116939c56118359593b23c0175788e9d3e543
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-04-27 08:39:06 +0900

    Test ServiceCollection

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionTryAddEnumerableTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionTryAddEnumerableTest.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace SampleTest.Extensions.DependencyInjection;
 
+// AddXxx/TryAddXxx/TryAddEnumerableの違いを確認する
 public class ServiceCollectionTryAddEnumerableTest {
 	private interface ISampleService {
 	}
@@ -10,5 +11,33 @@ public class ServiceCollectionTryAddEnumerableTest {
 	}
 
 	private class SampleService2 : ISampleService {
+	}
+
+	[Fact]
+	public void AddScoped_同じ実装を複数追加できる() {
+		// Arrange
+		var services = new ServiceCollection();
+
+		// Act
+		services
+			.AddScoped<ISampleService, SampleService1>()
+			.AddScoped<ISampleService, SampleService1>();
+
+		// Assert
+		Assert.Equal(2, services.Count);
+	}
+
+	[Fact]
+	public void AddScoped_異なる実装を複数追加できる() {
+		// Arrange
+		var services = new ServiceCollection();
+
+		// Act
+		services
+			.AddScoped<ISampleService, SampleService1>()
+			.AddScoped<ISampleService, SampleService2>();
+
+		// Assert
+		Assert.Equal(2, services.Count);
 	}
 }
