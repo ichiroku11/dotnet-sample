8d6a6b7144f7c7ac80f7eed2fd7ad819fc05ec61
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-04-27 08:28:00 +0900

    Refactor

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionTryAddEnumerableTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionTryAddEnumerableTest.cs
@@ -11,15 +11,4 @@ public class ServiceCollectionTryAddEnumerableTest {
 
 	private class SampleService2 : ISampleService {
 	}
-
-	// 当然だろうが念のため
-	[Fact]
-	public void インスタンスを生成した直後の要素は空() {
-		// Arrange
-		// Act
-		var services = new ServiceCollection();
-
-		// Assert
-		Assert.Empty(services);
-	}
 }
