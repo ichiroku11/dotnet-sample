d95cd52cac2197c81b7dd2a860ce1a6a495507a8
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-05-03 08:48:28 +0900

    Test OptionsBuilder.Configure

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionOptionsTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionOptionsTest.cs
@@ -10,8 +10,8 @@ public class ServiceCollectionOptionsTest {
 	[Fact]
 	public void Configure_IConfigureOptionsが登録されていることを確認する() {
 		// Arrange
-		var configured = false;
 		var services = new ServiceCollection();
+		var configured = false;
 
 		// Act
 		services
@@ -21,9 +21,29 @@ public class ServiceCollectionOptionsTest {
 			});
 
 		// Assert
-		// IConfigureOptionsは登録されるが、
+		// IConfigureOptionsが登録されるが
 		Assert.Contains(services, service => service.ServiceType == typeof(IConfigureOptions<SampleOptions>));
-		// 呼び出されない
+		// 登録しただけでは呼び出されない
 		Assert.False(configured);
+	}
+
+	[Fact]
+	public void PostConfigure_IPostConfigureOptionsが登録されていることを確認する() {
+		// Arrange
+		var services = new ServiceCollection();
+		var postConfigured = false;
+
+		// Act
+		services
+			.AddOptions<SampleOptions>()
+			.PostConfigure(options => {
+				postConfigured = true;
+			});
+
+		// Assert
+		// IPostConfigureOptionsが登録されるが
+		Assert.Contains(services, service => service.ServiceType == typeof(IPostConfigureOptions<SampleOptions>));
+		// 登録しただけでは呼び出されない
+		Assert.False(postConfigured);
 	}
 }
