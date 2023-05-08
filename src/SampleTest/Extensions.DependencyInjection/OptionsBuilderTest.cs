76dd2a2f586ed235bb280cacdd7f006c0d2e003a
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-05-09 06:15:30 +0900

    Test OptionsBuilder.Configure/OptionsBuilder.PostConfigure

--- src/SampleTest/Extensions.DependencyInjection/OptionsBuilderTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/OptionsBuilderTest.cs
@@ -126,6 +126,61 @@ public class OptionsBuilderTest {
 	}
 
 	[Fact]
+	public void Configure_PostConfigure_呼び出される順番を確認する() {
+		// Arrange
+		var services = new ServiceCollection();
+		var values = new List<string>();
+
+		// Act
+		services
+			.AddOptions<SampleOptions>()
+			.PostConfigure(options => {
+				var value = $"pc1-{options.Value}";
+				_output.WriteLine(value);
+				values.Add(value);
+
+				options.Value++;
+			})
+			.Configure(options => {
+				var value = $"c1-{options.Value}";
+				_output.WriteLine(value);
+				values.Add(value);
+
+				options.Value++;
+			})
+			.PostConfigure(options => {
+				var value = $"pc2-{options.Value}";
+				_output.WriteLine(value);
+				values.Add(value);
+
+				options.Value++;
+			})
+			.Configure(options => {
+				var value = $"c2-{options.Value}";
+				_output.WriteLine(value);
+				values.Add(value);
+
+				options.Value++;
+			});
+
+		var provider = services.BuildServiceProvider();
+		var factory = provider.GetRequiredService<IOptionsFactory<SampleOptions>>();
+
+		// Act
+		var options = factory.Create(Options.DefaultName);
+
+		// Assert
+		// Configure同士、PostConfigure同士では登録した順に実行される
+		// PostConfigureはConfigureの後に実行される
+		Assert.Collection(values,
+			value => Assert.Equal("c1-0", value),
+			value => Assert.Equal("c2-1", value),
+			value => Assert.Equal("pc1-2", value),
+			value => Assert.Equal("pc2-3", value));
+		Assert.Equal(4, options.Value);
+	}
+
+	[Fact]
 	public void PostConfigure_IPostConfigureOptionsが登録されていることを確認する() {
 		// Arrange
 		var services = new ServiceCollection();
