01fd53c626efd42bcd29fd771a3d59adc2c1bb63
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-05-11 15:36:27 +0900

    Test ServiceCollection.Configure

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionOptionsTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionOptionsTest.cs
@@ -4,9 +4,26 @@ using Microsoft.Extensions.Options;
 namespace SampleTest.Extensions.DependencyInjection;
 
 public class ServiceCollectionOptionsTest {
+	private class SampleOptions {
+	}
 	private readonly ITestOutputHelper _output;
 
 	public ServiceCollectionOptionsTest(ITestOutputHelper output) {
 		_output = output;
+	}
+
+	[Fact]
+	public void Configure_IConfigureOptionsが登録されていることを確認する() {
+		// Arrange
+		var services = new ServiceCollection();
+
+		// Act
+		services.Configure<SampleOptions>(options => {
+		});
+
+		// Assert
+		// AddOptionsしなくても
+		// IConfigureOptionsが登録されている
+		Assert.Single(services, service => service.ServiceType == typeof(IConfigureOptions<SampleOptions>));
 	}
 }
