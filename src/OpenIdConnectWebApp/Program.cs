4ff3ed7e8bdff51402c686ce8b7fae7e688f56d0
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-06-02 12:14:41 +0900

    LINE login

--- src/OpenIdConnectWebApp/Program.cs
+++ src/OpenIdConnectWebApp/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using System.Security.Claims;
 
 // 参考
@@ -23,10 +24,14 @@ services
 		config.GetSection("OpenIdConnect").Bind(options);
 
 		// 下記より
-		// https://developers.line.biz/ja/docs/line-login/verify-id-token/
+		// https://developers.line.biz/ja/docs/line-login/verify-id-token/#signature
 		// OpenIDプロバイダーの情報
 		// todo: LineDefaultsか
 		options.MetadataAddress = "https://access.line.me/.well-known/openid-configuration";
+
+		// response_typeはcode
+		// https://developers.line.biz/ja/docs/line-login/integrate-line-login/#applying-for-email-permission
+		options.ResponseType = OpenIdConnectResponseType.Code;
 	});
 
 services.AddAuthorization();
