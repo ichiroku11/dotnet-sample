606ea2e62f3f5fe4b16e33d72d37ed948a9e8a21
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-06-07 05:57:28 +0900

    Test CollectionCountAttribute

--- src/ModelBindingWebApp.Test/Controllers.Test/ValidationCollectionCountControllerTest.cs
+++ src/ModelBindingWebApp.Test/Controllers.Test/ValidationCollectionCountControllerTest.cs
@@ -12,6 +12,52 @@ public class ValidationCollectionCountControllerTest : ControllerTestBase {
 	public ValidationCollectionCountControllerTest(ITestOutputHelper output, WebApplicationFactory<Startup> factory) : base(output, factory) {
 	}
 
+	[Fact]
+	public async Task Test_BadRequest_空のJSON() {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		var response = await client.PostAsJsonAsync("/api/validation/collectioncount", new { });
+		var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+		// Assert
+		Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+		Assert.NotNull(problem);
+		Assert.Collection(
+			problem.Errors.OrderBy(error => error.Key),
+			entry => {
+				Assert.Equal("Values", entry.Key);
+				foreach (var message in entry.Value) {
+					WriteLine(message);
+				}
+			});
+	}
+
+	[Theory]
+	[InlineData(new[] { 1 })]
+	[InlineData(new[] { 1, 2, 3, 4 })]
+	public async Task Test_BadRequest_属性で指定した最大最小の範囲外(int[] values) {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		var response = await client.PostAsJsonAsync("/api/validation/collectioncount", new { values });
+		var problem = await response.Content.ReadFromJsonAsync<ValidationProblemDetails>();
+
+		// Assert
+		Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+		Assert.NotNull(problem);
+		Assert.Collection(
+			problem.Errors.OrderBy(error => error.Key),
+			entry => {
+				Assert.Equal("Values", entry.Key);
+				foreach (var message in entry.Value) {
+					WriteLine(message);
+				}
+			});
+	}
+
 	[Theory]
 	[InlineData(new[] { 1, 2 })]
 	[InlineData(new[] { 1, 2, 3 })]
