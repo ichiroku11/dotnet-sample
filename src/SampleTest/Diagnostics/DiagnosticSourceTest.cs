0b3fd090b2b1ae9f34ec843879083b1b5d195d56
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-06-23 07:43:31 +0900

    Test DiagnosticSource.IsEnabled

--- src/SampleTest/Diagnostics/DiagnosticSourceTest.cs
+++ src/SampleTest/Diagnostics/DiagnosticSourceTest.cs
@@ -3,13 +3,30 @@ using System.Diagnostics;
 namespace SampleTest.Diagnostics;
 
 public class DiagnosticSourceTest {
-	private static DiagnosticSource CreateDiagnosticSource(string name = "Test")
+	private const string _diagnosticSourceName = "Test";
+
+	private static DiagnosticSource CreateDiagnosticSource(string name = _diagnosticSourceName)
 		=> new DiagnosticListener(name);
 
 	[Fact]
 	public void IsEnabled_インスタンスを生成しただけではfalseになる() {
 		// Arrange
 		var source = CreateDiagnosticSource();
+
+		// Act
+		var actual = source.IsEnabled("e");
+
+		// Assert
+		Assert.False(actual);
+	}
+
+	[Fact]
+	public void IsEnabled_AllListenersをSubscribeしてもfalseになる() {
+		// Arrange
+		var source = CreateDiagnosticSource();
+
+		using var subscriber = DiagnosticListener.AllListeners.Subscribe(listener => {
+		});
 
 		// Act
 		var actual = source.IsEnabled("e");
