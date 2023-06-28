378e35504a5acc56cbc2ff6bdf59405bec99cd32
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-06-29 06:00:07 +0900

    Test JsonDocument.Parse

--- src/SampleTest/Text.Json/JsonDocumentTest.cs
+++ src/SampleTest/Text.Json/JsonDocumentTest.cs
@@ -4,10 +4,17 @@ namespace SampleTest.Text.Json;
 
 public class JsonDocumentTest {
 	[Theory]
-	[InlineData("[]", JsonValueKind.Array)]
-	[InlineData("[1]", JsonValueKind.Array)]
+	// undefined単体はパースできない様子？
+	//[InlineData("undefined", JsonValueKind.Undefined)]
 	[InlineData("{}", JsonValueKind.Object)]
 	[InlineData(@"{""x"":1}", JsonValueKind.Object)]
+	[InlineData("[]", JsonValueKind.Array)]
+	[InlineData("[1]", JsonValueKind.Array)]
+	[InlineData(@"""""", JsonValueKind.String)]
+	[InlineData("0", JsonValueKind.Number)]
+	[InlineData("true", JsonValueKind.True)]
+	[InlineData("false", JsonValueKind.False)]
+	[InlineData("null", JsonValueKind.Null)]
 	public void Parse_JSON文字列をパースしてValueKindを取得する(string json, JsonValueKind expected) {
 		// Arrange
 		// Act
