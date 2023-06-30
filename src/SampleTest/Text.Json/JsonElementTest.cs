dc55e1aa3b2fac33611fa86bff518c2df25adc12
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-07-01 08:47:56 +0900

    Test JsonElement.EnumerateArray

--- src/SampleTest/Text.Json/JsonElementTest.cs
+++ src/SampleTest/Text.Json/JsonElementTest.cs
@@ -16,7 +16,20 @@ public class JsonElementTest {
 		Assert.Equal(3, actual);
 	}
 
+	[Fact]
+	public void EnumerateArray_配列の要素を列挙できる() {
+		// Arrange
+		using var document = JsonDocument.Parse("[1, 2, 3]");
+		var root = document.RootElement;
+
+		// Act
+		var values = root.EnumerateArray()
+			.Select(element => element.GetInt32());
+
+		// Assert
+		Assert.Equal(new[] { 1, 2, 3 }, values);
+	}
+
 	// todo:
-	// JsonElement.EnumerateArray
 	// JsonElement.EnumerateObject
 }
