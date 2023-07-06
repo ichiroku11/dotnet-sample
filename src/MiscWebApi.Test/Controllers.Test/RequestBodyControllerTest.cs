a415a6c40bc4b5865daa2ee1c3d9f876d1e61f49
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-07-07 06:08:13 +0900

    Test RequestBodyController

---
+++ src/MiscWebApi.Test/Controllers.Test/RequestBodyControllerTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Net.Http.Json;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MiscWebApi.Controllers.Test;
+
+public class RequestBodyControllerTest : ControllerTestBase {
+	public RequestBodyControllerTest(ITestOutputHelper output, WebApplicationFactory<Program> factory)
+		: base(output, factory) {
+	}
+
+	private class Result {
+		public string Body { get; init; } = "";
+
+		public string Value { get; init; } = "";
+	}
+
+	[Fact]
+	public async Task JsonWithBind_モデルにバインド済みなのでRequestBodyの中身は空文字() {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		using var response = await client.PostAsJsonAsync("/api/requestbody/jsonwithbind", new { value = "abc" });
+		var actual = await response.Content.ReadFromJsonAsync<Result>();
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		Assert.NotNull(actual);
+		Assert.Empty(actual.Body);
+		Assert.Equal("abc", actual.Value);
+	}
+
+	[Fact]
+	public async Task JsonWithoutBind_モデルにバインドしていないのでRequestBodyから読み取れる() {
+		// Arrange
+		var client = CreateClient();
+
+		// Act
+		using var response = await client.PostAsJsonAsync("/api/requestbody/jsonwithoutbind", new { value = "abc" });
+		var actual = await response.Content.ReadFromJsonAsync<Result>();
+
+		// Assert
+		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		Assert.NotNull(actual);
+		Assert.Equal(@"{""value"":""abc""}", actual.Body);
+		Assert.Empty(actual.Value);
+	}
+}
---
+++ src/MiscWebApi/Controllers/RequestBodyController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiscWebApi.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class RequestBodyController : ControllerBase {
+	public class Sample {
+		public string Value { get; init; } = "";
+	}
+
+	// The request body can only be read once, from beginning to end.
+	// 要求本文は、最初から最後まで1回のみ読み取ることができます
+	// https://learn.microsoft.com/ja-jp/aspnet/core/fundamentals/use-http-context?view=aspnetcore-7.0#enable-request-body-buffering
+
+	// モデルにバインド済みなのでRequest.Bodyの中身は空文字
+	[HttpPost]
+	public async Task<IActionResult> JsonWithBind([FromBody] Sample model) {
+		// bodyは空文字
+		using var reader = new StreamReader(Request.Body);
+		var body = await reader.ReadToEndAsync();
+
+		return Ok(new { body, model.Value });
+	}
+
+	// モデルにバインドしていないのでRequest.Bodyから読み取れる
+	[HttpPost]
+	public async Task<IActionResult> JsonWithoutBind() {
+		using var reader = new StreamReader(Request.Body);
+		var body = await reader.ReadToEndAsync();
+
+		return Ok(new { body });
+	}
+
+	// todo: form
+}
