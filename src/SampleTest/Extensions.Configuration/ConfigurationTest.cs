318e6d3e488ad22e0e642f907c1ddcdf4c26c47e
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-07-15 08:31:55 +0900

    Test IConfiguration.GetChildren

--- src/SampleTest/Extensions.Configuration/ConfigurationTest.cs
+++ src/SampleTest/Extensions.Configuration/ConfigurationTest.cs
@@ -15,4 +15,23 @@ public class ConfigurationTest {
 		// Assert
 		Assert.Empty(children);
 	}
+
+	[Fact]
+	public void GetChildren_サブセクションが1つ() {
+		// Arrange
+		var root = new ConfigurationBuilder()
+			.AddInMemoryCollection(new Dictionary<string, string?>{
+				{ "x", "1" },
+			})
+			.Build();
+
+		// Act
+		var children = root.GetChildren();
+
+		// Assert
+		var child = Assert.Single(children);
+		Assert.Equal("x", child.Key);
+		Assert.Equal("x", child.Path);
+		Assert.Equal("1", child.Value);
+	}
 }
