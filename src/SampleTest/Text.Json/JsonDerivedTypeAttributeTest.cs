6c17d2ddfaa50509538626081d3c1cb164a74e3c
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-10-10 06:16:54 +0900

    Test JsonDerivedTypeAttribute

--- src/SampleTest/Text.Json/JsonDerivedTypeAttributeTest.cs
+++ src/SampleTest/Text.Json/JsonDerivedTypeAttributeTest.cs
@@ -51,4 +51,49 @@ public class JsonDerivedTypeAttributeTest {
 		// 派生クラスの情報が欠落する
 		Assert.Equal(@"{""base"":1}", actual);
 	}
+
+
+	[JsonDerivedType(typeof(DerivedWithAttribute))]
+	private abstract class BaseWithAttribute {
+		// 属性でJSON文字列のプロパティの並び順を制御する
+		[JsonPropertyOrder(1)]
+		public int Base { get; init; }
+	}
+
+	private class DerivedWithAttribute : BaseWithAttribute {
+		[JsonPropertyOrder(2)]
+		public int Derived { get; init; }
+	}
+
+	[Fact]
+	public void Serialize_基本クラスの型を指定してシリアライズしても派生クラスの情報が欠落しない() {
+		// Arrange
+		var derived = new DerivedWithAttribute {
+			Base = 1,
+			Derived = 2,
+		};
+
+		// Act
+		var actual = JsonSerializer.Serialize<BaseWithAttribute>(derived, _options);
+
+		// Assert
+		// 派生クラスの情報が欠落しない
+		Assert.Equal(@"{""base"":1,""derived"":2}", actual);
+	}
+
+	[Fact]
+	public void Serialize_基本クラスとしてシリアライズしても派生クラスの情報が欠落しない() {
+		// Arrange
+		BaseWithAttribute derived = new DerivedWithAttribute {
+			Base = 1,
+			Derived = 2,
+		};
+
+		// Act
+		var actual = JsonSerializer.Serialize(derived, _options);
+
+		// Assert
+		// 派生クラスの情報が欠落しない
+		Assert.Equal(@"{""base"":1,""derived"":2}", actual);
+	}
 }
