c1a088732113f4ed04e67988009385298be82b7a
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-10-31 13:42:24 +0900

    Test NCrontab

---
+++ src/SampleTest/NCrontabTest.cs
@@ -0,0 +1,18 @@
+using NCrontab;
+
+namespace SampleTest;
+
+public class NCrontabTest {
+	[Fact]
+	public void NCrontabを使ってみる() {
+		// Arrange
+		var schedule = CrontabSchedule.Parse("*/5 * * * *");
+		var today = DateTime.Today;
+
+		// Act
+		var actual = schedule.GetNextOccurrence(today);
+
+		// Assert
+		Assert.Equal(today.AddMinutes(5), actual);
+	}
+}
