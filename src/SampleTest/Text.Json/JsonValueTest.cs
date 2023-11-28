fedacec0fc75fe0f21b01a6cbf6d0557ca7c4a1f
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2023-11-29 06:06:24 +0900

    Test JsonValue

---
+++ src/SampleTest/Text.Json/JsonValueTest.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Nodes;
+
+namespace SampleTest.Text.Json;
+
+public class JsonValueTest {
+	private readonly ITestOutputHelper _output;
+
+	public JsonValueTest(ITestOutputHelper output) {
+		_output = output;
+	}
+
+	[Fact]
+	public void Create_引数にint32を渡して生成したインスタンスのプロパティなどを確認する() {
+		// Arrange
+
+		// Act
+		var actual = JsonValue.Create(1);
+		_output.WriteLine(actual.ToJsonString());
+
+		// Assert
+		Assert.Null(actual.Parent);
+		Assert.Same(actual, actual.Root);
+		Assert.Equal(1, (int)actual);
+	}
+}
