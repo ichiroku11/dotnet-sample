4b0f572b7110af94a9002da855ca555c35155042
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-01-30 06:04:32 +0900

    Test Base64StringAttribute

--- src/SampleTest/ComponentModel.DataAnnotations/Base64StringAttributeTest.cs
+++ src/SampleTest/ComponentModel.DataAnnotations/Base64StringAttributeTest.cs
@@ -2,9 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SampleTest.ComponentModel.DataAnnotations;
 
-public class Base64StringAttributeTest(ITestOutputHelper output) {
-	private readonly ITestOutputHelper _output = output;
-
+public class Base64StringAttributeTest {
 	[Theory]
 	[InlineData(null, true)]
 	// 文字列以外はfalseになる
@@ -12,7 +10,13 @@ public class Base64StringAttributeTest(ITestOutputHelper output) {
 	[InlineData(0, false)]
 	// 空文字はtrueになる
 	[InlineData("", true)]
-	public void IsValid(object? value, bool expected) {
+	// 文字列"abcd"
+	[InlineData("YWJjZA==", true)]
+	// 内部的にはConvert.TryFromBase64Stringが使われている？
+	// https://learn.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations.base64stringattribute?view=net-8.0
+	// Base64に使われる文字がtrueというわけではない
+	[InlineData("=", false)]
+	public void IsValid_確認する(object? value, bool expected) {
 		// Arrange
 		var attribute = new Base64StringAttribute();
 
