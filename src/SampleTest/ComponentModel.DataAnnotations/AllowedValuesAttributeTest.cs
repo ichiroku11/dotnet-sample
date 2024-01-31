bbbff5a6a69586f675317a8079346965b5e5e614
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-02-01 06:06:47 +0900

    AllowedValuesAttributeTest

---
+++ src/SampleTest/ComponentModel.DataAnnotations/AllowedValuesAttributeTest.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleTest.ComponentModel.DataAnnotations;
+
+public class AllowedValuesAttributeTest {
+	[Theory]
+	// 指定した値（許可する値）なので有効
+	[InlineData("a", true)]
+	[InlineData("b", true)]
+	// 指定した値ではないので無効
+	[InlineData(null, false)]
+	[InlineData("A", false)]
+	[InlineData("B", false)]
+	public void IsValid_確認する(object? value, bool expected) {
+		// Arrange
+		var attribute = new AllowedValuesAttribute("a", "b");
+
+		// Act
+		var actual = attribute.IsValid(value);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+}
