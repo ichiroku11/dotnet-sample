f5d573fa6ef62d543ff8395b8d8273e3c4fb1862
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-02-04 13:21:46 +0900

    Test LengthAttribute

---
+++ src/SampleTest/ComponentModel.DataAnnotations/LengthAttributeTest.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleTest.ComponentModel.DataAnnotations;
+
+public class LengthAttributeTest {
+	[Theory]
+#pragma warning disable CA1861 // Avoid constant arrays as arguments
+	[InlineData(new[] { 0 }, false)]
+	[InlineData(new[] { 0, 1 }, true)]
+	[InlineData(new[] { 0, 1, 2 }, true)]
+	[InlineData(new[] { 0, 1, 2, 3 }, false)]
+#pragma warning restore CA1861 // Avoid constant arrays as arguments
+	public void IsValid_配列の長さを確認する(int[] values, bool expected) {
+		// Arrange
+		var attribute = new LengthAttribute(2, 3);
+
+		// Act
+		var actual = attribute.IsValid(values);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+}
