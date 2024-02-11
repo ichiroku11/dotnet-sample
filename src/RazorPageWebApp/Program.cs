40acab761ba3bf2ded69c882fa6a2f0382c4febd
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-02-11 16:30:43 +0900

    Use lowercase url

--- src/RazorPageWebApp/Program.cs
+++ src/RazorPageWebApp/Program.cs
@@ -5,6 +5,11 @@ var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 services.AddRazorPages();
 
+services.Configure<RouteOptions>(options => {
+	options.LowercaseQueryStrings = true;
+	options.LowercaseUrls = true;
+});
+
 services.AddScoped<MonsterRepository>();
 
 var app = builder.Build();
