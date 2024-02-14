87fbb1aa617c5f441d2d3a0ff048358bb8935d95
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-02-15 06:15:31 +0900

    Refactor

--- src/RazorPageWebApp/Pages/Index.cshtml.cs
+++ src/RazorPageWebApp/Pages/Index.cshtml.cs
@@ -7,12 +7,12 @@ namespace RazorPageWebApp.Pages;
 public class IndexModel(MonsterRepository repository) : PageModel {
 	private readonly MonsterRepository _repository = repository;
 
-	public Task<IList<Monster>> QueryMonstersAsync()
-		=> _repository.QueryAsync(new MonsterListQueryOption(Query));
+	public IList<Monster> Monsters { get; private set; } = new List<Monster>();
 
 	[BindProperty(SupportsGet = true)]
 	public string Query { get; set; } = "";
 
-	public void OnGet() {
+	public async Task OnGetAsync() {
+		Monsters = await _repository.QueryAsync(new MonsterListQueryOption(Query));
 	}
 }
