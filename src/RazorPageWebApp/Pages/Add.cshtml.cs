009cb25412205b57fa3e0b8bb5fcd62c4b43bba6
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-02-16 06:10:48 +0900

    Update Add.cshtml

--- src/RazorPageWebApp/Pages/Add.cshtml.cs
+++ src/RazorPageWebApp/Pages/Add.cshtml.cs
@@ -1,8 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPageWebApp.Pages;
 
 public class AddModel : PageModel {
+	[BindProperty]
+	public int Id { get; set; }
+
+	[BindProperty]
+	public string Name { get; set; } = "";
+
 	public void OnGet() {
+	}
+
+	public Task OnPostAsync() {
+
+		// todo:
+
+		return Task.CompletedTask;
 	}
 }
