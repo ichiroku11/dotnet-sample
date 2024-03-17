e454f81e8ac6a350e1b2faff1cbf552d70a67550
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-03-18 06:13:53 +0900

    Test OpenIdConnectMessage

--- src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectMessageTest.cs
+++ src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectMessageTest.cs
@@ -4,6 +4,17 @@ namespace SampleTest.IdentityModel.Protocols.OpenIdConnect;
 
 public class OpenIdConnectMessageTest {
 	[Fact]
+	public void Properties_インスタンスの各プロパティの値を確認する() {
+		// Arrange
+		var message = new OpenIdConnectMessage();
+
+		// Act
+		// Assert
+		Assert.Empty(message.IssuerAddress);
+		Assert.Empty(message.Parameters);
+	}
+
+	[Fact]
 	public void BuildRedirectUrl_インスタンスを生成しただけで呼び出す空文字になる() {
 		// Arrange
 		var message = new OpenIdConnectMessage();
