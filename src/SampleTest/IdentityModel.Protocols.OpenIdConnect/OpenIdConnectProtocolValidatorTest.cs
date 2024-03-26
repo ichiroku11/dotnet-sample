2fb3d6091f186e01fd198a11631e2d4ffcc94afe
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-03-27 05:59:17 +0900

    Test OpenIdConnectProtocolValidator

--- src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectProtocolValidatorTest.cs
+++ src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectProtocolValidatorTest.cs
@@ -11,4 +11,31 @@ public class OpenIdConnectProtocolValidatorTest {
 		Assert.Equal(TimeSpan.FromMinutes(60), OpenIdConnectProtocolValidator.DefaultNonceLifetime);
 		Assert.True(OpenIdConnectProtocolValidator.RequireSubByDefault);
 	}
+
+	[Fact]
+	public void Properties_デフォルトコンストラクターで生成したインスタンスの各プロパティの値を確認する() {
+		// Arrange
+		var validator = new OpenIdConnectProtocolValidator();
+
+		// Act
+		// Assert
+		Assert.NotNull(validator.CryptoProviderFactory);
+		Assert.NotEmpty(validator.HashAlgorithmMap);
+		Assert.Null(validator.IdTokenValidator);
+		Assert.Equal(TimeSpan.FromMinutes(60), validator.NonceLifetime);
+		Assert.False(validator.RequireAcr);
+		Assert.False(validator.RequireAmr);
+		Assert.False(validator.RequireAuthTime);
+		Assert.False(validator.RequireAzp);
+		Assert.True(validator.RequireNonce);
+		Assert.True(validator.RequireState);
+		Assert.True(validator.RequireStateValidation);
+		Assert.True(validator.RequireSub);
+		Assert.True(validator.RequireTimeStampInNonce);
+	}
+
+	// todo:
+	// ValidateAuthenticationResponse
+	// ValidateTokenResponse
+	// ValidateUserInfoResponse
 }
