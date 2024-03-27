a7359dbd48c1c473dd3a8451db31a2490b145e79
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-03-28 06:09:27 +0900

    Test OpenIdConnectProtocolValidator

--- src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectProtocolValidatorTest.cs
+++ src/SampleTest/IdentityModel.Protocols.OpenIdConnect/OpenIdConnectProtocolValidatorTest.cs
@@ -2,7 +2,9 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace SampleTest.IdentityModel.Protocols.OpenIdConnect;
 
-public class OpenIdConnectProtocolValidatorTest {
+public class OpenIdConnectProtocolValidatorTest(ITestOutputHelper output) {
+	private readonly ITestOutputHelper _output = output;
+
 	[Fact]
 	public void Properties_staticプロパティの値を確認する() {
 		// Arrange
@@ -34,8 +36,43 @@ public class OpenIdConnectProtocolValidatorTest {
 		Assert.True(validator.RequireTimeStampInNonce);
 	}
 
+	public static TheoryData<OpenIdConnectProtocolValidationContext> GetTheoryData_ValidateAuthenticationResponse_ThrowsException() {
+		return new() {
+			// https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/blob/dev/src/Microsoft.IdentityModel.Protocols.OpenIdConnect/OpenIdConnectProtocolValidator.cs#L208
+			// IDX21333: ProtocolMessage is null
+			new OpenIdConnectProtocolValidationContext(),
+			// IDX21334: Both 'id_token' and 'code' are null
+			new OpenIdConnectProtocolValidationContext
+			{
+				ProtocolMessage = new OpenIdConnectMessage(),
+			},
+			// IDX21332: ValidatedIdToken is null
+			new OpenIdConnectProtocolValidationContext
+			{
+				ProtocolMessage = new OpenIdConnectMessage
+				{
+					IdToken = "id-token",
+					Code = "code"
+				},
+			},
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryData_ValidateAuthenticationResponse_ThrowsException))]
+	public void ValidateAuthenticationResponse_ThrowsException(OpenIdConnectProtocolValidationContext context) {
+		// Arrange
+		var validator = new OpenIdConnectProtocolValidator();
+
+		// Act
+		var actual = Record.Exception(() => validator.ValidateAuthenticationResponse(context));
+
+		// Assert
+		Assert.NotNull(actual);
+		_output.WriteLine(actual.Message);
+	}
+
 	// todo:
-	// ValidateAuthenticationResponse
 	// ValidateTokenResponse
 	// ValidateUserInfoResponse
 }
