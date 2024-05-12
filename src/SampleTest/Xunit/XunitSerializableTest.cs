d48ff0d8e439a229850dce95f6bc75da4544c124
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-05-12 09:47:34 +0900

    Test IXunitSerializable

--- src/SampleTest/Xunit/XunitSerializableTest.cs
+++ src/SampleTest/Xunit/XunitSerializableTest.cs
@@ -4,7 +4,7 @@ public class XunitSerializableTest {
 	// TheoryDataの型引数がシリアラズできる必要がある
 	// テストデータごとにテスト結果が把握できる
 	// https://xunit.net/xunit.analyzers/rules/xUnit1045
-	public static TheoryData<string> GetTheoryData_Test_Serializable() {
+	public static TheoryData<string> GetTheoryData_Test_String() {
 		return new() {
 			"a",
 			"b",
@@ -12,8 +12,8 @@ public class XunitSerializableTest {
 	}
 
 	[Theory]
-	[MemberData(nameof(GetTheoryData_Test_Serializable))]
-	public void Test_Serializable(string value) {
+	[MemberData(nameof(GetTheoryData_Test_String))]
+	public void Test_String(string value) {
 		// Arrange
 		// Act
 		// Assert
@@ -23,21 +23,21 @@ public class XunitSerializableTest {
 
 	// TheoryDataの型引数がシリアラズできないので
 	// テストデータごとにテスト結果が把握できない（テスト結果が1つにまとめる）
-	public class Sample(string value) {
+	public class SampleNotSerializable(string value) {
 		public string Value { get; } = value;
 	}
 
-	public static TheoryData<Sample> GetTheoryData_Test_NotSerializable() {
+	public static TheoryData<SampleNotSerializable> GetTheoryData_Test_NotSerializable() {
 		return new() {
-			new Sample("a"),
-			new Sample("b"),
+			new SampleNotSerializable("a"),
+			new SampleNotSerializable("b"),
 		};
 	}
 
 #pragma warning disable xUnit1045
 	[Theory]
 	[MemberData(nameof(GetTheoryData_Test_NotSerializable))]
-	public void Test_NotSerializable(Sample sample) {
+	public void Test_NotSerializable(SampleNotSerializable sample) {
 		// Arrange
 		// Act
 		// Assert
@@ -45,4 +45,37 @@ public class XunitSerializableTest {
 		Assert.NotEmpty(sample.Value);
 	}
 #pragma warning restore xUnit1045
+
+	// テストデータごとにテスト結果を把握するために、
+	// シリアライズできないテストデータ（TheoryDataの型引数）はIXunitSerializableを実装する
+	// IXunitSerializableを実装するには、パラメーターが存在しないパブリックコンストラクターが必要
+	// https://xunit.net/xunit.analyzers/rules/xUnit3001
+	public class SampleSerializable() : IXunitSerializable {
+		public string Value { get; set; } = "";
+
+		public void Deserialize(IXunitSerializationInfo info) {
+			Value = info.GetValue<string>(nameof(Value));
+		}
+
+		public void Serialize(IXunitSerializationInfo info) {
+			info.AddValue(nameof(Value), Value, typeof(string));
+		}
+	}
+
+	public static TheoryData<SampleSerializable> GetTheoryData_Test_Serializable() {
+		return new() {
+			new SampleSerializable { Value = "a" },
+			new SampleSerializable { Value = "b" },
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryData_Test_Serializable))]
+	public void Test_Serializable(SampleSerializable sample) {
+		// Arrange
+		// Act
+		// Assert
+		// 特に意味はないAssert
+		Assert.NotEmpty(sample.Value);
+	}
 }
