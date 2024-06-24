2aa99e1a7a0ddd439f296d5a31b4a40609d41b69
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-06-25 06:08:20 +0900

    Test spread element

---
+++ src/SampleTest/CSharpLanguage/OperatorsAndExpressions/SpreadElementTest.cs
@@ -0,0 +1,28 @@
+using System.Linq.Expressions;
+using System;
+
+namespace SampleTest.CSharpLanguage.OperatorsAndExpressions;
+
+// https://learn.microsoft.com/ja-jp/dotnet/csharp/language-reference/operators/collection-expressions#spread-element
+public class SpreadElementTest {
+	[Fact]
+	public void SpreadElement_使ってみる() {
+		// Arrange
+		int[] values1 = [1, 2, 3];
+		int[] values2 = [4, 5];
+
+		// Act
+		int[] actual = [.. values1, .. values2];
+
+		// 型を指定しないとエラーになるっぽい
+		// CS9176
+		// There is no target type for the collection expression.
+		//var actual = [.. values1, .. values2];
+		//var actual = [.. new[] { 1, 2, 3 }, .. new[] { 4, 5, 6 }];
+		//var actual = [.. [1, 2, 3], .. [4, 5, 6]];
+
+		// Assert
+		int[] expected = [1, 2, 3, 4, 5];
+		Assert.Equal(expected, actual);
+	}
+}
