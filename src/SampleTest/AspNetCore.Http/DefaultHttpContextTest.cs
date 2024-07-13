5bcae39bd56d28ff2e1f59e1ba451920d35fff48
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-07-14 07:19:35 +0900

    Test DefaultHttpContext

--- src/SampleTest/AspNetCore.Http/DefaultHttpContextTest.cs
+++ src/SampleTest/AspNetCore.Http/DefaultHttpContextTest.cs
@@ -14,9 +14,31 @@ public class DefaultHttpContextTest {
 		Assert.Null(context.RequestServices);
 
 		Assert.NotNull(context.Request);
+		// Request.BodyはStream.Null
 		Assert.Same(Stream.Null, context.Request.Body);
 
 		Assert.NotNull(context.Response);
+		// Response.BodyはStream.Null
 		Assert.Same(Stream.Null, context.Response.Body);
+	}
+
+	[Fact]
+	public void Properties_FeatureCollectionを指定して生成したインスタンスのプロパティを確認する() {
+		// Arrange
+		var context = new DefaultHttpContext(new FeatureCollection());
+
+		// Act
+		// Assert
+		Assert.Null(context.RequestServices);
+
+		Assert.NotNull(context.Request);
+		// Request.Bodyを取得しようとすると例外
+		var exception = Record.Exception(() => context.Request.Body);
+		Assert.IsType<NullReferenceException>(exception);
+
+		Assert.NotNull(context.Response);
+		// Response.Bodyを取得しようとすると例外
+		exception = Record.Exception(() => context.Response.Body);
+		Assert.IsType<NullReferenceException>(exception);
 	}
 }
