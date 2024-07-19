c27efb78cd529e6639b1ad737e50d98d9294d782
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-07-20 07:38:43 +0900

    Test BadRequestOfT

--- src/SampleTest/AspNetCore.Http.HttpResults/BadRequestOfTValueTest.cs
+++ src/SampleTest/AspNetCore.Http.HttpResults/BadRequestOfTValueTest.cs
@@ -19,4 +19,23 @@ public class BadRequestOfTValueTest {
 		Assert.Equal(400, result.StatusCode);
 		Assert.Same(value, result.Value);
 	}
+
+	[Fact]
+	public async Task ExecuteAsync_レスポンスを確認する() {
+		// Arrange
+		var value = new { x = 1, y = "abc" };
+		var result = TypedResults.BadRequest(value);
+
+		var context = HttpContextHelper.CreateWithResponseBody();
+
+		// Act
+		await result.ExecuteAsync(context);
+
+		// Assert
+		Assert.Equal(400, context.Response.StatusCode);
+		Assert.Equal("application/json; charset=utf-8", context.Response.ContentType);
+
+		var responseBody = await context.Response.ReadBodyAsString();
+		Assert.Equal(@"{""x"":1,""y"":""abc""}", responseBody);
+	}
 }
