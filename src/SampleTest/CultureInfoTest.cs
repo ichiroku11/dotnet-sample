0029482e3e4759f6d2092b48ae170d07c372f1e2
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-07-29 06:13:36 +0900

    Test CultureInfo with DateTime.TryParse

--- src/SampleTest/CultureInfoTest.cs
+++ src/SampleTest/CultureInfoTest.cs
@@ -5,34 +5,48 @@ namespace SampleTest;
 public class CultureInfoTest(ITestOutputHelper output) {
 	private readonly ITestOutputHelper _output = output;
 
-	public static TheoryData<string, DateTime> GetTheoryData_DateTime_TryParse_JapaneseCulture() {
+	public static TheoryData<string, string, bool, DateTime> GetTheoryData_DateTime_TryParse() {
 		var today = DateTime.Today;
+
+		var cultures = new {
+			Invariant = "",
+			Japanese = "ja-JP",
+		};
 
 		return new() {
 			// 想定通り
-			{ today.ToString("yyyy/MM/dd"), today },
-			{ today.ToString("yyyy-MM-dd"), today },
+			{ today.ToString("yyyy/MM/dd"), cultures.Japanese, true, today },
 			// ちょっと不思議
-			{ today.ToString("MM/dd/yyyy"), today },
-			{ today.ToString("yyyy.MM.dd"), today },
+			{ today.ToString("yyyy/MM/dd"), cultures.Invariant, true, today },
+
 			// 想定通り
-			{ today.ToString("yyyy年M月d日"), today },
-			{ today.ToString("yyyy年MM月dd日"), today },
+			{ today.ToString("yyyy-MM-dd"), cultures.Japanese, true, today },
+			// 想定通り
+			{ today.ToString("yyyy-MM-dd"), cultures.Invariant, true, today },
+
+			// todo:
+			// ちょっと不思議
+			{ today.ToString("MM/dd/yyyy"), cultures.Japanese, true, today },
+			{ today.ToString("yyyy.MM.dd"), cultures.Japanese, true, today },
+			// 想定通り
+			{ today.ToString("yyyy年M月d日"), cultures.Japanese, true, today },
+			{ today.ToString("yyyy年MM月dd日"), cultures.Japanese, true, today },
 		};
 	}
 
-	[Theory]
-	[MemberData(nameof(GetTheoryData_DateTime_TryParse_JapaneseCulture))]
-	public void DateTime_TryParse_JapaneseCulture_文字列を日付に変換できる(string text, DateTime expected) {
+	[Theory, MemberData(nameof(GetTheoryData_DateTime_TryParse))]
+	public void DateTime_TryParse_文字列を日付に変換できる(string text, string cultureName, bool expectedParsed, DateTime expectedResult) {
 		// Arrange
-		var culture = new CultureInfo("ja-JP");
+		var culture = string.IsNullOrWhiteSpace(cultureName)
+			? CultureInfo.InvariantCulture
+			: new CultureInfo("ja-JP");
 
 		// Act
-		var result = DateTime.TryParse(text, culture, out var actual);
-		_output.WriteLine(result.ToString());
+		var actualParsed = DateTime.TryParse(text, culture, out var actualResult);
+		_output.WriteLine(actualParsed.ToString());
 
 		// Assert
-		Assert.True(result);
-		Assert.Equal(expected, actual);
+		Assert.Equal(expectedParsed, actualParsed);
+		Assert.Equal(expectedResult, actualResult);
 	}
 }
