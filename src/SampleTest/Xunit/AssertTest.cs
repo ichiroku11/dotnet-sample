4bcefed966a20a0525991047bcb68c63d755123c
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-08-22 06:09:19 +0900

    Refactor

--- src/SampleTest/Xunit/AssertTest.cs
+++ src/SampleTest/Xunit/AssertTest.cs
@@ -3,73 +3,95 @@ namespace SampleTest.Xunit;
 public class AssertTest {
 	[Fact]
 	public void All_すべての要素がパスすることを確認する() {
+		// Arrange
+		// Act
+		// Assert
 		Assert.All(new[] { 2, 4, 6 }, value => Assert.Equal(0, value % 2));
 	}
 
 	[Fact]
 	public void NotEqual_文字列の比較は大文字小文字を区別する() {
+		// Arrange
+		// Act
+		// Assert
 		Assert.NotEqual("x", "X");
 	}
 
 	[Fact]
 	public async Task ThrowsAsync_例外の発生をテストする() {
+		// Arrange
+		// Act
+		// Assert
 		var exception = await Assert.ThrowsAsync<InvalidOperationException>(() => {
 			throw new InvalidOperationException();
 		});
 	}
 
 	[Fact]
-	public void Contains_DoesNotContain_文字列が含まれているか含まれていないかを検証する() {
-		var text = "abcdefg";
-
-		Assert.Contains("cde", text);
-		Assert.DoesNotContain("fgh", text);
+	public void Contains_文字列が含まれているか検証する() {
+		// Arrange
+		// Act
+		// Assert
+		Assert.Contains("cde", "abcdefg");
 	}
 
 	[Fact]
-	public void Contains_DoesNotContain_コレクションに要素が含まれているか含まれていないかを検証する() {
+	public void DoesNotContain_文字列が含まれていないか検証する() {
+		// Arrange
+		// Act
+		// Assert
+		Assert.DoesNotContain("fgh", "abcdefg");
+	}
+
+	[Fact]
+	public void Contains_コレクションに要素が含まれているか検証する() {
+		// Arrange
+		// Act
+		// Assert
 		// IEnumerable<T>
-		{
-			// 数値
-			var values = new[] { 1 };
-			Assert.Contains(1, values);
-			Assert.DoesNotContain(2, values);
-		}
+		// 数値
+		Assert.Contains(1, new[] { 1 });
 
 		// IEnumerable<T>
-		{
-			// 文字列
-			var values = new[] { "a" };
-			Assert.Contains("a", values);
-			Assert.DoesNotContain("b", values);
-		}
+		// 文字列
+		Assert.Contains("a", new[] { "a" });
 
 		// 条件を満たす要素が存在するか、存在しないか
-		{
-			var values = new[] { 1 };
-			Assert.Contains(values, value => value % 2 == 1);
-			Assert.DoesNotContain(values, value => value % 2 == 0);
-		}
+		Assert.Contains(new[] { 1 }, value => value % 2 == 1);
 
 		// HashSet<T>
-		{
-			var set = new HashSet<int> { 1 };
-			Assert.Contains(1, set);
-			Assert.DoesNotContain(2, set);
-		}
+		Assert.Contains(1, new HashSet<int> { 1 });
 
 		// SortedSet<T>
-		{
-			var set = new SortedSet<int> { 1 };
-			Assert.Contains(1, set);
-			Assert.DoesNotContain(2, set);
-		}
+		Assert.Contains(1, new SortedSet<int> { 1 });
 
 		// Dictionary<TKey, TValue>のキー
-		{
-			var dictionary = new Dictionary<int, string> { [1] = "" };
-			Assert.Contains(1, dictionary);
-			Assert.DoesNotContain(2, dictionary);
-		}
+		Assert.Contains(1, new Dictionary<int, string> { [1] = "" });
+	}
+
+	[Fact]
+	public void DoesNotContain_コレクションに要素が含まれていないか検証する() {
+		// Arrange
+		// Act
+		// Assert
+		// IEnumerable<T>
+		// 数値
+		Assert.DoesNotContain(2, new[] { 1 });
+
+		// IEnumerable<T>
+		// 文字列
+		Assert.DoesNotContain("b", new[] { "a" });
+
+		// 条件を満たす要素が存在するか、存在しないか
+		Assert.DoesNotContain(new[] { 1 }, value => value % 2 == 0);
+
+		// HashSet<T>
+		Assert.DoesNotContain(2, new HashSet<int> { 1 });
+
+		// SortedSet<T>
+		Assert.DoesNotContain(2, new SortedSet<int> { 1 });
+
+		// Dictionary<TKey, TValue>のキー
+		Assert.DoesNotContain(2, new Dictionary<int, string> { [1] = "" });
 	}
 }
