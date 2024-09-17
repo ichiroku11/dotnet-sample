5429459334b1592ee9a9f6c520bf49bc6b8f16ce
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-09-18 06:07:22 +0900

    Fix method name

--- src/SampleTest/UriTest.cs
+++ src/SampleTest/UriTest.cs
@@ -180,7 +180,7 @@ public class UriTest(ITestOutputHelper output) {
 	[InlineData("https://example.jp/")]
 	[InlineData("https://example.jp/app")]
 	[InlineData("file://example.jp/app")]
-	public void TryCreate_絶対URLの成功する(string url) {
+	public void TryCreate_絶対URLの作成に成功する(string url) {
 		// Arrange
 		// Act
 		var result = Uri.TryCreate(url, UriKind.Absolute, out var _);
