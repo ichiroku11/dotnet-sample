2b2b31e458d2ebe5f3b54585c507c0672c9022f2
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-09-25 06:10:24 +0900

    Test HttpListener

--- src/SampleTest/Net/HttpListenerTest.cs
+++ src/SampleTest/Net/HttpListenerTest.cs
@@ -2,7 +2,9 @@ using System.Net;
 
 namespace SampleTest.Net;
 
-public class HttpListenerTest {
+public class HttpListenerTest(ITestOutputHelper output) {
+	private readonly ITestOutputHelper _output = output;
+
 	[Fact]
 	public void Properties_生成したインスタンスのプロパティを確認する() {
 		// Arrange
@@ -15,5 +17,18 @@ public class HttpListenerTest {
 		Assert.False(listener.IsListening);
 		Assert.Empty(listener.Prefixes);
 		Assert.Null(listener.Realm);
+	}
+
+	[Fact]
+	public void GetContext_インスタンスを生成した直後に呼び出すと例外が発生する() {
+		// Arrange
+		using var listener = new HttpListener();
+
+		// Act
+		var exception = Record.Exception(() => listener.GetContext());
+
+		// Assert
+		Assert.IsType<InvalidOperationException>(exception);
+		_output.WriteLine(exception.Message);
 	}
 }
