d6f3ef86fc427fa2bccb5e237523985bdc640631
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-10-14 09:14:09 +0900

    Test Ascii.IsValid

--- src/SampleTest/Text/AsciiTest.cs
+++ src/SampleTest/Text/AsciiTest.cs
@@ -8,6 +8,8 @@ public class AsciiTest {
 	[InlineData("0123456789", true)]
 	[InlineData("abcdefghijklmnopqrstuvwxyz", true)]
 	[InlineData("ABCDEFGHIJKLMNOPQRSTUVWXYZ", true)]
+	// 全角の"０"を含む
+	[InlineData("０", false)]
 	public void IsValid_ASCII文字列のみを含むか判定できる(string value, bool expected) {
 		// Arrange
 		// Act
