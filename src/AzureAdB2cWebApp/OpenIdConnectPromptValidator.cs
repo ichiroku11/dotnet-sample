dd42a8a100e6d78f3b26870b5cafa4c96cc5a989
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-10-21 05:40:40 +0900

    Add OpenIdConnectPromptValidator

---
+++ src/AzureAdB2cWebApp/OpenIdConnectPromptValidator.cs
@@ -0,0 +1,15 @@
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace AzureAdB2cWebApp;
+
+// 3.1.2.1.  Authentication Request
+// https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+public static class OpenIdConnectPromptValidator {
+	public static bool IsValid(string prompt) {
+		// Azure AD B2Cでは"login"と"none"に対応している
+		// "login"はドキュメントに記載があるが、"none"については記載なさげ
+		return
+			string.Equals(prompt, OpenIdConnectPrompt.Login, StringComparison.Ordinal) ||
+			string.Equals(prompt, OpenIdConnectPrompt.None, StringComparison.Ordinal);
+	}
+}
