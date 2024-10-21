9bbb405179093668c9747396a7dea4859be7cd5b
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-10-22 05:58:51 +0900

    Update

--- src/AzureAdB2cWebApp/AuthenticationPropertiesExtensions.cs
+++ src/AzureAdB2cWebApp/AuthenticationPropertiesExtensions.cs
@@ -4,12 +4,20 @@ using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 namespace AzureAdB2cWebApp;
 
 public static class AuthenticationPropertiesExtensions {
-	public static AuthenticationProperties SetPromptIfValid(this AuthenticationProperties properties, string prompt) {
+	public static AuthenticationProperties SetPromptIfValid(this AuthenticationProperties properties, string? prompt) {
 		if (!OpenIdConnectPromptValidator.IsValid(prompt)) {
 			return properties;
 		}
 
 		properties.SetParameter(OpenIdConnectParameterNames.Prompt, prompt);
+
+		return properties;
+	}
+
+	private const string _policy = "policy";
+
+	public static AuthenticationProperties SetPolicy(this AuthenticationProperties properties, string? policy) {
+		properties.Items[_policy] = policy;
 
 		return properties;
 	}
--- src/AzureAdB2cWebApp/OpenIdConnectPromptValidator.cs
+++ src/AzureAdB2cWebApp/OpenIdConnectPromptValidator.cs
@@ -5,9 +5,10 @@ namespace AzureAdB2cWebApp;
 // 3.1.2.1.  Authentication Request
 // https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
 public static class OpenIdConnectPromptValidator {
-	public static bool IsValid(string prompt) {
+	public static bool IsValid(string? prompt) {
 		// Azure AD B2Cでは"login"と"none"に対応している
 		// "login"はドキュメントに記載があるが、"none"については記載なさげ
+		// https://learn.microsoft.com/ja-jp/azure/active-directory-b2c/openid-connect
 		return
 			string.Equals(prompt, OpenIdConnectPrompt.Login, StringComparison.Ordinal) ||
 			string.Equals(prompt, OpenIdConnectPrompt.None, StringComparison.Ordinal);
