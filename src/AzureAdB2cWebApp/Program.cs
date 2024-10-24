b74766da42ec1b493829484563007a0edceebd29
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-10-25 06:11:49 +0900

    Remove Startup

--- src/AzureAdB2cWebApp/Program.cs
+++ src/AzureAdB2cWebApp/Program.cs
@@ -1,13 +1,50 @@
-namespace AzureAdB2cWebApp;
+using AzureAdB2cWebApp;
+using Microsoft.Identity.Web;
 
-public class Program {
-	public static void Main(string[] args) {
-		CreateHostBuilder(args).Build().Run();
-	}
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var env = builder.Environment;
 
-	public static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => {
-				webBuilder.UseStartup<Startup>();
-			});
+var services = builder.Services;
+
+services.AddControllersWithViews();
+
+// Azure AD B2C認証
+// Microsoft.Identity.Webを使う
+services.AddMicrosoftIdentityWebAppAuthentication(
+	configuration: config,
+	configSectionName: "AzureAdB2c",
+	// OpenIdConnectEventsのデバッグログの出力
+	subscribeToOpenIdConnectMiddlewareDiagnosticsEvents: true);
+
+var handlers = new OpenIdConnectEventHandlers();
+services.PostConfigure<MicrosoftIdentityOptions>(options => {
+	// Microsoft.Identity.Web.UIを使わず動きを確認したいため、
+	// デフォルトの動きを上書きする
+	// https://github.com/AzureAD/microsoft-identity-web/blob/master/src/Microsoft.Identity.Web/AzureADB2COpenIDConnectEventHandlers.cs
+	options.Events.OnRemoteFailure = handlers.OnRemoteFailure;
+});
+
+services.Configure<RouteOptions>(options => {
+	options.LowercaseUrls = true;
+	options.LowercaseQueryStrings = true;
+});
+
+var app = builder.Build();
+
+if (env.IsDevelopment()) {
+	app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+// 認証
+app.UseAuthentication();
+
+// 承認
+// AuthorizeAttributeのために必要
+app.UseAuthorization();
+
+app.MapDefaultControllerRoute();
+
+app.Run();
