c6b4cc077eb64977bbd47a986728e3ca9afc645e
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-11-08 05:48:29 +0900

    Refactor

--- src/AzureStorageConsoleApp/Program.cs
+++ src/AzureStorageConsoleApp/Program.cs
@@ -1,21 +1,16 @@
+using AzureStorageConsoleApp;
 using Azure.Storage.Blobs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace AzureStorageConsoleApp;
-
-class Program {
-	static async Task Main(string[] args) {
-		await Host
-			.CreateDefaultBuilder(args)
-			.ConfigureServices((context, services) => {
-				var config = context.Configuration;
-				services
-					.AddHostedService<SampleService>()
-					.AddSingleton(provider => new BlobServiceClient(config.GetConnectionString("Storage")))
-					.AddTransient<BlobSample>();
-			})
-			.RunConsoleAsync();
-	}
-}
+await Host
+	.CreateDefaultBuilder(args)
+	.ConfigureServices((context, services) => {
+		var config = context.Configuration;
+		services
+			.AddHostedService<SampleService>()
+			.AddSingleton(provider => new BlobServiceClient(config.GetConnectionString("Storage")))
+			.AddTransient<BlobSample>();
+	})
+	.RunConsoleAsync();
