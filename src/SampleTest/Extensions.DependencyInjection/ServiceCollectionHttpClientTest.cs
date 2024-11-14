a5f6e9c2133ccbe039b11c76b4aa989c15b793ce
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-11-15 06:06:47 +0900

    Test ServiceCollection.AddHttpClient

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientTest.cs
@@ -18,7 +18,21 @@ public class ServiceCollectionHttpClientTest {
 		Assert.IsType<HttpClient>(client);
 	}
 
-	// todo: HttpClientを複数取得すると同じインスタンス？
+	[Fact]
+	public void AddHttpClient_GetRequiredServiceを呼び出すごとに異なるHttpClientインスタンスを取得できる() {
+		// Arrange
+		var services = new ServiceCollection();
+		services.AddHttpClient();
+
+		var provider = services.BuildServiceProvider();
+
+		// Act
+		var client1 = provider.GetRequiredService<HttpClient>();
+		var client2 = provider.GetRequiredService<HttpClient>();
+
+		// Assert
+		Assert.NotSame(client1, client2);
+	}
 
 	[Fact]
 	public void AddHttpClient_IHttpClientFactoryを実装するインスタンスを取得できる() {
