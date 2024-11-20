1aafa8ee83b55f1c9fdcab52198c33d1a5b9b124
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-11-21 06:09:47 +0900

    Refactor

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientTest.cs
@@ -82,4 +82,35 @@ public class ServiceCollectionHttpClientTest {
 		Assert.Empty(options.HttpClientActions);
 		Assert.Empty(options.HttpMessageHandlerBuilderActions);
 	}
+
+	[Fact]
+	public void AddHttpClient_HttpClientBuilderを取得できる() {
+		// Arrange
+		var services = new ServiceCollection();
+
+		// Act
+		var builder = services.AddHttpClient(name: "abc");
+
+		// Assert
+		Assert.Equal("abc", builder.Name);
+		// 同じインスタンスではない？
+		//Assert.Same(services, builder.Services);
+	}
+
+	[Fact]
+	public void ConfigureHttpClientDefaults_HttpClientFactoryOptionsを取得できる() {
+		// Arrange
+		var services = new ServiceCollection();
+		services.ConfigureHttpClientDefaults(builder => {
+		});
+
+		var provider = services.BuildServiceProvider();
+
+		// Act
+		var options = provider.GetRequiredService<IOptions<HttpClientFactoryOptions>>().Value;
+
+		// Assert
+		Assert.Empty(options.HttpClientActions);
+		Assert.Empty(options.HttpMessageHandlerBuilderActions);
+	}
 }
