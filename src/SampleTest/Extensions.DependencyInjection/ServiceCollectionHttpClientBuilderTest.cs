5c218137251f6c448c9df144edf27dc4b24ff336
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-11-22 05:43:55 +0900

    Test ConfigurePrimaryHttpMessageHandler

--- src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientBuilderTest.cs
+++ src/SampleTest/Extensions.DependencyInjection/ServiceCollectionHttpClientBuilderTest.cs
@@ -5,5 +5,21 @@ using Microsoft.Extensions.Options;
 namespace SampleTest.Extensions.DependencyInjection;
 
 public class ServiceCollectionHttpClientBuilderTest {
+	[Fact]
+	public void ConfigurePrimaryHttpMessageHandler_HttpMessageHandlerBuilderActionsに追加される() {
+		// Arrange
+		var services = new ServiceCollection();
+		services.ConfigureHttpClientDefaults(builder => {
+			builder.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler());
+		});
 
+		var provider = services.BuildServiceProvider();
+
+		// Act
+		var options = provider.GetRequiredService<IOptions<HttpClientFactoryOptions>>().Value;
+
+		// Assert
+		Assert.Empty(options.HttpClientActions);
+		Assert.Single(options.HttpMessageHandlerBuilderActions);
+	}
 }
