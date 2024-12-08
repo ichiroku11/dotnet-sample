c4453c848a3422119200aa98b0d15488e685b667
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-08 09:19:27 +0900

    Test AuthenticationProperties

--- src/SampleTest/AspNetCore.Authentication/AuthenticationPropertiesTest.cs
+++ src/SampleTest/AspNetCore.Authentication/AuthenticationPropertiesTest.cs
@@ -16,4 +16,18 @@ public class AuthenticationPropertiesTest {
 		Assert.False(properties.IsPersistent);
 		Assert.Empty(properties.Parameters);
 	}
+
+	[Fact]
+	public void RedirectUri_設定するとItemsに追加される() {
+		// Arrange
+		// Act
+		var properties = new AuthenticationProperties {
+			RedirectUri = "redirect-uri"
+		};
+
+		// Assert
+		var item = Assert.Single(properties.Items);
+		Assert.Equal(".redirect", item.Key);
+		Assert.Equal("redirect-uri", item.Value);
+	}
 }
