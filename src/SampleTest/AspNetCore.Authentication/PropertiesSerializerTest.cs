515f03899f9977427134aaa5452ab777384545a0
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2024-12-11 06:07:56 +0900

    Test PropertiesSerializer

---
+++ src/SampleTest/AspNetCore.Authentication/PropertiesSerializerTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace SampleTest.AspNetCore.Authentication;
+
+public class PropertiesSerializerTest {
+	[Fact]
+	public void Serialize_Deserialize_動きを確認する() {
+		// Arrange
+		var expected = new AuthenticationProperties {
+			RedirectUri = "redirect-uri",
+		};
+		var serializer = new PropertiesSerializer();
+
+		// Act
+		var serializedData = serializer.Serialize(expected);
+		var actual = serializer.Deserialize(serializedData);
+
+		// Assert
+		Assert.NotNull(actual);
+		Assert.Equal(expected.RedirectUri, actual.RedirectUri);
+	}
+
+	// todo: Parametersはシリアライズされない
+}
