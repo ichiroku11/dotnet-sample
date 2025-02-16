10f33ebbec7598c7b2a191ae107f6056616b7a98
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-02-16 10:30:57 +0900

    Test Queue

--- src/SampleTest/Collections/QueueTest.cs
+++ src/SampleTest/Collections/QueueTest.cs
@@ -3,6 +3,23 @@ namespace SampleTest.Collections;
 
 public class QueueTest {
 	[Fact]
+	public void Enqueue_Dequeue_Enqueueした順にDequeueできる() {
+		// Arrange
+		var queue = new Queue<string>();
+		queue.Enqueue("a");
+		queue.Enqueue("b");
+
+		// Act
+		var actual = new string[] {
+			queue.Dequeue(),
+			queue.Dequeue(),
+		};
+
+		// Assert
+		Assert.Equal(["a", "b"], actual);
+	}
+
+	[Fact]
 	public void ToArray_Enqueueした順に格納された配列を取得できる() {
 		// Arrange
 		var queue = new Queue<string>();
