e5d18dbba6322a9e12466e4fd76ff24d9be6d535
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-01 08:54:39 +0900

    Test Enumerable.RankDescending

--- src/SampleLib.Test/Linq.Test/EnumerableExtensionsTest.cs
+++ src/SampleLib.Test/Linq.Test/EnumerableExtensionsTest.cs
@@ -101,4 +101,37 @@ public class EnumerableExtensionsTest {
 		// Assert
 		Assert.Equal(expected, actual);
 	}
+
+	public static TheoryData<int[], (int, int)[]> GetTheoryData_RankDescending() {
+		return new TheoryData<int[], (int, int)[]> {
+			// 同じ順位がない
+			{
+				new int[] { 10, 20, 30 },
+				new (int, int)[] { (10, 3), (20, 2), (30, 1) }
+			},
+			// 同じ順位がある
+			{
+				new int[] { 10, 20, 30, 30, 40 },
+				new (int, int)[] { (10, 5), (20, 4), (30, 2), (30, 2), (40, 1) }
+			},
+			// 同じ順位があり、シーケンスの並び順は適当
+			// => シーケンスの並びは変わらない
+			{
+				new int[] { 30, 40, 30, 10, 20 },
+				new (int, int)[] { (30, 2), (40, 1), (30, 2), (10, 5), (20, 4) }
+			},
+		};
+	}
+
+	[Theory]
+	[MemberData(nameof(GetTheoryData_RankDescending))]
+	public void RankDescending(int[] values, (int, int)[] expected) {
+		// Arrange
+
+		// Act
+		var actual = values.RankDescending();
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
 }
