6e3e652d870e7aa1021855e4a2d5df6c991b52ef
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-04 05:44:18 +0900

    Test PriorityQueue.Dequeue

--- src/SampleTest/Collections/PriorityQueueTest.cs
+++ src/SampleTest/Collections/PriorityQueueTest.cs
@@ -12,4 +12,21 @@ public class PriorityQueueTest {
 		Assert.NotNull(queue.Comparer);
 		Assert.NotNull(queue.UnorderedItems);
 	}
+
+	[Fact]
+	public void Dequeue_コンストラクターで指定したコレクションの優先度順で取得できる() {
+		// Arrange
+		var queue = new PriorityQueue<string, int>([("a", 2), ("b", 1)]);
+
+		// Act
+		var actual = new string[] {
+			queue.Dequeue(),
+			queue.Dequeue(),
+		};
+
+		// Assert
+		// 優先度順（値が小さい順）に取り出される
+		Assert.Equal(["b", "a"], actual);
+	}
+
 }
