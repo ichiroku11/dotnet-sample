1f51d92071631985830940114240ca6020f440f1
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-19 05:53:11 +0900

    Use Minimal API

--- src/AntiForgeryWebApp/Program.cs
+++ src/AntiForgeryWebApp/Program.cs
@@ -1,13 +1,31 @@
-namespace AntiForgeryWebApp;
+using Microsoft.AspNetCore.Mvc;
 
-public class Program {
-	public static void Main(string[] args) {
-		CreateHostBuilder(args).Build().Run();
-	}
+var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+var env = builder.Environment;
 
-	public static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => {
-				webBuilder.UseStartup<Startup>();
-			});
+var services = builder.Services;
+
+services.AddControllersWithViews(options => {
+	// GET/HEAD/OPTIONS/TRACE以外のメソッドに対してトークンの検証を行う
+	options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
+});
+
+services.Configure<RouteOptions>(options => {
+	options.LowercaseUrls = true;
+	options.LowercaseQueryStrings = true;
+});
+
+var app = builder.Build();
+
+if (env.IsDevelopment()) {
+	app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Default}/{action=Index}/{id?}");
+
+app.Run();
