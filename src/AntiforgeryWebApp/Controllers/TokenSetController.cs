5e094066a980b726d3eaf9bcc608cedc5479c138
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-22 07:40:01 +0900

    Test IAntiforgery

--- src/AntiforgeryWebApp/Controllers/TokenSetController.cs
+++ src/AntiforgeryWebApp/Controllers/TokenSetController.cs
@@ -3,17 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AntiforgeryWebApp.Controllers;
 
-public class TokenSetController(IAntiforgery antiforgery) : Controller {
+public class TokenSetController(IAntiforgery antiforgery, ILogger<TokenSetController> logger) : Controller {
 	private readonly IAntiforgery _antiforgery = antiforgery;
+	private readonly ILogger<TokenSetController> _logger = logger;
 
 	public IActionResult GetTokens() {
 		var tokenSet = _antiforgery.GetTokens(HttpContext);
+
+		_logger.LogInformation("SetCookie: {setCookie}", (string?)Response.Headers.SetCookie);
 
 		return Json(tokenSet);
 	}
 
 	public IActionResult GetAndStoreTokens() {
 		var tokenSet = _antiforgery.GetAndStoreTokens(HttpContext);
+
+		_logger.LogInformation("SetCookie: {setCookie}", (string?)Response.Headers.SetCookie);
 
 		return Json(tokenSet);
 	}
