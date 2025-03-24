01b889771ae36dd5f31eeee36d6e251423751c01
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-25 05:45:26 +0900

    Test Antiforgery

--- src/SampleTest/AspNetCore.Antiforgery/AntiforgeryTest.cs
+++ src/SampleTest/AspNetCore.Antiforgery/AntiforgeryTest.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 namespace SampleTest.AspNetCore.Antiforgery;
 
@@ -17,6 +18,7 @@ public class AntiforgeryTest {
 		};
 
 		var antiforgery = context.RequestServices.GetRequiredService<IAntiforgery>();
+		var options = context.RequestServices.GetRequiredService<IOptions<AntiforgeryOptions>>().Value;
 
 		// Act
 		var actual = antiforgery.GetTokens(context);
@@ -24,5 +26,11 @@ public class AntiforgeryTest {
 		// Assert
 		Assert.NotNull(actual.RequestToken);
 		Assert.NotNull(actual.CookieToken);
+		Assert.Equal(options.FormFieldName, actual.FormFieldName);
+		Assert.Equal(options.HeaderName, actual.HeaderName);
 	}
+
+	// todo: GetAndStoreTokens
+	// todo: GetTokensでResponseにCookieが設定されない
+	// todo: GetAndStoreTokensでResponseにCookieが設定される
 }
