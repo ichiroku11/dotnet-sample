fd55b38569651713bbaaec05d31750d5842045c8
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-03-29 10:54:50 +0900

    Test DefaultHttpContext.Request.Method

--- src/SampleTest/AspNetCore.Http/DefaultHttpContextTest.cs
+++ src/SampleTest/AspNetCore.Http/DefaultHttpContextTest.cs
@@ -20,6 +20,18 @@ public class DefaultHttpContextTest(ITestOutputHelper output) {
 		Assert.Null(context.RequestServices);
 	}
 
+	[Fact]
+	public void Request_インスタンスを生成したときのメソッドは空文字列() {
+		// Arrange
+		var context = new DefaultHttpContext();
+
+		// Act
+		// Assert
+		Assert.NotNull(context.Request);
+		Assert.NotNull(context.Request.Method);
+		Assert.Empty(context.Request.Method);
+	}
+
 	[Theory]
 	[InlineData(false)]
 	[InlineData(true)]
