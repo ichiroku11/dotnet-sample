20cbe96237cffd21d981d8ed2cf0eaaf8e164f42
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-04-08 05:39:51 +0900

    Test TokenSetController

--- src/AntiforgeryWebApp.Test/Controllers.Test/TokenSetControllerTest.cs
+++ src/AntiforgeryWebApp.Test/Controllers.Test/TokenSetControllerTest.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net;
+using System.Net.Http.Json;
 
 namespace AntiforgeryWebApp.Controllers.Test;
 
 public class TokenSetControllerTest(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>> {
 
 	private readonly WebApplicationFactory<Program> _factory = factory;
+
+	private class AntiforgeryTokenSet {
+		public string? RequestToken { get; init; }
+		public string FormFieldName { get; init; } = "";
+		public string? HeaderName { get; init; }
+		public string? CookieToken { get; init; }
+	}
 
 	[Fact]
 	public async Task GetTokens() {
@@ -14,8 +22,14 @@ public class TokenSetControllerTest(WebApplicationFactory<Program> factory) : IC
 
 		// Act
 		var response = await client.GetAsync("/tokenset/gettokens");
+		var tokens = await response.Content.ReadFromJsonAsync<AntiforgeryTokenSet>();
 
 		// Assert
 		Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+		Assert.NotNull(tokens);
+		Assert.NotNull(tokens.RequestToken);
+		Assert.NotNull(tokens.FormFieldName);
+		Assert.NotNull(tokens.HeaderName);
+		Assert.NotNull(tokens.CookieToken);
 	}
 }
