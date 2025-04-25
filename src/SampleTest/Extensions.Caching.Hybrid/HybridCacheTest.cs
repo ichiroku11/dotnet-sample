3bf7756f9eaffa3f54179ad5eb6373002427745d
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-04-26 08:59:24 +0900

    Test HybridCache

---
+++ src/SampleTest/Extensions.Caching.Hybrid/HybridCacheTest.cs
@@ -0,0 +1,22 @@
+using Microsoft.Extensions.Caching.Hybrid;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SampleTest.Extensions.Caching.Hybrid;
+
+public class HybridCacheTest {
+	// HybridCacheクラスは抽象クラスなのでインスタンス化できない
+	// DIで取得する必要がある
+	[Fact]
+	public void Constructor_DIで取得する必要がある() {
+		// Arrange
+		var services = new ServiceCollection();
+		services.AddHybridCache();
+		var provider = services.BuildServiceProvider();
+
+		// Act
+		var cache = provider.GetRequiredService<HybridCache>();
+
+		// Assert
+		Assert.IsAssignableFrom<HybridCache>(cache);
+	}
+}
