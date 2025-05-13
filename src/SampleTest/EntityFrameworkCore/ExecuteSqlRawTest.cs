2a6c809671542459303f71b7bb3cfeefa00c197c
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-05-14 06:01:57 +0900

    Test ExecuteSqlRawAsync

--- src/SampleTest/EntityFrameworkCore/ExecuteSqlRawTest.cs
+++ src/SampleTest/EntityFrameworkCore/ExecuteSqlRawTest.cs
@@ -44,6 +44,8 @@ public class ExecuteSqlRawTest : IDisposable {
 
 		// Act
 		var result = await _context.Database.ExecuteSqlRawAsync(sql, param);
+		// 実行されるSQL
+		// set @p = 1
 
 		// Assert
 		// 戻り値は0ではない（ドキュメントに記載を見つけられず）
@@ -56,11 +58,14 @@ public class ExecuteSqlRawTest : IDisposable {
 	public async Task ExecuteSqlRawAsync_DirectionのOutputを使う必要があるのに使わないとSqlExceptionが発生する() {
 		// Arrange
 		const string sql = "set @p = 1";
+		// パラメーター名に"@"は不要みたい
 		var param = new SqlParameter("p", SqlDbType.Int);
 
 		// Act
 		var exception = await Record.ExceptionAsync(async () => {
 			await _context.Database.ExecuteSqlRawAsync(sql, param);
+			// 実行されるSQL
+			// set @p = 1
 		});
 
 		// Assert
@@ -70,5 +75,51 @@ public class ExecuteSqlRawTest : IDisposable {
 		// パラメーター化クエリ '(@p int)set @p = 1' に必要なパラメーター '@p' が指定されていません。
 
 		Assert.Null(param.Value);
+	}
+
+	// この使い方が良いのかはわからない
+	[Fact]
+	public async Task ExecuteSqlRawAsync_SqlParameterをSQL文に埋め込む() {
+		// Arrange
+		var param = new SqlParameter("@p", SqlDbType.Int) {
+			Direction = ParameterDirection.Output,
+		};
+
+		// パラメーター名がSQL文に埋め込まれる
+		var sql = $"set {param} = 1";
+
+		// Act
+		var result = await _context.Database.ExecuteSqlRawAsync(sql, param);
+		// 実行されるSQL
+		// set @p = 1
+
+		// Assert
+		Assert.Equal(-1, result);
+		Assert.Equal(1, param.Value);
+	}
+
+	[Fact]
+	public async Task ExecuteSqlRawAsync_SqlParameterをSQL文に埋め込むがSqlExceptionが発生する() {
+		// Arrange
+		// パラメーター名の接頭辞に"@"を付けないとSqlExceptionが発生する
+		var param = new SqlParameter("p", SqlDbType.Int) {
+			Direction = ParameterDirection.Output,
+		};
+		// パラメーター名がSQL文に埋め込まれるが
+		// "@"で始まらない文字列なのでエラー
+		var sql = $"set {param} = 1";
+
+		// Act
+		var exception = await Record.ExceptionAsync(async () => {
+			await _context.Database.ExecuteSqlRawAsync(sql, param);
+			// 実行されるSQL
+			// set p = 1
+		});
+
+		// Assert
+		Assert.NotNull(exception);
+		Assert.IsType<SqlException>(exception);
+		_output.WriteLine(exception.Message);
+		// '=' 付近に不適切な構文があります。
 	}
 }
