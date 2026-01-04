6fabda28fd817545de1cbe8823d633e500bb354d
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-01-05 05:16:16 +0900

    Remove using

--- src/SampleTest/AspNetCore.TestHost/TestServerTest.cs
+++ src/SampleTest/AspNetCore.TestHost/TestServerTest.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
