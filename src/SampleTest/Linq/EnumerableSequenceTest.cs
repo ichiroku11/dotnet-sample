7ff82305be407f8ae2bc53597a05f3dd95106135
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-01-16 05:13:55 +0900

    Test Enumerable.Sequence

---
+++ src/SampleTest/Linq/EnumerableSequenceTest.cs
@@ -0,0 +1,20 @@
+namespace SampleTest.Linq;
+
+public class EnumerableSequenceTest {
+	[Theory]
+	// 2つ目の引数endInclusiveに指定した値を含む
+	[InlineData(1, 5, 1, new int[] { 1, 2, 3, 4, 5 })]
+	[InlineData(1, 5, 2, new int[] { 1, 3, 5 })]
+	// 3つ目の引数stepによってはendInclusiveを含まない場合もある
+	[InlineData(1, 5, 3, new int[] { 1, 4 })]
+	[InlineData(1, 5, 5, new int[] { 1 })]
+	public void Sequence_指定した引数からシーケンスを生成する(int start, int endInclusive, int step, int[] expected) {
+		// Arrange
+		// Act
+		// 2つ目の引数endInclusiveはシーケンスの最後の値（含む）を指定する
+		var actual = Enumerable.Sequence(start, endInclusive, step);
+
+		// Assert
+		Assert.Equal(expected, actual);
+	}
+}
