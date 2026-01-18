aaa542fb895756045298a9c4853ac5dd784d2ddf
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-01-19 05:08:33 +0900

    Test Enumerable.Sequence

--- src/SampleTest/Linq/EnumerableSequenceTest.cs
+++ src/SampleTest/Linq/EnumerableSequenceTest.cs
@@ -1,6 +1,8 @@
 namespace SampleTest.Linq;
 
-public class EnumerableSequenceTest {
+public class EnumerableSequenceTest(ITestOutputHelper output) {
+	private readonly ITestOutputHelper _output = output;
+
 	[Theory]
 	// 2つ目の引数endInclusiveに指定した値を含む
 	[InlineData(1, 5, 1, new int[] { 1, 2, 3, 4, 5 })]
@@ -22,5 +24,20 @@ public class EnumerableSequenceTest {
 
 		// Assert
 		Assert.Equal(expected, actual);
+	}
+
+	[Theory]
+	// 1から1ずつ増やしても-1にならない
+	[InlineData(1, -1, 1)]
+	// 1から0ずつ増やしても2にならない
+	[InlineData(1, 2, 0)]
+	public void Sequence_ArgumentOutOfRangeExceptionが発生する(int start, int endInclusive, int step) {
+		// Arrange
+		// Act
+		var exception = Record.Exception(() => Enumerable.Sequence(start, endInclusive, step));
+
+		// Assert
+		Assert.IsType<ArgumentOutOfRangeException>(exception);
+		_output.WriteLine(exception.Message);
 	}
 }
