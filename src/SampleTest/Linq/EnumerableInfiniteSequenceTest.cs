f67448268adb430796fb5b584710e2b21f97cfaa
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-01-20 06:04:18 +0900

    Test Enumerable.InfiniteSequence

--- src/SampleTest/Linq/EnumerableInfiniteSequenceTest.cs
+++ src/SampleTest/Linq/EnumerableInfiniteSequenceTest.cs
@@ -4,6 +4,8 @@ public class EnumerableInfiniteSequenceTest {
 	[Theory]
 	[InlineData(1, 1, 3, new[] { 1, 2, 3 })]
 	[InlineData(3, -1, 3, new[] { 3, 2, 1 })]
+	// stepが0でもOK
+	[InlineData(1, 0, 3, new[] { 1, 1, 1 })]
 	public void InfiniteSequence_指定した引数からシーケンスを生成する(int start, int step, int count, int[] expected) {
 		// Arrange
 		// Act
