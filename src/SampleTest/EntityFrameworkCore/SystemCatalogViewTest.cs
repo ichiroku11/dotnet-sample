29d5c034b39be030e5eb76f69c3d0c8f1612ccee
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-01-24 07:22:02 +0900

    Test CatalogView

--- src/SampleTest/EntityFrameworkCore/SystemCatalogViewTest.cs
+++ src/SampleTest/EntityFrameworkCore/SystemCatalogViewTest.cs
@@ -3,7 +3,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SampleTest.EntityFrameworkCore;
 
-public class SystemCatalogViewTest {
+[Collection(CollectionNames.EfCoreSample)]
+public class SystemCatalogViewTest : IDisposable {
 	[Keyless]
 	[Table("schemas", Schema = "sys")]
 	private class SystemSchema {
@@ -32,4 +33,57 @@ public class SystemCatalogViewTest {
 
 		public DbSet<SystemTable> Tables => Set<SystemTable>();
 	}
+
+	private readonly SampleDbContext _context = new();
+
+	public SystemCatalogViewTest() {
+		DropTable();
+		InitTable();
+	}
+
+	public void Dispose() {
+		DropTable();
+
+		_context.Dispose();
+
+		GC.SuppressFinalize(this);
+	}
+
+	private void InitTable() {
+		FormattableString sql = $@"
+create table dbo.Sample(
+	Id int,
+	Name nvarchar(10) not null,
+	constraint PK_Sample primary key(Id)
+);
+
+insert into dbo.Sample(Id, Name)
+output inserted.*
+values
+	(1, N'a'),
+	(2, N'b');";
+
+		_context.Database.ExecuteSql(sql);
+	}
+
+	private void DropTable() {
+		FormattableString sql = $@"
+drop table if exists dbo.Sample;";
+
+		_context.Database.ExecuteSql(sql);
+	}
+
+	[Fact]
+	public async Task スキーマの情報を取得する() {
+		// Arrange
+
+		// Act
+		var schemas = await _context.Schemas.ToListAsync();
+
+		// Assert
+		Assert.Contains(schemas, s => s.Name == "dbo");
+	}
+
+	// todo: tables
+	// todo: partitionsで行数
 }
