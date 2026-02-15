1a51c9106f32fa884b795ccdb19996af7d78d7ac
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-02-15 17:05:52 +0900

    Test MemoryPool

--- src/SampleTest/Buffers/MemoryPoolTest.cs
+++ src/SampleTest/Buffers/MemoryPoolTest.cs
@@ -15,4 +15,21 @@ public class MemoryPoolTest {
 		Assert.False(memory.IsEmpty);
 		Assert.Equal(1024, memory.Length);
 	}
+
+	[Theory]
+	[InlineData(1, 16)]
+	[InlineData(10, 16)]
+	[InlineData(17, 32)]
+	[InlineData(32, 32)]
+	public void Rent_最小バッファサイズを指定して取得したMemoryのLengthプロパティを確認する(int minBufferSize, int actual) {
+		// Arrange
+		using var owner = MemoryPool<int>.Shared.Rent(minBufferSize);
+
+		// Act
+		var memory = owner.Memory;
+
+		// Assert
+		Assert.False(memory.IsEmpty);
+		Assert.Equal(actual, memory.Length);
+	}
 }
