631c62b991443721e109411df7f74054ef11995c
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-02-21 08:56:33 +0900

    Unuse Bind method

--- src/EntraExternalIdWebApp/Program.cs
+++ src/EntraExternalIdWebApp/Program.cs
@@ -12,13 +12,15 @@ services
 	.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
 	.AddMicrosoftIdentityWebApp(
 		configureMicrosoftIdentityOptions: options => {
-			config.GetSection("AzureAd").Bind(options);
+			// AuthorityとClientIdがあればEntra External IDに接続できる様子
+			//config.GetSection("AzureAd").Bind(options);
+			options.Authority = config.GetSection("AzureAd:Authority").Value;
+			options.ClientId = config.GetSection("AzureAd:ClientId").Value;
 		},
 		configureCookieAuthenticationOptions: options => {
 
 		},
 		subscribeToOpenIdConnectMiddlewareDiagnosticsEvents: true);
-
 services.AddAuthorization();
 
 var app = builder.Build();
@@ -26,12 +28,14 @@ var app = builder.Build();
 app.UseAuthentication();
 app.UseAuthorization();
 
+// 認証後にクレームを表示するエンドポイント
 app.MapGet("/claim", static async (ClaimsPrincipal user) => {
 	var claims = user.Claims.Select(claim => new { claim.Type, claim.Value });
 	return TypedResults.Json(claims);
 })
 	.RequireAuthorization();
 
+// サインインへ
 app.MapGet("/account/signin", static () => {
 	var authProps = new AuthenticationProperties {
 		RedirectUri = "/claim",
