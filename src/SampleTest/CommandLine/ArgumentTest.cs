68e5ed76541a949c0834c739fa4115f90a88fc2c
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-03-15 06:03:51 +0900

    Test Argument

--- src/SampleTest/CommandLine/ArgumentTest.cs
+++ src/SampleTest/CommandLine/ArgumentTest.cs
@@ -4,35 +4,39 @@ namespace SampleTest.CommandLine;
 
 public class ArgumentTest {
 	[Fact]
-	public void DefaultValueFactory_string型のインスタンスの場合はnull() {
+	public void Properties_string型のインスタンスのプロパティを確認する() {
 		// Arrange
 		var argument = new Argument<string>("test") {
 		};
 
 		// Act
 		// Assert
+		Assert.False(argument.HasDefaultValue);
 		Assert.Null(argument.DefaultValueFactory);
 	}
 
 	[Fact]
-	public void DefaultValueFactory_int型のインスタンスの場合はnull() {
+	public void Properties_int型のインスタンスのプロパティを確認する() {
 		// Arrange
 		var argument = new Argument<int>("test") {
 		};
 
 		// Act
 		// Assert
+		Assert.False(argument.HasDefaultValue);
 		Assert.Null(argument.DefaultValueFactory);
 	}
 
 	[Fact]
-	public void DefaultValueFactory_bool型のインスタンスの場合は非null() {
+	public void Properties_bool型のインスタンスのプロパティを確認する() {
 		// Arrange
 		var argument = new Argument<bool>("test") {
 		};
 
 		// Act
 		// Assert
+		// boolの場合はデフォルト値がある
+		Assert.True(argument.HasDefaultValue);
 		// boolの場合は非null
 		Assert.NotNull(argument.DefaultValueFactory);
 	}
