4ba3bd34e0f5cdd6e03906cacebcfdf9cabc4ba5
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-03-25 05:26:51 +0900

    Test Option.DefaultValueFactory

--- src/SampleTest/CommandLine/OptionTest.cs
+++ src/SampleTest/CommandLine/OptionTest.cs
@@ -55,4 +55,61 @@ public class OptionTest {
 		Assert.False(option.Recursive);
 		Assert.False(option.Required);
 	}
+
+	[Fact]
+	public void DefaultValueFactory_オプションを指定しない場合はParseのタイミングで呼び出される() {
+		// Arrange
+		var called = false;
+		var option = new Option<int>("--test") {
+			DefaultValueFactory = result => {
+				// Command.Parseのタイミングで呼び出される
+				called = true;
+				return 99;
+			},
+
+		};
+		var command = new Command("test") {
+			option
+		};
+
+		// Act
+		// Assert
+		// Parse前は呼び出されない
+		Assert.False(called);
+
+		var result = command.Parse(["test"]);
+
+		// Parseで呼び出される
+		Assert.True(called);
+
+		Assert.Equal(99, result.GetValue(option));
+	}
+
+	[Fact]
+	public void DefaultValueFactory_オプションを指定した場合は呼び出されない() {
+		// Arrange
+		var called = false;
+		var option = new Option<int>("--test") {
+			DefaultValueFactory = result => {
+				// オプションを指定した場合は呼び出されない
+				Assert.Fail();
+
+				called = true;
+				return 99;
+			},
+		};
+		var command = new Command("test") {
+			option
+		};
+
+		// Act
+		var result = command.Parse(["test", "--test", "100"]);
+
+		// Assert
+		// Parseで呼び出されない
+		Assert.False(called);
+
+		// コマンドライン引数に指定した値が取得できる
+		Assert.Equal(100, result.GetValue(option));
+	}
 }
