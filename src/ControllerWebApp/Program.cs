dc99a8dbc471ad8e004ced3c9ef7aba94af972af
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-04-29 05:39:28 +0900

    Remove Startup

--- src/ControllerWebApp/Program.cs
+++ src/ControllerWebApp/Program.cs
@@ -1,13 +1,23 @@
-namespace ControllerWebApp;
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var env = builder.Environment;
 
-public class Program {
-	public static void Main(string[] args) {
-		CreateHostBuilder(args).Build().Run();
-	}
+services.AddControllers();
 
-	public static IHostBuilder CreateHostBuilder(string[] args) =>
-		Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(webBuilder => {
-				webBuilder.UseStartup<Startup>();
-			});
+var app = builder.Build();
+
+if (env.IsDevelopment()) {
+	app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+app.MapAreaControllerRoute(
+	name: "admin",
+	areaName: "Admin",
+	pattern: "Admin/{controller=AdminDefault}/{action=Index}/{id?}");
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Default}/{action=Index}/{id?}");
+
+app.Run();
