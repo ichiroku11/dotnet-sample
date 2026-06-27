227a8aa8d7776f7407eabc649838768c78d52091
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-06-27 13:12:40 +0900

    Test ActivityContext

---
+++ src/SampleTest/Diagnostics/ActivityContextTest.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace SampleTest.Diagnostics;
+
+public class ActivityContextTest {
+	[Fact]
+	public void Propeties_インスタンスを生成してプロパティを確認する() {
+		// Arrange
+		var expected = new {
+			TraceId = ActivityTraceId.CreateRandom(),
+			SpanId = ActivitySpanId.CreateRandom(),
+			TraceFlags = ActivityTraceFlags.Recorded,
+			TraceState = "test=1",
+		};
+
+		// Act
+		var actual = new ActivityContext(expected.TraceId, expected.SpanId, expected.TraceFlags, expected.TraceState);
+
+		// Assert
+		Assert.Equal(expected.TraceId, actual.TraceId);
+		Assert.Equal(expected.SpanId, actual.SpanId);
+		Assert.Equal(expected.TraceFlags, actual.TraceFlags);
+		Assert.Equal(expected.TraceState, actual.TraceState);
+	}
+}
