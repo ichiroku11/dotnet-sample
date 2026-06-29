6b20c2e708d24b0c35753bd50dee19b7bbe20343
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2026-06-30 05:11:28 +0900

    Refactor test

--- src/SampleTest/Diagnostics/ActivityHttpClientTest.cs
+++ src/SampleTest/Diagnostics/ActivityHttpClientTest.cs
@@ -1,21 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
 using System.Diagnostics;
 using System.Net.Http.Json;
 
 namespace SampleTest.Diagnostics;
 
-public class ActivityHttpClientTest(ITestOutputHelper output) {
+public class ActivityHttpClientTest(ITestOutputHelper output) : IAsyncDisposable {
 	private readonly ITestOutputHelper _output = output;
-
-	// todo: localhostのポート番号を固定？
-	private const string _url = "http://localhost:5000";
+	private readonly WebApplication _app = CreateAndStartWebApp(_url);
 
 	private class TraceResponse {
 		public string TraceParent { get; init; } = "";
 	}
 
-	private static WebApplication CreateWebApp(string url) {
+	// todo: localhostのポート番号を固定？
+	private const string _url = "http://localhost:5000";
+
+	private static WebApplication CreateAndStartWebApp(string url) {
 		var builder = WebApplication.CreateBuilder();
 
 		var app = builder.Build();
@@ -33,16 +35,18 @@ public class ActivityHttpClientTest(ITestOutputHelper output) {
 		// todo: ポート番号
 		app.Urls.Add(url);
 
+		app.Start();
+
 		return app;
+	}
+
+	public async ValueTask DisposeAsync() {
+		await _app.StopAsync();
 	}
 
 	[Fact]
 	public async Task HttpClient_TraceParentヘッダが付与されることを確認する() {
 		// Arrange
-		// todo: appをプライベート変数にして、テストの開始と終了でStartAsyncとStopAsyncを呼び出すようにする
-		var app = CreateWebApp(_url);
-		await app.StartAsync();
-
 		using var client = new HttpClient();
 
 		// Act
@@ -57,8 +61,5 @@ public class ActivityHttpClientTest(ITestOutputHelper output) {
 
 		// Assert
 		Assert.Equal(activity.TraceId, activityContext.TraceId);
-
-		// todo:
-		await app.StopAsync();
 	}
 }
